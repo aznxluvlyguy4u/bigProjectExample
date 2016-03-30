15f1d35548c7682eb0ae00eea32ae0fa36245d0b
Steven <steven@jongensvantechniek.nl>
2016-03-31 01:18:41 +0200

    Implement base APIController, setup serializer and QService

--- src/AppBundle/Controller/APIController.php
+++ src/AppBundle/Controller/APIController.php
@@ -1,14 +1,70 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: c0d3
- * Date: 30/03/16
- * Time: 18:29
- */
 
 namespace AppBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class APIController {
+/**
+ * Class APIController
+ * @package AppBundle\Controller
+ */
+class APIController extends Controller
+{
+  /**
+   * @var \JMS\Serializer\Serializer
+   */
+  private $serializer;
+
+  /**
+   * @var string
+   */
+  private $jsonNamespace  = 'json';
+
+  /**
+   * @var \AppBundle\Service\AWSQueueService
+   */
+  private $queueService;
+
+  /**
+   * @return \JMS\Serializer\Serializer
+   */
+  private function getSerializer()
+  {
+    if($this->serializer == null){
+      $this->serializer = $this->get('jms_serializer');
+    }
+
+    return $this->serializer;
+  }
+
+  /**
+   * @return \AppBundle\Service\AWSQueueService
+   */
+  protected function getQueueService(){
+    if($this->queueService == null){
+      $this->queueService = $this->get('app.aws.queueservice');
+    }
+
+    return $this->queueService;
+  }
+
+  /**
+   * @param $object
+   * @return mixed|string
+   */
+  protected function serializeToJSON($object)
+  {
+    return $this->getSerializer()->serialize($object, $this->jsonNamespace);
+  }
+
+  /**
+   * @param $json
+   * @param $entity
+   * @return array|\JMS\Serializer\scalar|mixed|object
+   */
+  protected function deserializeToObject($json, $entity)
+  {
+    return $this->getSerializer()->deserialize($json, $entity, $this->jsonNamespace);
+  }
 
 }
