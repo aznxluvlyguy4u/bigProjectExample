d0bdf4a6104b75fff5a1ef57df8d1fa45e8ca1a2
Steven <steven@jongensvantechniek.nl>
2016-04-14 21:40:40 +0200

    Refactor AnimalAPI controller, setup default structure add api doc

--- src/AppBundle/Controller/AnimalAPIController.php
+++ src/AppBundle/Controller/AnimalAPIController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Ewe;
+use AppBundle\Entity\Ram;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -9,55 +11,119 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use AppBundle\Entity\Animal;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Component\HttpFoundation\JsonResponse;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 /**
- * @Route("/api/v1")
+ * @Route("/api/v1/animals")
  */
 class AnimalAPIController extends Controller
 {
 
   /**
-   * @Route("/arrival/{arrival}")
+   * Retrieve a list of animals.
+   *
+   * Animal-types = { Ram, Ewe }
+   *
+   *
+   *
+   * @ApiDoc(
+   *   parameters={
+   *      {
+   *        "name"="type",
+   *        "dataType"="string",
+   *        "required"=false,
+   *        "description"=" animal type to retrieve",
+   *        "format"="?type=animal-type"
+   *      },
+   *   },
+   *   resource = true,
+   *   description = "Retrieve a list of animals",
+   *   output = "AppBundle\Entity\Animal"
+   * )
+   * @param Request $request the request object
+   * @param string $state
+   * @return JsonResponse
+   * @Route("/")
    * @Method("GET")
    */
-  public function getIndexAction()
+  public function getAllAnimalsByType()
   {
     $animals = $this->getDoctrine()->getRepository('AppBundle:Animal')->findAll();
 
-    return new JsonResponse($animals);
+    return new JsonResponse($animals, 200);
   }
 
   /**
-   * @Route("/animal/{id}")
+   * Retrieve an animal, found by it's Id.
+   *
+   * @ApiDoc(
+   *   resource = true,
+   *   description = "Retrieve an Animal by given ID",
+   *   output = "AppBundle\Entity\Animal"
+   * )
+   *
+   * @param int $Id Id of the Animal to be returned
+   *
+   * @return JsonResponse
+   *
    * @ParamConverter("animal", class="AppBundle:Animal")
    * @Method("GET")
    */
-  public function getAnimalAction($animal)
+  public function getAnimalById($animal)
   {
-    return new JsonResponse($animal);
+    return new JsonResponse($animal, 200);
   }
 
   /**
-   * @Route("/animal")
+   * Save a new animal.
+   *
+   * @ApiDoc(
+   *   resource = true,
+   *   description = "Save a new animal",
+   *   input = "AppBundle\Entity\Animal",
+   *   output = "AppBundle\Component\HttpFoundation\JsonResponse"
+   * )
+   * @param Request $request the request object
+   * @return JsonResponse
+
+   *
+   * @Route("/")
    * @Method("POST")
    */
-  public function postAnimalAction(Request $request)
+  public function postNewAnimal(Request $request)
   {
-    $owners = $request->request->get('owners');
-    $form = $request->request->get('form');
-    $locations = $request->request->get('locations');
-    $startdate = $request->request->get('startdate');
-    $enddate = $request->request->get('enddate');
+    return new JsonResponse("OK", 200);
+  }
 
-    $ev = $this->get('api.entity.validate');
-    $animal = $ev->validate($request, 'AppBundle\Entity\Animal');
+  /**
+   *
+   * Debug endpoint
+   *
+   * @Route("/test/debug")
+   * @Method("GET")
+   */
+  public function debugAPI(Request $request)
+  {
+    $father = new Ram();
+    $father->setUlnCountryCode("NL");
+    $father->setUlnNumber("00001");
+    $father->setAnimalType(1);
 
-    if (!$animal) {
-      return new JsonResponse($ev->getErrors(), 400);
-    }
+    $mother = new Ewe();
+    $mother->setUlnCountryCode("NL");
+    $mother->setUlnNumber("00002");
+    $mother->setAnimalType(2);
 
-    $em = $this->getDoctrine()->getRepository('AppBundle:Animal')->persist($animal);
+    $child = new Ram();
+    $child->setUlnCountryCode("NL");
+    $child->setUlnNumber("1234566");
+    $child->setAnimalType(1);
+    $child->setDateOfBirth(new \DateTime());
+    $child->setParentFather($father);
+    $child->setParentMother($mother);
 
-    return new JsonResponse($animal);
+    $child = $this->getDoctrine()->getRepository('AppBundle:Ram')->persist($child);
+
+    return new JsonResponse($child, 200);
   }
 }
