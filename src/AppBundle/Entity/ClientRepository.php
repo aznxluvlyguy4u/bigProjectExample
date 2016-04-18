ff20ae2782084fa48ad81c14a604f47eb0dfd113
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-04-18 08:56:08 +0200

    Add database retrieval methods for Client and the RelationNumberKeeper

--- src/AppBundle/Entity/ClientRepository.php
+++ src/AppBundle/Entity/ClientRepository.php
@@ -4,4 +4,20 @@ namespace AppBundle\Entity;
 
 class ClientRepository extends BaseRepository {
 
+
+    public function getByToken($token)
+    {
+
+        $em = $this->getEntityManager();
+        $client = $em->getRepository('AppBundle:Client')->findOneBy(array('accessToken' => $token));
+
+        return $client;
+    }
+
+    public function getRelationNumberKeeper($token)
+    {
+        $client = $this->getByToken($token);
+
+        return $client->getRelationNumberKeeper();
+    }
 }
