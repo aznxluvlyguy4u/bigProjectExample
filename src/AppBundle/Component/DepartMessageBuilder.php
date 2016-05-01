a004644685b034eeea5a59a14de08bba5ffe9803
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-05-01 17:51:40 +0200

    Create the DepartMessageBuilder and update the RequestMessageBuilder accordingly

---
+++ src/AppBundle/Component/DepartMessageBuilder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AppBundle\Component;
+
+use AppBundle\Enumerator\AnimalType;
+use AppBundle\Entity\DeclareDepart;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Common\Collections\ArrayCollection;
+use AppBundle\Entity\Person;
+
+/**
+ * Class DepartMessageBuilderAPIController
+ * @package AppBundle\Controller
+ */
+class DepartMessageBuilder extends MessageBuilderBase
+{
+    /**
+     * @var Person
+     */
+    private $person;
+
+    public function __construct(EntityManager $em)
+    {
+        parent::__construct($em);
+    }
+
+    /**
+     *
+     * Accept front-end input and create a complete NSFO+IenR Message.
+     *
+     * @param DeclareDepart $messageObject the message received from the front-end
+     * @param string $relationNumberKeeper
+     * @return ArrayCollection
+     */
+    public function buildMessage(DeclareDepart $messageObject, Person $person)
+    {
+        $this->person = $person;
+        $baseMessageObject = $this->buildBaseMessageObject($messageObject, $person);
+        $completeMessageObject = $this->addDeclareDepartData($baseMessageObject);
+
+        return $completeMessageObject;
+    }
+
+    /**
+     * @param DeclareDepart $messageObject the message received from the front-end
+     * @param Person $person
+     * @return DeclareDepart
+     */
+    private function addDeclareDepartData(DeclareDepart $messageObject)
+    {
+        $animal = $messageObject->getAnimal();
+        $animal->setAnimalType(AnimalType::sheep);
+
+        //TODO For FASE 2 retrieve the correct location & company for someone having more than one location and/or company.
+        $messageObject->setLocation($this->person->getCompanies()[0]->getLocations()[0]);
+        return $messageObject;
+    }
+
+}
