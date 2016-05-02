2d810f3bde5260086e9daf2a8ba2b9bd550b917f
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-05-02 16:43:31 +0200

    Add birth dates to all mock objects, but keep the dateOfBirth optional for now

--- src/AppBundle/DataFixtures/ORM/MockedAnimal.php
+++ src/AppBundle/DataFixtures/ORM/MockedAnimal.php
@@ -56,11 +56,13 @@ class MockedAnimal implements FixtureInterface, ContainerAwareInterface, Ordered
     self::$mockedParentRam->setUlnCountryCode("NL");
     self::$mockedParentRam->setUlnNumber("11111111");
     self::$mockedParentRam->setAnimalType(AnimalType::sheep);
+    self::$mockedParentRam->setDateOfBirth(new \DateTime());
 
     self::$mockedParentEwe = new Ewe();
     self::$mockedParentEwe->setUlnCountryCode("NL");
     self::$mockedParentEwe->setUlnNumber("222222222");
     self::$mockedParentEwe->setAnimalType(AnimalType::sheep);
+    self::$mockedParentEwe->setDateOfBirth(new \DateTime());
 
     self::$mockedRamWithParents = new Ram();
     self::$mockedRamWithParents->setUlnCountryCode("UK");
