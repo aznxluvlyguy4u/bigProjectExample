52cfa054c1bef01ebeeaaacdda90ecf3b189469b
Steven <steven@jongensvantechniek.nl>
2016-05-07 20:16:14 +0200

    Add constants

--- src/AppBundle/Constant/Constant.php
+++ src/AppBundle/Constant/Constant.php
@@ -13,7 +13,13 @@ class Constant
     const REQUEST_ID_NAMESPACE = 'requestId';
     const RESULT_NAMESPACE = "result";
     const CONTINENT_NAMESPACE = 'continent';
+    const LOCATION_NAMESPACE = 'location';
+    const ANIMAL_TYPE_NAMESPACE = 'type';
     const ALL_NAMESPACE = 'all';
+    const ALIVE_NAMESPACE = 'alive';
+    const IS_ALIVE_NAMESPACE = 'isAlive';
+    const BOOLEAN_TRUE_NAMESPACE = 'true';
+    const UNKNOWN_NAMESPACE = 'unknown';
 
     //Request repositories
     const DECLARE_ARRIVAL_REPOSITORY = 'AppBundle:DeclareArrival';
@@ -23,5 +29,18 @@ class Constant
     const DECLARE_EARTAGS_TRANSFER_REPOSITORY = 'AppBundle:DeclareEartagsTransfer';
     const DECLARE_LOSS_REPOSITORY = 'AppBundle:DeclareLoss';
 
+    const ANIMAL_REPOSITORY = 'AppBundle:Animal';
+
+    const EWE_REPOSITORY = 'AppBundle:Ewe';
+    const RAM_REPOSITORY = 'AppBundle:Ram';
+    const NEUTER_REPOSITORY = 'AppBundle:Neuter';
+
+    const PERSON_REPOSITORY = 'AppBundle:Person';
+    const CLIENT_REPOSITORY = 'AppBundle:Client';
+    const EMPLOYEE_REPOSITORY = 'AppBundle:Employee';
+
     const COUNTRY_REPOSITORY = 'AppBundle:Country';
+    const COMPANY_REPOSITORY = 'AppBundle:Company';
+    const LOCATION_REPOSITORY = 'AppBundle:Location';
+
 }
