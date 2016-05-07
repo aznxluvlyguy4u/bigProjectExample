73ad65011e88c080ab257453a1e1430f5a8d8c67
Steven <steven@jongensvantechniek.nl>
2016-05-07 20:22:07 +0200

    Implement custom findByUser for retrieving locations

--- src/AppBundle/Entity/LocationRepository.php
+++ src/AppBundle/Entity/LocationRepository.php
@@ -2,11 +2,35 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+
 /**
  * Class LocationRepository
  * @package AppBundle\Entity
  */
 class LocationRepository extends BaseRepository
 {
+  /**
+   * @return ArrayCollection
+   */
+  public function findByUser($user)
+  {
+    //Result set
+    $locations = new ArrayCollection();
 
+    //Get companies of user
+    $companies = $user->getCompanies();
+
+    foreach($companies as $company) {
+      //Get locations of every company, add it to result set
+      $locationsRetrieved = $company->getLocations();
+
+      foreach($locationsRetrieved as $location) {
+        $locations->add($location);
+      }
+
+    }
+
+    return $locations;
+  }
 }
