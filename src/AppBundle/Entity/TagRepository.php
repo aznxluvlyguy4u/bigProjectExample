6e0d76a1a0c40e2d28b1ac5128d5a88aa11d31e9
Steven <steven@jongensvantechniek.nl>
2016-05-08 19:46:38 +0200

    Add custom filter to Tag repository, findby uln & countryCode

--- src/AppBundle/Entity/TagRepository.php
+++ src/AppBundle/Entity/TagRepository.php
@@ -4,4 +4,13 @@ namespace AppBundle\Entity;
 
 class TagRepository extends BaseRepository {
 
+  /**
+   * @param $ulnCountryCode
+   * @param $ulnNumber
+   * @return array|null
+   */
+  public function findByUlnNumberAndCountryCode($ulnCountryCode, $ulnNumber)
+  {
+    return $this->findOneBy(array('ulnCountryCode'=>$ulnCountryCode, 'ulnNumber'=>$ulnNumber));
+  }
 }
