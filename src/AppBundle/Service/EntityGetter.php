92bd29b0372af27f80867572b31e7e5a4117aab7
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-05-10 12:30:09 +0200

    Map animal values from the array to the newly created object

--- src/AppBundle/Service/EntityGetter.php
+++ src/AppBundle/Service/EntityGetter.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Service;
 
 use AppBundle\Constant\Constant;
+use AppBundle\Enumerator\RequestType;
 use \AppBundle\Entity\Ewe;
 use AppBundle\Entity\Neuter;
 use AppBundle\Entity\Ram;
@@ -115,77 +116,103 @@ class EntityGetter
             $ulnNumber = $animalContentArray[Constant::ULN_NUMBER_NAMESPACE];
             $countryCode = $animalContentArray[Constant::ULN_COUNTRY_CODE_NAMESPACE];
 
+            //Find registered tag, assign to this animal
+            $tag = $this->entityManager->getRepository(Constant::TAG_REPOSITORY)->findByUlnNumberAndCountryCode($countryCode, $ulnNumber);
+            $animal->setAssignedTag($tag);
+            $animal->setAnimalType(AnimalType::sheep);
+            $animal->setIsAlive(true);
+
+            //Set the uln in the newly created animal
+            $animal->setUlnCountryCode($countryCode);
+            $animal->setUlnNumber($ulnNumber);
+
         } else if(array_key_exists(Constant::PEDIGREE_NUMBER_NAMESPACE, $animalContentArray) && array_key_exists(Constant::PEDIGREE_COUNTRY_CODE_NAMESPACE, $animalContentArray)) {
-            $ulnNumber = $animalContentArray[Constant::PEDIGREE_NUMBER_NAMESPACE];
+            $pedigreeNumber = $animalContentArray[Constant::PEDIGREE_NUMBER_NAMESPACE];
             $countryCode = $animalContentArray[Constant::PEDIGREE_COUNTRY_CODE_NAMESPACE];
+
+            //Set the pedigree number in the newly created animal
+            $animal->setPedigreeCountryCode($countryCode);
+            $animal->setPedigreeNumber($pedigreeNumber);
         }
 
-        //Find registered tag, assign to this animal
-        $tag = $this->entityManager->getRepository(Constant::TAG_REPOSITORY)->findByUlnNumberAndCountryCode($countryCode, $ulnNumber);
-        $animal->setAssignedTag($tag);
-        $animal->setAnimalType(AnimalType::sheep);
-        $animal->setIsAlive(true);
+        //Transfer values from the array to the newly created animal
+        if (array_key_exists('name', $declareContentArray)) {
+            $animal->setName($declareContentArray['name']); }
 
-        if(array_key_exists('date_of_birth', $declareContentArray)){
-            $animal->setDateOfBirth(new \DateTime($declareContentArray['date_of_birth']));
+        if (array_key_exists('date_of_birth', $declareContentArray)) {
+            $animal->setDateOfBirth(new \DateTime($declareContentArray['date_of_birth'])); }
+
+        if (array_key_exists('date_of_death', $declareContentArray)) {
+            $animal->setDateOfDeath(new \DateTime($declareContentArray['date_of_death'])); }
+
+        if (array_key_exists('gender', $declareContentArray)) {
+            $animal->setGender($declareContentArray['gender']); }
+
+        if (array_key_exists('parent_father', $declareContentArray)) {
+            $animal->setParentFather($declareContentArray['parent_father']); }
+
+        if (array_key_exists('parent_mother', $declareContentArray)) {
+            $animal->setParentMother($declareContentArray['parent_mother']); }
+
+        if (array_key_exists('parent_neuter', $declareContentArray)) {
+            $animal->setParentNeuter($declareContentArray['parent_neuter']); }
+
+        if (array_key_exists('animal_type', $declareContentArray)) {
+            $animal->setAnimalType($declareContentArray['animal_type']);
+        } else { //by default it is a sheep
+            $animal->setAnimalType(AnimalType::sheep);
         }
 
-        if(array_key_exists('date_of_death', $declareContentArray)){
-            $animal->setDateOfDeath(new \DateTime($declareContentArray['date_of_death']));
+        if (array_key_exists('animal_category', $declareContentArray)) {
+            $animal->setAnimalCategory($declareContentArray['animal_category']); }
+
+        if (array_key_exists('animal_order_number', $declareContentArray)) {
+            $animal->setAnimalOrderNumber($declareContentArray['animal_order_number']); }
+
+        if (array_key_exists('animal_hair_colour', $declareContentArray)) {
+            $animal->setAnimalHairColour($declareContentArray['animal_hair_colour']); }
+
+        //Note a newly created animal cannot have any messages yet,
+        //(like arrivals, departures, imports)
+        //so it is not necessary to map them here.
+        //TODO Check if it is necessary to also create an new animal including the children/parents/surrogateMother etc.
+
+
+        //TODO After the june2016 deadline organize the if statements above in this switch case
+        switch($declareType) {
+            case RequestType::DECLARATION_DETAIL_ENTITY:
+                //TODO: only add the mininum required fields for this Message Type
+            case RequestType::DECLARE_ANIMAL_FLAG_ENTITY:
+                //TODO: only add the mininum required fields for this Message Type
+            case RequestType::DECLARE_ARRIVAL_ENTITY:
+                //TODO: only add the mininum required fields for this Message Type
+            case RequestType::DECLARE_BIRTH_ENTITY:
+                //TODO: only add the mininum required fields for this Message Type
+            case RequestType::DECLARE_DEPART_ENTITY:
+                //TODO: only add the mininum required fields for this Message Type
+            case RequestType::DECLARE_EARTAGS_TRANSFER_ENTITY:
+                //TODO: only add the mininum required fields for this Message Type
+            case RequestType::DECLARE_LOSS_ENTITY:
+                //TODO: only add the mininum required fields for this Message Type
+            case RequestType::DECLARE_EXPORT_ENTITY:
+                //TODO: only add the mininum required fields for this Message Type
+            case RequestType::DECLARE_IMPORT_ENTITY:
+                //TODO: only add the mininum required fields for this Message Type
+            case RequestType::RETRIEVE_EARTAGS_ENTITY:
+                //TODO: only add the mininum required fields for this Message Type
+            case RequestType::REVOKE_DECLARATION_ENTITY:
+                //TODO: only add the mininum required fields for this Message Type
+            default:
+                break;
+
         }
 
-        //Persist the new Neuter with an unregistered Tag
+
+        //Persist the new Ram/Ewe/Neuter with an unregistered Tag
         $this->entityManager->persist($animal);
         //$this->entityManager->persist($tag);
         $this->entityManager->flush();
 
         return $animal;
     }
-
-    //TODO Add switch case for setting values
-//    <<<<<<< HEAD
-//        if (array_key_exists('name', $animal)) {
-//            $neuter->setName($animal['name']); }
-//
-//        //FIXME: if dateOfBirth is refactored out of Animal into the message, delete this
-//        if (array_key_exists('date_of_birth', $animal)) {
-//            $neuter->setDateOfBirth(new \DateTime($animal['date_of_birth'])); }
-//
-//        //FIXME: if dateOfDeath is refactored out of Animal into the message, delete this
-//        if (array_key_exists('date_of_death', $animal)) {
-//            $neuter->setDateOfDeath(new \DateTime($animal['date_of_death'])); }
-//
-//        if (array_key_exists('gender', $animal)) {
-//            $neuter->setGender($animal['gender']); }
-//
-//        if (array_key_exists('parent_father', $animal)) {
-//            $neuter->setParentFather($animal['parent_father']); }
-//
-//        if (array_key_exists('parent_mother', $animal)) {
-//            $neuter->setParentMother($animal['parent_mother']); }
-//
-//        if (array_key_exists('parent_neuter', $animal)) {
-//            $neuter->setParentNeuter($animal['parent_neuter']); }
-//
-//        if (array_key_exists('animal_type', $animal)) {
-//            $neuter->setAnimalType($animal['animal_type']); }
-//
-//        if (array_key_exists('animal_category', $animal)) {
-//            $neuter->setAnimalCategory($animal['animal_category']); }
-//
-//        if (array_key_exists('animal_working_number', $animal)) {
-//            $neuter->setAnimalWorkingNumber($animal['animal_working_number']); }
-//
-//        if (array_key_exists('animal_hair_colour', $animal)) {
-//            $neuter->setAnimalHairColour($animal['animal_hair_colour']); }
-//
-//        //Note a newly created neuter cannot have any messages yet,
-//        //(like arrivals, departures, imports)
-//        //so it is not necessary to map them here.
-//        //TODO Check again for all messages if this is correct: A newly created neuter is also not created with any children.
-//
-//
-//        $neuter->setAnimalType(AnimalType::sheep);
-//=======
-
 }
