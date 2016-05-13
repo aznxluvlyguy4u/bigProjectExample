ec1efcb8546c34ec3e7b49dd48831c9c0b846491
Steven <steven@jongensvantechniek.nl>
2016-05-13 17:03:10 +0200

    Implment TagTransfer messsage update method

--- src/AppBundle/Entity/DeclareEartagsTransferRepository.php
+++ src/AppBundle/Entity/DeclareEartagsTransferRepository.php
@@ -2,10 +2,34 @@
 
 namespace AppBundle\Entity;
 
+use AppBundle\Constant\Constant;
+
 /**
  * Class DeclareEartagsTransferRepository
  * @package AppBundle\Entity
  */
 class DeclareEartagsTransferRepository extends BaseRepository {
 
+  /**
+   * @param $declareTagsTransferUpdate
+   * @param $Id
+   * @return null|object
+   */
+  public function updateDeclareTagsTransferMessage($declareTagsTransferUpdate, $Id) {
+    $declareEartagsTransfer = $this->findOneBy(array (Constant::REQUEST_ID_NAMESPACE => $Id));
+
+    if($declareEartagsTransfer == null) {
+      return null;
+    }
+
+    if ($declareTagsTransferUpdate->getTags() != null) {
+      $declareEartagsTransfer->setTag($declareTagsTransferUpdate->getTags());
+    }
+
+    if ($declareTagsTransferUpdate->getRelationNumberAcceptant()) {
+      $declareEartagsTransfer->setRelationNumberAcceptant($declareTagsTransferUpdate->getRelationNumberAcceptant());
+    }
+
+    return $declareEartagsTransfer;
+  }
 }
