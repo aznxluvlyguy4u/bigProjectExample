e56146817f8347ed694329e412ea269720b0bef9
Steven <steven@jongensvantechniek.nl>
2016-05-16 18:17:26 +0200

    Re-add persisting RetrieveTags

--- src/AppBundle/Controller/AnimalAPIController.php
+++ src/AppBundle/Controller/AnimalAPIController.php
@@ -165,7 +165,7 @@ class AnimalAPIController extends APIController implements AnimalAPIControllerIn
       $messageObject = $this->buildMessageObject(RequestType::RETRIEVE_ANIMALS_ENTITY, $content, $this->getAuthenticatedUser($request));
 
       //First Persist object to Database, before sending it to the queue
-     // $this->persist($messageObject, RequestType::RETRIEVE_ANIMALS_ENTITY);
+      $this->persist($messageObject, RequestType::RETRIEVE_ANIMALS_ENTITY);
 
       //Send it to the queue and persist/update any changed state to the database
       $this->sendMessageObjectToQueue($messageObject, RequestType::RETRIEVE_ANIMALS_ENTITY, RequestType::RETRIEVE_ANIMALS);
