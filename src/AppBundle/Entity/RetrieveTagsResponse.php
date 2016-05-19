dcce04f26d048d91eff7220e4e6506e4289ebfe7
Steven <steven@jongensvantechniek.nl>
2016-05-19 17:09:09 +0200

    Refactor RetrieveTags request & response, detach DeclareBase

--- src/AppBundle/Entity/RetrieveTagsResponse.php
+++ src/AppBundle/Entity/RetrieveTagsResponse.php
@@ -12,13 +12,76 @@ use JMS\Serializer\Annotation as JMS;
  * @ORM\Entity(repositoryClass="AppBundle\Entity\RetrieveTagsResponseRepository")
  * @package AppBundle\Entity
  */
-class RetrieveTagsResponse extends DeclareBaseResponse {
+class RetrieveTagsResponse
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
 
     /**
-     * @var RetrieveTags
+     * @ORM\Column(type="datetime")
+     * @Assert\Date
+     * @Assert\NotBlank
+     * @JMS\Type("DateTime")
+     */
+    private $logDate;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\Length(max = 20)
+     * @Assert\NotBlank
+     * @JMS\Type("string")
+     */
+    private $requestId;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\Length(max = 20)
+     * @Assert\NotBlank
+     * @JMS\Type("string")
+     */
+    private $messageId;
+
+    /**
+     * @var string;
+     *
+     * @ORM\Column(type="string", nullable=true)
+     *
+     */
+    private $errorCode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $errorMessage;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", nullable=true)
+     * @Assert\Length(max = 1)
+     */
+    private $errorKindIndicator;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", nullable=true)
+     * @Assert\Length(max = 1)
+     */
+    private $successIndicator;
+
+    /**
+     * @var RetrieveAnimals
      *
      * @Assert\NotBlank
-     * @ORM\ManyToOne(targetEntity="RetrieveTags", cascade={"persist"}, inversedBy="responses")
+     * @ORM\ManyToOne(targetEntity="RetrieveTags")
+     * @ORM\JoinColumn(name="retrieve_tags_id", referencedColumnName="id")
      * @JMS\Type("AppBundle\Entity\RetrieveTags")
      */
     private $retrieveTagsRequestMessage;
@@ -35,6 +98,9 @@ class RetrieveTagsResponse extends DeclareBaseResponse {
      */
     private $tagsRetrieved;
 
+    /**
+     * RetrieveTagsResponse constructor.
+     */
     public function __construct() {
       $this->tagsRetrieved = new ArrayCollection();
     }
@@ -95,5 +161,183 @@ class RetrieveTagsResponse extends DeclareBaseResponse {
     public function getTagsRetrieved()
     {
         return $this->tagsRetrieved;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set logDate
+     *
+     * @param \DateTime $logDate
+     *
+     * @return RetrieveTagsResponse
+     */
+    public function setLogDate($logDate)
+    {
+        $this->logDate = $logDate;
+
+        return $this;
+    }
+
+    /**
+     * Get logDate
+     *
+     * @return \DateTime
+     */
+    public function getLogDate()
+    {
+        return $this->logDate;
+    }
+
+    /**
+     * Set requestId
+     *
+     * @param string $requestId
+     *
+     * @return RetrieveTagsResponse
+     */
+    public function setRequestId($requestId)
+    {
+        $this->requestId = $requestId;
+
+        return $this;
+    }
+
+    /**
+     * Get requestId
+     *
+     * @return string
+     */
+    public function getRequestId()
+    {
+        return $this->requestId;
+    }
+
+    /**
+     * Set messageId
+     *
+     * @param string $messageId
+     *
+     * @return RetrieveTagsResponse
+     */
+    public function setMessageId($messageId)
+    {
+        $this->messageId = $messageId;
+
+        return $this;
+    }
+
+    /**
+     * Get messageId
+     *
+     * @return string
+     */
+    public function getMessageId()
+    {
+        return $this->messageId;
+    }
+
+    /**
+     * Set errorCode
+     *
+     * @param string $errorCode
+     *
+     * @return RetrieveTagsResponse
+     */
+    public function setErrorCode($errorCode)
+    {
+        $this->errorCode = $errorCode;
+
+        return $this;
+    }
+
+    /**
+     * Get errorCode
+     *
+     * @return string
+     */
+    public function getErrorCode()
+    {
+        return $this->errorCode;
+    }
+
+    /**
+     * Set errorMessage
+     *
+     * @param string $errorMessage
+     *
+     * @return RetrieveTagsResponse
+     */
+    public function setErrorMessage($errorMessage)
+    {
+        $this->errorMessage = $errorMessage;
+
+        return $this;
+    }
+
+    /**
+     * Get errorMessage
+     *
+     * @return string
+     */
+    public function getErrorMessage()
+    {
+        return $this->errorMessage;
+    }
+
+    /**
+     * Set errorKindIndicator
+     *
+     * @param string $errorKindIndicator
+     *
+     * @return RetrieveTagsResponse
+     */
+    public function setErrorKindIndicator($errorKindIndicator)
+    {
+        $this->errorKindIndicator = $errorKindIndicator;
+
+        return $this;
+    }
+
+    /**
+     * Get errorKindIndicator
+     *
+     * @return string
+     */
+    public function getErrorKindIndicator()
+    {
+        return $this->errorKindIndicator;
+    }
+
+    /**
+     * Set successIndicator
+     *
+     * @param string $successIndicator
+     *
+     * @return RetrieveTagsResponse
+     */
+    public function setSuccessIndicator($successIndicator)
+    {
+        $this->successIndicator = $successIndicator;
+
+        return $this;
+    }
+
+    /**
+     * Get successIndicator
+     *
+     * @return string
+     */
+    public function getSuccessIndicator()
+    {
+        return $this->successIndicator;
     }
 }
