2520bde23bd530b0978ec09b1f6baac005b095be
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-05-19 22:51:05 +0200

    Remove duplicate import and export, create and edit

--- src/AppBundle/Controller/ExportAPIControllerInterface.php
+++ src/AppBundle/Controller/ExportAPIControllerInterface.php
@@ -7,6 +7,4 @@ use Symfony\Component\HttpFoundation\Request;
 interface ExportAPIControllerInterface {
   public function getExportById(Request $request, $Id);
   public function getExports(Request $request);
-  public function createExport(Request $request);
-  public function updateExport(Request $request, $Id);
 }
--- src/AppBundle/Controller/ImportAPIControllerInterface.php
+++ src/AppBundle/Controller/ImportAPIControllerInterface.php
@@ -7,6 +7,4 @@ use Symfony\Component\HttpFoundation\Request;
 interface ImportAPIControllerInterface {
   public function getImportById(Request $request, $Id);
   public function getImports(Request $request);
-  public function createImport(Request $request);
-  public function updateImport(Request $request, $Id);
 }
