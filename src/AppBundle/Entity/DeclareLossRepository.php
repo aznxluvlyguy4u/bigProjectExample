fa1432ab479e9a4790e66fffa43ff3cb33444757
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-05-21 23:35:30 +0200

    Delete old outcommented code

--- src/AppBundle/Entity/DeclareLossRepository.php
+++ src/AppBundle/Entity/DeclareLossRepository.php
@@ -33,48 +33,5 @@ class DeclareLossRepository extends BaseRepository {
 
         return $this->getRequestByRequestId($losses, $requestId);
     }
-    
-//    /**
-//     * @param Client $client
-//     * @param string $state
-//     * @return ArrayCollection
-//     */
-//    public function getLosses(Client $client, $state = null)
-//    {
-//        $location = $client->getCompanies()->get(0)->getLocations()->get(0);
-//        $retrievedLosses = $location->getLosses();
-//
-//        if($state == null) {
-//            $declareLosses = $retrievedLosses;
-//
-//        } else {
-//            $declareLosses = new ArrayCollection();
-//            foreach($retrievedLosses as $retrievedLoss) {
-//                if($retrievedLoss->getRequestState() == $state) {
-//                    $declareLosses->add($retrievedLoss);
-//                }
-//            }
-//        }
-//
-//        return $declareLosses;
-//    }
-//
-//    /**
-//     * @param Client $client
-//     * @param string $requestId
-//     * @return DeclareLoss|null
-//     */
-//    public function getLossesById(Client $client, $requestId)
-//    {
-//        $losses = $this->getLosses($client);
-//
-//        foreach($losses as $loss) {
-//            $foundRequestId = $loss->getRequestId($requestId);
-//            if($foundRequestId == $requestId) {
-//                return $loss;
-//            }
-//        }
-//
-//        return null;
-//    }
+
 }
