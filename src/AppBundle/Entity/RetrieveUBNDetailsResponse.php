8867433d04f5bbaf234ba2ef2a9a243d508bba41
Steven <steven@jongensvantechniek.nl>
2016-05-21 19:41:18 +0200

    Add companyHolderName field, match I&R response model

--- src/AppBundle/Entity/RetrieveUBNDetailsResponse.php
+++ src/AppBundle/Entity/RetrieveUBNDetailsResponse.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use JMS\Serializer\Annotation as JMS;
@@ -76,46 +77,17 @@ class RetrieveUbnDetailsResponse
     private $successIndicator;
 
     /**
-     * @var string
-     * @ORM\Column(type="string", nullable=true)
+     * @var arrayCollection
      */
-    private $ubnNumber;
+    private $locations;
 
-    /**
-     * @var string
-     * @ORM\Column(type="string", nullable=true)
-     */
-    private $ubnStreetName;
-
-    /**
-     * @var string
-     * @ORM\Column(type="string", nullable=true)
-     */
-    private $ubnPostalCode;
-
-    /**
-     * @var string
-     * @ORM\Column(type="string", nullable=true)
-     */
-    private $ubnCity;
-
-    /**
-     * @var string
-     * @ORM\Column(type="string", nullable=true)
-     */
-    private $companyType;
-
-    /**
-     * @var string
-     * @ORM\Column(type="string", nullable=true)
-     */
-    private $companyHolderName;
 
     /**
      * RetrieveTagsResponse constructor.
      */
     public function __construct() {
-      $this->setLogDate(new \DateTime());
+        $this->setLogDate(new \DateTime());
+        $this->locations = new ArrayCollection();
     }
 
     /**
@@ -294,149 +266,5 @@ class RetrieveUbnDetailsResponse
     public function getSuccessIndicator()
     {
       return $this->successIndicator;
-    }
-
-    /**
-     * Set ubnNumber
-     *
-     * @param string $ubnNumber
-     *
-     * @return RetrieveUBNDetailsResponse
-     */
-    public function setUbnNumber($ubnNumber)
-    {
-        $this->ubnNumber = $ubnNumber;
-
-        return $this;
-    }
-
-    /**
-     * Get ubnNumber
-     *
-     * @return string
-     */
-    public function getUbnNumber()
-    {
-        return $this->ubnNumber;
-    }
-
-    /**
-     * Set ubnStreetName
-     *
-     * @param string $ubnStreetName
-     *
-     * @return RetrieveUBNDetailsResponse
-     */
-    public function setUbnStreetName($ubnStreetName)
-    {
-        $this->ubnStreetName = $ubnStreetName;
-
-        return $this;
-    }
-
-    /**
-     * Get ubnStreetName
-     *
-     * @return string
-     */
-    public function getUbnStreetName()
-    {
-        return $this->ubnStreetName;
-    }
-
-    /**
-     * Set ubnPostalCode
-     *
-     * @param string $ubnPostalCode
-     *
-     * @return RetrieveUBNDetailsResponse
-     */
-    public function setUbnPostalCode($ubnPostalCode)
-    {
-        $this->ubnPostalCode = $ubnPostalCode;
-
-        return $this;
-    }
-
-    /**
-     * Get ubnPostalCode
-     *
-     * @return string
-     */
-    public function getUbnPostalCode()
-    {
-        return $this->ubnPostalCode;
-    }
-
-    /**
-     * Set ubnCity
-     *
-     * @param string $ubnCity
-     *
-     * @return RetrieveUBNDetailsResponse
-     */
-    public function setUbnCity($ubnCity)
-    {
-        $this->ubnCity = $ubnCity;
-
-        return $this;
-    }
-
-    /**
-     * Get ubnCity
-     *
-     * @return string
-     */
-    public function getUbnCity()
-    {
-        return $this->ubnCity;
-    }
-
-    /**
-     * Set companyType
-     *
-     * @param string $companyType
-     *
-     * @return RetrieveUBNDetailsResponse
-     */
-    public function setCompanyType($companyType)
-    {
-        $this->companyType = $companyType;
-
-        return $this;
-    }
-
-    /**
-     * Get companyType
-     *
-     * @return string
-     */
-    public function getCompanyType()
-    {
-        return $this->companyType;
-    }
-
-    /**
-     * Set companyHolderName
-     *
-     * @param string $companyHolderName
-     *
-     * @return RetrieveUBNDetailsResponse
-     */
-    public function setCompanyHolderName($companyHolderName)
-    {
-        $this->companyHolderName = $companyHolderName;
-
-        return $this;
-    }
-
-    /**
-     * Get companyHolderName
-     *
-     * @return string
-     */
-    public function getCompanyHolderName()
-    {
-        return $this->companyHolderName;
     }
 }
