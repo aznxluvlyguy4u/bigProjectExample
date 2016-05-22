9e89bd2da7b12dd011f04ab3915729fedc2f0b3d
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-05-22 23:58:01 +0200

    Refactor variable name

--- src/AppBundle/Output/DeclareImportResponseOutput.php
+++ src/AppBundle/Output/DeclareImportResponseOutput.php
@@ -3,49 +3,48 @@
 namespace AppBundle\Output;
 
 
-use AppBundle\Entity\DeclareArrival;
 use AppBundle\Entity\DeclareImport;
 
 class DeclareImportResponseOutput
 {
     /**
-     * @param DeclareImport $arrival
+     * @param DeclareImport $import
      * @return array
      */
-    public static function createHistoryResponse($arrival)
+    public static function createHistoryResponse($import)
     {
         return array(
-            "request_id" => $arrival->getRequestId(),
-            "log_datum" => $arrival->getLogDate(),
-            "uln_country_code" => $arrival->getUlnCountryCode(),
-            "uln_number" => $arrival->getUlnNumber(),
-            "pedigree_country_code" => $arrival->getPedigreeCountryCode(),
-            "pedigree_number" => $arrival->getPedigreeNumber(),
-            "is_import_animal" => $arrival->getIsImportAnimal(),
-            "arrival_date" => $arrival->getImportDate(),
-            "animal_country_origin" => $arrival->getAnimalCountryOrigin(),
-            "request_state" => $arrival->getRequestState()
+            "request_id" => $import->getRequestId(),
+            "log_datum" => $import->getLogDate(),
+            "uln_country_code" => $import->getUlnCountryCode(),
+            "uln_number" => $import->getUlnNumber(),
+            "pedigree_country_code" => $import->getPedigreeCountryCode(),
+            "pedigree_number" => $import->getPedigreeNumber(),
+            "is_import_animal" => $import->getIsImportAnimal(),
+            "import_date" => $import->getImportDate(),
+            "animal_country_origin" => $import->getAnimalCountryOrigin(),
+            "request_state" => $import->getRequestState()
         );
     }
 
     /**
-     * @param DeclareImport $arrival
+     * @param DeclareImport $import
      * @return array
      */
-    public static function createErrorResponse($arrival)
+    public static function createErrorResponse($import)
     {
-        $lastResponse = $arrival->getResponses()->last();
+        $lastResponse = $import->getResponses()->last();
 
-        $res = array("request_id" => $arrival->getRequestId(),
-            "log_datum" => $arrival->getLogDate(),
-            "uln_country_code" => $arrival->getUlnCountryCode(),
-            "uln_number" => $arrival->getUlnNumber(),
-            "pedigree_country_code" => $arrival->getPedigreeCountryCode(),
-            "pedigree_number" => $arrival->getPedigreeNumber(),
-            "is_import_animal" => $arrival->getIsImportAnimal(),
-            "arrival_date" => $arrival->getImportDate(),
-            "animal_country_origin" => $arrival->getAnimalCountryOrigin(),
-            "request_state" => $arrival->getRequestState(),
+        $res = array("request_id" => $import->getRequestId(),
+            "log_datum" => $import->getLogDate(),
+            "uln_country_code" => $import->getUlnCountryCode(),
+            "uln_number" => $import->getUlnNumber(),
+            "pedigree_country_code" => $import->getPedigreeCountryCode(),
+            "pedigree_number" => $import->getPedigreeNumber(),
+            "is_import_animal" => $import->getIsImportAnimal(),
+            "import_date" => $import->getImportDate(),
+            "animal_country_origin" => $import->getAnimalCountryOrigin(),
+            "request_state" => $import->getRequestState(),
             "error_code" => $lastResponse->getErrorCode(),
             "error_message" => $lastResponse->getErrorMessage()
         );
