bc6f2e7fda1caefbf8144870a3b8ddb63f0d66a2
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-05-23 13:11:02 +0200

    Add notes for mandatory IenR  fields to Ext Worker Queue

--- src/AppBundle/Output/DeclareArrivalOutput.php
+++ src/AppBundle/Output/DeclareArrivalOutput.php
@@ -24,26 +24,28 @@ class DeclareArrivalOutput
         }
 
         $result = array("id" => $arrival->getId(),
-            "request_id" => $arrival->getRequestId(),
             "message_id" => $arrival->getMessageId(),
-            "request_state" => $arrival->getRequestState(),
-            "action" => $arrival->getAction(),
-            "recovery_indicator" => $arrival->getRecoveryIndicator(),
-            "relation_number_keeper" => $arrival->getRelationNumberKeeper(),
+            "request_id" => $arrival->getRequestId(), //Mandatory for IenR
+            "relation_number_keeper" => $arrival->getRelationNumberKeeper(),  //Mandatory for IenR
+            "ubn" => $arrival->getUbn(),  //Mandatory for IenR,
+            "action" => $arrival->getAction(),  //Mandatory for IenR
+            "recovery_indicator" => $arrival->getRecoveryIndicator(),  //Mandatory for IenR
+            "arrival_date" => $arrival->getArrivalDate(),  //Mandatory for IenR
             "log_date" => $arrival->getLogDate(),
-            "arrival_date" => $arrival->getArrivalDate(),
             "is_import_animal" => $arrival->getIsImportAnimal(),
             "ubn_previous_owner" => $arrival->getUbnPreviousOwner(),
-            "ubn" => $arrival->getUbn(),
+            "request_state" => $arrival->getRequestState(),
             "animal" =>
             array("id" => $animalId,
                   "uln_country_code" => $arrival->getUlnCountryCode(),
                   "uln_number" => $arrival->getUlnNumber(),
                   "pedigree_country_code" => $arrival->getPedigreeCountryCode(),
-                  "pedigree_number" => $arrival->getPedigreeNumber()),
+                  "pedigree_number" => $arrival->getPedigreeNumber()
+                  //FIXME ADD animalType ALSO TO ENTITY  /Mandatory for IenR
+            ),
             "location"=>
             array("id" => $arrival->getLocation()->getId(),
-                  "ubn" => $arrival->getLocation()->getUbn())
+                  "ubn" => $arrival->getLocation()->getUbn())  //Mandatory for IenR or use the own above
         );
 
         return $result;
