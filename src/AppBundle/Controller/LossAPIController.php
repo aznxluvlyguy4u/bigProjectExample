be7a1ad26c23df6a059ff0f736f407def489850a
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-05-24 16:14:44 +0200

    Match losses  history and errors to frontend

--- src/AppBundle/Controller/LossAPIController.php
+++ src/AppBundle/Controller/LossAPIController.php
@@ -238,7 +238,7 @@ class LossAPIController extends APIController implements LossAPIControllerInterf
     $repository = $this->getDoctrine()->getRepository(Constant::DECLARE_LOSS_RESPONSE_REPOSITORY);
     $declareLosses = $repository->getLossesWithLastErrorResponses($client);
 
-    return new JsonResponse(array(Constant::RESULT_NAMESPACE => array($declareLosses, 200)), 200);
+    return new JsonResponse(array(Constant::RESULT_NAMESPACE => $declareLosses), 200);
   }
 
 
@@ -255,6 +255,6 @@ class LossAPIController extends APIController implements LossAPIControllerInterf
     $repository = $this->getDoctrine()->getRepository(Constant::DECLARE_LOSS_RESPONSE_REPOSITORY);
     $declareLosses = $repository->getLossesWithLastHistoryResponses($client);
 
-    return new JsonResponse(array(Constant::RESULT_NAMESPACE => array($declareLosses, 200)),200);
+    return new JsonResponse(array(Constant::RESULT_NAMESPACE => $declareLosses),200);
   }
 }
