267f3b4ecaba6c62e16ec9e3e20bf1e5265f09a8
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-05-25 12:29:13 +0200

    Create Mocked DeclareDepart request and response and persist to database

--- src/AppBundle/DataFixtures/ORM/MockedDeclareDepart.php
+++ src/AppBundle/DataFixtures/ORM/MockedDeclareDepart.php
@@ -4,14 +4,8 @@ namespace AppBundle\DataFixtures\ORM;
 
 use AppBundle\Constant\Constant;
 use AppBundle\Entity\Client;
-use AppBundle\Entity\DeclareArrival;
-use AppBundle\Entity\LocationAddress;
-use AppBundle\Entity\BillingAddress;
-use AppBundle\Entity\CompanyAddress;
-use AppBundle\Entity\Company;
-use AppBundle\Entity\Location;
-use AppBundle\Enumerator\RequestStateType;
-use AppBundle\JsonFormat\DeclareArrivalJsonFormat;
+use AppBundle\Entity\DeclareDepart;
+use AppBundle\Entity\DeclareDepartResponse;
 use AppBundle\Service\IRSerializer;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
@@ -21,12 +15,11 @@ use Liip\FunctionalTestBundle\Test\WebTestCase;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Client as RequestClient;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
-class MockedDeclareArrival implements FixtureInterface, ContainerAwareInterface, OrderedFixtureInterface
+class MockedDeclareDepart implements FixtureInterface, ContainerAwareInterface, OrderedFixtureInterface
 {
-    const DECLARE_ARRIVAL_ENDPOINT = "/api/v1/arrivals";
-
-    static public $hasCascadePersistenceIssueBeenFixed = false;
+    const DECLARE_DEPART_ENDPOINT = "/api/v1/departs";
 
     /**
      * @var ContainerInterface
@@ -49,9 +42,19 @@ class MockedDeclareArrival implements FixtureInterface, ContainerAwareInterface,
     static private $entityManager;
 
     /**
-     * @var DeclareArrival
+     * @var DeclareDepart
      */
-    static private $mockedArrival;
+    static private $mockedDepart;
+
+    /**
+     * @var DeclareDepartResponse
+     */
+    static private $mockedDepartSuccessResponse;
+
+    /**
+     * @var DeclareDepartResponse
+     */
+    static private $mockedDepartFailedResponse;
 
     /**
      * @var array
@@ -65,73 +68,105 @@ class MockedDeclareArrival implements FixtureInterface, ContainerAwareInterface,
 
     public function load(ObjectManager $manager)
     {
-        if(self::$hasCascadePersistenceIssueBeenFixed) {
-            //Create mocked data
+        //Get service classes
+        self::$serializer = $this->container->get('app.serializer.ir');
+        self::$entityManager = $this->container->get('doctrine.orm.entity_manager');
+        $departRepository = self::$entityManager->getRepository(Constant::DECLARE_DEPART_REPOSITORY);
 
-            //DeclareArrival
-            self::$mockedArrival = new DeclareArrival();
+        //Create client
+        $this->client = new RequestClient($this->container->get('kernel'));
 
-            $animal = MockedAnimal::getMockedAnotherEwe();
-            $client = MockedClient::getMockedClient();
-            $location = MockedClient::getMockedLocation();
-            $id = uniqid(mt_rand(0,999999));
-            $relationNumberKeeper = $client->getRelationNumberKeeper();
+        $this->defaultHeaders = array(
+            'CONTENT_TYPE' => 'application/json',
+            'HTTP_ACCESSTOKEN' => MockedClient::getMockedClient()->getAccessToken(),
+        );
 
-            self::$mockedArrival->setAnimal($animal);
-            self::$mockedArrival->setArrivalDate(new \DateTime('2023-03-20'));
-            self::$mockedArrival->setUbnPreviousOwner("48624");
-            self::$mockedArrival->setIsImportAnimal(false);
-            self::$mockedArrival->setLocation($location);
-            self::$mockedArrival->setLogDate(new \DateTime('2023-03-22'));
-            self::$mockedArrival->setRequestId($id);
-            self::$mockedArrival->setMessageId($id);
-            self::$mockedArrival->setRequestState(RequestStateType::FINISHED);
-            self::$mockedArrival->setAction('C');
-            self::$mockedArrival->setRecoveryIndicator('N');
-            self::$mockedArrival->setRelationNumberKeeper($relationNumberKeeper);
+        $ram = MockedAnimal::getMockedParentRam();
 
-            //Persist mocked data
-            $manager->persist(self::$mockedArrival);
-            $manager->flush();
+        //Create declare depart
+        $declareDepartArray = array(
+            "reason_of_depart" => "a very good reason",
+            "ubn_new_owner" => "123456",
+            "is_export_animal"  =>  false,
+            "animal" => array(
+                "uln_country_code" => $ram->getUlnCountryCode(),
+                "uln_number" => $ram->getUlnNumber(),
+                "pedigree_country_code" => $ram->getPedigreeCountryCode(),
+                "pedigree_number" => $ram->getPedigreeNumber()
+            ),
+            "depart_date" => "2012-04-21T18:25:43-05:00"
+        );
 
-        } else {
+        $declareDepartJson = json_encode($declareDepartArray);
 
-            //Get service classes
-            self::$serializer = $this->container->get('app.serializer.ir');
-            self::$entityManager = $this->container->get('doctrine.orm.entity_manager');
+        $this->client->request('POST',
+            $this::DECLARE_DEPART_ENDPOINT,
+            array(),
+            array(),
+            $this->defaultHeaders,
+            $declareDepartJson
+        );
 
-            //Create client
-            $this->client = new RequestClient($this->container->get('kernel'));
+        $response = $this->client->getResponse()->getContent();
+        $declareDepartArray = json_decode($response, true);
 
-            $this->defaultHeaders = array(
-                'CONTENT_TYPE' => 'application/json',
-                'HTTP_ACCESSTOKEN' => MockedClient::getMockedClient()->getAccessToken(),
-            );
+        self::$mockedDepart = $departRepository->findAll()['0'];
 
-            //Create declare arrival
-            $declareArrivalJsonFormat = new DeclareArrivalJsonFormat();
-            $declareArrivalJsonFormat->setArrivalDate(new \DateTime());
-            $declareArrivalJsonFormat->setUbnPreviousOwner("123456");
-            $declareArrivalJsonFormat->setIsImportAnimal(false);
-            $declareArrivalJsonFormat->setAnimal(MockedAnimal::getMockedRamWithParents());
+        //Create Responses
+        $messageNumberFailedResponse = uniqid(mt_rand(0,99));
+        $messageNumberSuccessResponse = uniqid(mt_rand(0,99));
 
-            //Create json to be posted
-            $declareArrivalJson = self::$serializer->serializeToJSON($declareArrivalJsonFormat);
+        //It is necessary to retrieve the DeclareDepart entity from the Repository first
+        $declareDepart = $departRepository->findAll()['0'];
 
-            $this->client->request('POST',
-                $this::DECLARE_ARRIVAL_ENDPOINT,
-                array(),
-                array(),
-                $this->defaultHeaders,
-                $declareArrivalJson
-            );
+        //Create and persist failed Response
+        self::$mockedDepartFailedResponse = new DeclareDepartResponse();
+        self::$mockedDepartFailedResponse->setDeclareDepartRequestMessage($declareDepart);
+        self::$mockedDepartFailedResponse->setLogDate(new \DateTime('2023-03-21'));
+        self::$mockedDepartFailedResponse->setRequestId($declareDepart->getRequestId());
+        self::$mockedDepartFailedResponse->setMessageId($declareDepart->getMessageId());
+        self::$mockedDepartFailedResponse->setMessageNumber($messageNumberFailedResponse);
+        self::$mockedDepartFailedResponse->setDepartDate($declareDepart->getDepartDate());
+        self::$mockedDepartFailedResponse->setUbnNewOwner($declareDepart->getUbnNewOwner());
+        self::$mockedDepartFailedResponse->setErrorCode("IRD-00363");
+        self::$mockedDepartFailedResponse->setErrorMessage("Er zijn geen dieren gevonden bij het opgegeven werknummer");
+        self::$mockedDepartFailedResponse->setErrorKindIndicator("F");
+        self::$mockedDepartFailedResponse->setSuccessIndicator("N");
+        //Worker logic
+        self::$mockedDepartFailedResponse->setUlnCountryCode($declareDepart->getUlnCountryCode());
+        self::$mockedDepartFailedResponse->setUlnNumber($declareDepart->getUlnNumber());
+        self::$mockedDepartFailedResponse->setPedigreeCountryCode($declareDepart->getPedigreeCountryCode());
+        self::$mockedDepartFailedResponse->setPedigreeNumber($declareDepart->getPedigreeNumber());
+        //status changes
+        self::$mockedDepartFailedResponse->setIsExportAnimal(false);
+        self::$mockedDepartFailedResponse->setIsRemovedByUser(false);
 
-            $response = $this->client->getResponse()->getContent();
-            $declareArrivalArray = json_decode($response, true);
+        //Create and persist successful Response
+        self::$mockedDepartSuccessResponse = new DeclareDepartResponse();
+        self::$mockedDepartSuccessResponse->setDeclareDepartRequestMessage($declareDepart);
+        self::$mockedDepartSuccessResponse->setLogDate(new \DateTime('2023-03-22'));
+        self::$mockedDepartSuccessResponse->setRequestId($declareDepart->getRequestId());
+        self::$mockedDepartSuccessResponse->setMessageId($declareDepart->getMessageId());
+        self::$mockedDepartSuccessResponse->setMessageNumber($messageNumberSuccessResponse);
+        self::$mockedDepartSuccessResponse->setDepartDate($declareDepart->getDepartDate());
+        self::$mockedDepartSuccessResponse->setUbnNewOwner($declareDepart->getUbnNewOwner());
+        self::$mockedDepartSuccessResponse->setErrorCode(null);
+        self::$mockedDepartSuccessResponse->setErrorMessage(null);
+        self::$mockedDepartSuccessResponse->setErrorKindIndicator(null);
+        self::$mockedDepartSuccessResponse->setSuccessIndicator("J");
+        //Worker logic
+        self::$mockedDepartSuccessResponse->setUlnCountryCode($declareDepart->getUlnCountryCode());
+        self::$mockedDepartSuccessResponse->setUlnNumber($declareDepart->getUlnNumber());
+        self::$mockedDepartSuccessResponse->setPedigreeCountryCode($declareDepart->getPedigreeCountryCode());
+        self::$mockedDepartSuccessResponse->setPedigreeNumber($declareDepart->getPedigreeNumber());
+        //status changes
+        self::$mockedDepartSuccessResponse->setIsExportAnimal(true);
+        self::$mockedDepartSuccessResponse->setIsRemovedByUser(false);
 
-            self::$mockedArrival = self::$entityManager->getRepository(Constant::DECLARE_ARRIVAL_REPOSITORY)->findAll()['0'];
-
-        }
+        //Persist mocked data
+        $manager->persist(self::$mockedDepartSuccessResponse);
+        $manager->persist(self::$mockedDepartFailedResponse);
+        $manager->flush();
     }
 
     /**
@@ -146,12 +181,27 @@ class MockedDeclareArrival implements FixtureInterface, ContainerAwareInterface,
     }
 
     /**
-     * @return DeclareArrival
+     * @return DeclareDepart
      */
-    public static function getMockedArrival()
+    public static function getMockedDepart()
     {
-        return self::$mockedArrival;
+        return self::$mockedDepart;
     }
 
+    /**
+     * @return DeclareDepartResponse
+     */
+    public static function getMockedDepartSuccessResponse()
+    {
+        return self::$mockedDepartSuccessResponse;
+    }
+
+    /**
+     * @return DeclareDepartResponse
+     */
+    public static function getMockedDepartFailedResponse()
+    {
+        return self::$mockedDepartFailedResponse;
+    }
 
 }
