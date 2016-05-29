dd57828a582f64db34f20f3b0c13dd2170bbc345
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-05-29 06:17:47 +0200

    Add Unknown to HealthStatus enumerator

--- src/AppBundle/Enumerator/HealthStatus.php
+++ src/AppBundle/Enumerator/HealthStatus.php
@@ -10,4 +10,5 @@ class HealthStatus
     const HEALTHY_LEVEL_1 = 'HEALTHY_LEVEL_1';
     const HEALTHY_LEVEL_2 = 'HEALTHY_LEVEL_2';
     const HEALTHY_LEVEL_3 = 'HEALTHY_LEVEL_3';
+    const UNKNOWN = 'UNKNOWN';
 }
