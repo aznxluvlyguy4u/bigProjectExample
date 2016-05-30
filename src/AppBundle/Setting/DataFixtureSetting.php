82915c2493dc5b3cf5b008e15b38f3defba79b62
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-05-30 19:17:43 +0200

    Activate all datafixtures by default

--- src/AppBundle/Setting/DataFixtureSetting.php
+++ src/AppBundle/Setting/DataFixtureSetting.php
@@ -8,8 +8,8 @@ class DataFixtureSetting
     const USE_MOCKED_CLIENT = true;
     const USE_MOCKED_EMPLOYEE = true;
     const USE_MOCKED_CLIENT_TWO = true;
-    const USE_MOCKED_TAGS = false;
-    const USE_MOCKED_ANIMALS = false;
-    const USE_MOCKED_COUNTRIES = false;
-    const USE_MOCKED_REQUESTS_AND_RESPONSES = false;
+    const USE_MOCKED_TAGS = true;
+    const USE_MOCKED_ANIMALS = true;
+    const USE_MOCKED_COUNTRIES = true;
+    const USE_MOCKED_REQUESTS_AND_RESPONSES = true;
 }
