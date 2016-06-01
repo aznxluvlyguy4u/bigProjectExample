e86951ffca8c50bd74d6db429dd187fa1dddcdc5
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-06-01 12:15:57 +0200

    Reactivate the MockedCountries because they are real values to be use in the dropdown menu in the frontend

--- src/AppBundle/Setting/DataFixtureSetting.php
+++ src/AppBundle/Setting/DataFixtureSetting.php
@@ -6,10 +6,10 @@ namespace AppBundle\Setting;
 class DataFixtureSetting
 {
     const USE_MOCKED_CLIENT = true;
+    const USE_MOCKED_COUNTRIES = true;
     const USE_MOCKED_EMPLOYEE = false;
     const USE_MOCKED_CLIENT_TWO = false;
     const USE_MOCKED_TAGS = false;
     const USE_MOCKED_ANIMALS = false;
-    const USE_MOCKED_COUNTRIES = false;
     const USE_MOCKED_REQUESTS_AND_RESPONSES = false;
 }
