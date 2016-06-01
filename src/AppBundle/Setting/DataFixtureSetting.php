fc135d3a21d80bfa315d9e3a8ba00b994bda17dc
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-06-01 10:57:32 +0200

    Deactivate DataFixtures by default

--- src/AppBundle/Setting/DataFixtureSetting.php
+++ src/AppBundle/Setting/DataFixtureSetting.php
@@ -6,10 +6,10 @@ namespace AppBundle\Setting;
 class DataFixtureSetting
 {
     const USE_MOCKED_CLIENT = true;
-    const USE_MOCKED_EMPLOYEE = true;
-    const USE_MOCKED_CLIENT_TWO = true;
-    const USE_MOCKED_TAGS = true;
-    const USE_MOCKED_ANIMALS = true;
-    const USE_MOCKED_COUNTRIES = true;
-    const USE_MOCKED_REQUESTS_AND_RESPONSES = true;
+    const USE_MOCKED_EMPLOYEE = false;
+    const USE_MOCKED_CLIENT_TWO = false;
+    const USE_MOCKED_TAGS = false;
+    const USE_MOCKED_ANIMALS = false;
+    const USE_MOCKED_COUNTRIES = false;
+    const USE_MOCKED_REQUESTS_AND_RESPONSES = false;
 }
