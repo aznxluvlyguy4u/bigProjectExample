6736dec5fe7e650601a2e716ad5be4eb80c6e682
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-06-08 14:42:25 +0200

    Create MigrationStatus enumerator

---
+++ src/AppBundle/Enumerator/MigrationStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\Enumerator;
+
+
+class MigrationStatus
+{
+    const MIGRATED = 'MIGRATED';
+    const NEW_PASSWORD = 'NEW_PASSWORD';
+    const PASSWORD_UPDATED = 'PASSWORD_UPDATED';
+}
