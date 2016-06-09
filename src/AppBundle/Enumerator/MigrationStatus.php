76303f542c0912ce30ef0c5a69ccfa0e4bc698d8
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-06-09 11:28:41 +0200

    Add EMAIL_SENT to MigrationStatus enumerator

--- src/AppBundle/Enumerator/MigrationStatus.php
+++ src/AppBundle/Enumerator/MigrationStatus.php
@@ -7,5 +7,6 @@ class MigrationStatus
 {
     const MIGRATED = 'MIGRATED';
     const NEW_PASSWORD = 'NEW_PASSWORD';
+    const EMAIL_SENT = 'EMAIL_SENT';
     const PASSWORD_UPDATED = 'PASSWORD_UPDATED';
 }
