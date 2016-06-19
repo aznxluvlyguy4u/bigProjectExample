f1d96267826150bd761b8f5cde2f78cfcae58583
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-06-19 19:43:22 +0200

    Create function to return all LocationHealths created after a given id

--- src/AppBundle/Entity/LocationHealthRepository.php
+++ src/AppBundle/Entity/LocationHealthRepository.php
@@ -8,4 +8,30 @@ namespace AppBundle\Entity;
  */
 class LocationHealthRepository extends BaseRepository {
 
+    /**
+     * @param int $id
+     * @return array
+     */
+    public function getAllAfterId($locationHealthId, $location)
+    {
+        //TODO MORE OPTIMIZATION NEEDED
+
+        $locationId = $location->getId();
+
+        $sql = "SELECT id FROM location_health
+                WHERE (id > '"  . $locationHealthId . "' AND  location_id = '"  . $locationId . "')";
+
+        $query = $this->getEntityManager()->getConnection()->prepare($sql);
+        $query->execute();
+
+        $results = array();
+
+        //FIXME HOW TO RETURN OBJECTS DIRECTLY WITHOUT NEEDING find(id)
+        foreach($query->fetchAll() as $item) {
+            $results[] = $this->find($item['id']);
+        }
+
+        return $results;
+    }
+
 }
