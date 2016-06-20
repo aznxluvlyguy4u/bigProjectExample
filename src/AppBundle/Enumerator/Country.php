4b49b00e8da4f526640b149daaad4e1853170a44
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-06-20 18:33:29 +0200

    Add Country NL enumerator

--- src/AppBundle/Enumerator/Country.php
+++ src/AppBundle/Enumerator/Country.php
@@ -1,15 +1,9 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: radiancekfx
- * Date: 20-6-16
- * Time: 17:09
- */
 
 namespace AppBundle\Enumerator;
 
 
 class Country
 {
-
+    const NL = 'NL';
 }
