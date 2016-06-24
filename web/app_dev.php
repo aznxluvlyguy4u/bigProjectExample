83eb63c31b7a23e7ec8c4a33550f1fa3f6dd3225
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-06-24 10:47:44 +0200

    Allow remote connections in dev environment, so the frontend on another machine can connect to local running API

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,14 +8,26 @@ use Symfony\Component\Debug\Debug;
 // for more information
 //umask(0000);
 
+$isDevelopmentEnvironment = false;
+if($kernel->getEnvironment() != null) {
+    if($kernel->getEnvironment() == 'dev') {
+        $isDevelopmentEnvironment = true;
+    }
+}
+
+//Deactivate the default code block below, so it doesn't interfere with testing the frontend using ngrok
+if(!$isDevelopmentEnvironment) {
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1']) || php_sapi_name() === 'cli-server')
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    if (isset($_SERVER['HTTP_CLIENT_IP'])
+        || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+        || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1']) || php_sapi_name() === 'cli-server')
+    ) {
+        +
+        header('HTTP/1.0 403 Forbidden');
+        exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
+
+    }
 }
 
 /**
