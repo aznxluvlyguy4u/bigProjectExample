ad2846d5b57fbb209ad3949e337faaa2638465a8
Steven <steven@jongensvantechniek.nl>
2016-07-11 08:30:16 +0200

    Setup base class for Measurement

---
+++ src/AppBundle/Entity/Measurement.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * Class Measurement
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\MeasurementRepository")
+ * @ORM\InheritanceType("JOINED")
+ * @ORM\DiscriminatorColumn(name="type", type="string")
+ * @ORM\DiscriminatorMap({"BodyFat" = "BodyFat", "MuscleThickness" = "MuscleThickness", "TailLength" = "TailLength", "Weight" = "Weight"})
+ * @package AppBundle\Entity
+ */
+abstract class Measurement {
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime")
+     * @Assert\Date
+     * @Assert\NotBlank
+     * @JMS\Type("DateTime")
+     */
+    protected $logDate;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime")
+     * @Assert\Date
+     * @Assert\NotBlank
+     * @JMS\Type("DateTime")
+     */
+    protected $measurementDate;
+
+
+  
+   /**
+    * Measurement constructor.
+    */
+    public function __construct() {
+      $this->logDate = new \DateTime();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set logDate
+     *
+     * @param \DateTime $logDate
+     *
+     * @return Measurement
+     */
+    public function setLogDate($logDate)
+    {
+        $this->logDate = $logDate;
+
+        return $this;
+    }
+
+    /**
+     * Get logDate
+     *
+     * @return \DateTime
+     */
+    public function getLogDate()
+    {
+        return $this->logDate;
+    }
+
+    /**
+     * Set measurementDate
+     *
+     * @param \DateTime $measurementDate
+     *
+     * @return Measurement
+     */
+    public function setMeasurementDate($measurementDate)
+    {
+        $this->measurementDate = $measurementDate;
+
+        return $this;
+    }
+
+    /**
+     * Get measurementDate
+     *
+     * @return \DateTime
+     */
+    public function getMeasurementDate()
+    {
+        return $this->measurementDate;
+    }
+
+    /**
+     * Set animal
+     *
+     * @param \AppBundle\Entity\Animal $animal
+     *
+     * @return Measurement
+     */
+    public function setAnimal(\AppBundle\Entity\Animal $animal = null)
+    {
+        $this->animal = $animal;
+
+        return $this;
+    }
+
+    /**
+     * Get animal
+     *
+     * @return \AppBundle\Entity\Animal
+     */
+    public function getAnimal()
+    {
+        return $this->animal;
+    }
+}
