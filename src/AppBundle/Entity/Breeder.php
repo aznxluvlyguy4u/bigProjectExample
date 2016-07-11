5f76d546832677b72bb5dba96cb1c397e835b57a
Steven <steven@jongensvantechniek.nl>
2016-07-11 17:43:35 +0200

    Setup Breeder entity

---
+++ src/AppBundle/Entity/Breeder.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: c0d3
+ * Date: 11/07/16
+ * Time: 16:07
+ */
+
+namespace AppBundle\Entity;
+
+
+class Breeder {
+
+}
---
+++ src/AppBundle/Entity/BreederRepository.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: c0d3
+ * Date: 11/07/16
+ * Time: 16:09
+ */
+
+namespace AppBundle\Entity;
+
+
+class BreederRepository {
+
+}
