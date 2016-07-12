a2c4a0ab9394699c590a15281ca75c9ddd8d1e7c
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-07-12 10:32:04 +0200

    Setup skeleton for Pedigree certificate report controller endpoint and implement input validation

---
+++ src/AppBundle/Controller/ReportAPIController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Validation\UlnValidator;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\EntityManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Component\HttpFoundation\JsonResponse;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+/**
+ * @Route("/api/v1/reports")
+ */
+class ReportAPIController extends APIController {
+
+  /**
+   * Generate pedigree certificates for multiple sheep and return a download link for the pdf.
+   *
+   * @ApiDoc(
+   *   requirements={
+   *     {
+   *       "name"="AccessToken",
+   *       "dataType"="string",
+   *       "requirement"="",
+   *       "description"="A valid accesstoken belonging to the user that is registered with the API"
+   *     }
+   *   },
+   *   resource = true,
+   *   description = "Generate pedigree certificates for multiple sheep and return a download link for the pdf",
+   *   output = "AppBundle\Entity\Animal"
+   * )
+   * @param Request $request the request object
+   * @return JsonResponse
+   * @Route("/{Id}")
+   * @ParamConverter("Id", class="AppBundle\Entity\AnimalRepository")
+   * @Method("POST")
+   */
+  public function getPedigreeCertificates(Request $request) {
+    $client = $this->getAuthenticatedUser($request);
+    $content = $this->getContentAsArray($request);
+    $em = $this->getDoctrine()->getEntityManager();
+      
+    $ulnValidator = new UlnValidator($em, $content, true, $client);
+    if(!$ulnValidator->getIsUlnSetValid()) {
+      return $ulnValidator->createArrivalJsonErrorResponse();
+    }
+
+    $result = 'success!';
+
+    //TODO Retrieve the Animals and bloodline/pedigree
+
+    //TODO Generate pdf document from twig view
+    //https://github.com/KnpLabs/KnpSnappyBundle#generate-a-pdf-document-from-a-twig-view
+//    $this->get('knp_snappy.pdf')->generateFromHtml(
+//        $this->renderView(
+//            'MyBundle:Foo:bar.html.twig',
+//            array(
+//                'some'  => $vars
+//            )
+//        ),
+//        '/path/to/the/file.pdf'
+//    );
+
+    return new JsonResponse($result, 200);
+  }
+
+}
---
+++ src/AppBundle/Validation/UlnValidator.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace AppBundle\Validation;
+
+
+use AppBundle\Component\Utils;
+use AppBundle\Constant\Constant;
+use AppBundle\Entity\Animal;
+use AppBundle\Entity\Client;
+use AppBundle\Entity\Location;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\EntityManager;
+use Monolog\Handler\Curl\Util;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class UlnValidator
+{
+    const ERROR_CODE = 428;
+    const ERROR_MESSAGE = 'ANIMAL IS NOT REGISTERED WITH GIVEN CLIENT';
+    const VALID_CODE = 200;
+    const VALID_MESSAGE = 'ULN OF ANIMAL IS VALID';
+    const MISSING_INPUT_CODE = 428;
+    const MISSING_INPUT_MESSAGE = 'NO ULN GIVEN';
+    const MISSING_ANIMAL_CODE = 428;
+    const MISSING_ANIMAL_MESSAGE = 'ANIMAL IS NOT REGISTERED WITH NSFO';
+
+    /** @var boolean */
+    private $isUlnSetValid;
+
+    /** @var boolean */
+    private $isInputMissing;
+
+    /** @var boolean */
+    private $isInDatabase;
+
+    /** @var string */
+    private $ulnCountryCode;
+
+    /** @var string */
+    private $ulnNumber;
+
+    /** @var ArrayCollection */
+    private $locations;
+
+    /** @var EntityManager */
+    private $manager;
+
+    /**
+     * UbnValidator constructor.
+     * @param EntityManager $manager
+     * @param Client $client
+     * @param Collection $content
+     * @param boolean $multipleAnimals
+     */
+    public function __construct(EntityManager $manager, Collection $content, $multipleAnimals = false, Client $client = null)
+    {
+        $this->manager = $manager;
+
+        if($client != null) {
+            $this->locations = $manager->getRepository(Location::class)->findAllLocationsOfClient($client);
+        }
+
+        $animalArray = null;
+        $this->isInputMissing = true;
+        $this->isUlnSetValid = false;
+        $this->isInDatabase = true;
+
+        if($multipleAnimals == false) {
+
+            $animalArray = null;
+            foreach ($content->getKeys() as $key) {
+                if($key == Constant::ANIMAL_NAMESPACE) {
+                    $animalArray = $content->get($key);
+
+                    $this->isInputMissing = false;
+                    $this->isUlnSetValid = true;
+
+                    $this->isUlnSetValid = $this->validateUlnInput($animalArray, $client);
+                }
+            }
+
+        } else {
+
+            $animalArrays = null;
+            foreach ($content->getKeys() as $key) {
+                if($key == Constant::ANIMALS_NAMESPACE) {
+                    $animalArrays = $content->get($key);
+
+                    $this->isInputMissing = false;
+                    $this->isUlnSetValid = true;
+
+                    foreach ($animalArrays as $animalArray) {
+                        $isUlnValid = $this->validateUlnInput($animalArray, $client);
+
+                        if(!$isUlnValid) {
+                            $this->isUlnSetValid = false;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIsUlnSetValid() {
+        return $this->isUlnSetValid;
+    }
+
+    /**
+     * @param array $animalArray
+     * @param Client $client
+     * @return boolean
+     */
+    private function validateUlnInput($animalArray, $client)
+    {
+        $ulnExists = array_key_exists(Constant::ULN_NUMBER_NAMESPACE, $animalArray) &&
+            array_key_exists(Constant::ULN_NUMBER_NAMESPACE, $animalArray);
+
+        if ($ulnExists) {
+            $numberToCheck = $animalArray[Constant::ULN_NUMBER_NAMESPACE];
+            $countryCodeToCheck = $animalArray[Constant::ULN_COUNTRY_CODE_NAMESPACE];
+            $this->ulnCountryCode = $countryCodeToCheck;
+            $this->ulnNumber = $numberToCheck;
+        } else {
+            $this->isInputMissing = true;
+            return false;
+        }
+
+        $criteria = Criteria::create()
+            ->where(Criteria::expr()->eq('ulnCountryCode', $countryCodeToCheck))
+            ->andWhere(Criteria::expr()->eq('ulnNumber', $numberToCheck));
+
+        $animal = $this->manager->getRepository(Animal::class)
+            ->matching($criteria)->first();
+
+        //First verify if animal actually exists
+        if($animal == null) {
+            $this->isInDatabase = false;
+            return false;
+        }
+
+        if($client == null) { //Get any animals regardless of client
+            return true;
+
+        } else { //Get only animals owned by client
+
+            foreach ($this->locations as $location) {
+                if($animal->getLocation() == $location) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    /**
+     * Only create this JsonResponse when there actually are errors.
+     *
+     * @return JsonResponse
+     */
+    public function createArrivalJsonErrorResponse()
+    {
+        $uln = null;
+
+        if($this->isUlnSetValid) {
+            $code = self::VALID_CODE;
+            $message = self::VALID_MESSAGE;
+        } else {
+            $code = self::ERROR_CODE;
+            $message = self::ERROR_MESSAGE;
+        }
+
+        //Only return the values for the identification type being tested
+        if (!$this->isInputMissing && $this->isInDatabase) {
+            $uln = $this->ulnCountryCode . $this->ulnNumber;
+        } else if (!$this->isInDatabase) {
+            $code = self::MISSING_ANIMAL_CODE;
+            $message = self::MISSING_ANIMAL_MESSAGE;
+            $uln = $this->ulnCountryCode . $this->ulnNumber;
+        } else { //If no ULN or PEDIGREE found
+            $code = self::MISSING_INPUT_CODE;
+            $message = self::MISSING_INPUT_MESSAGE;
+        }
+
+        $result = array(
+            Constant::CODE_NAMESPACE => $code,
+            Constant::MESSAGE_NAMESPACE => $message,
+            Constant::ULN_NAMESPACE => $uln,
+            );
+
+        return new JsonResponse($result, $code);
+    }
+
+}
