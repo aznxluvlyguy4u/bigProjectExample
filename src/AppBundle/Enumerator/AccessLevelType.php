5a8b39aaabed54dab62666a8ebf67d9498dab566
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-07-12 17:35:08 +0200

    Create Admin AccessLevel enumerator

---
+++ src/AppBundle/Enumerator/AccessLevelType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Enumerator;
+
+/**
+ * Class ActionType
+ * @package AppBundle\Enumerator
+ */
+class AccessLevelType
+{
+    const DEVELOPER = 'DEVELOPER';
+    const SUPER_ADMIN = 'SUPER_ADMIN';
+    const ADMIN = 'ADMIN';
+}
