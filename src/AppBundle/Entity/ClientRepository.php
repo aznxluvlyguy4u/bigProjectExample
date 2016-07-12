43faff71111889d6ae0f182e7bfa9c01601f6475
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-07-12 19:23:03 +0200

    Delete outdated and unused functions that don't work anymore with the new AccessToken array structure

--- src/AppBundle/Entity/ClientRepository.php
+++ src/AppBundle/Entity/ClientRepository.php
@@ -7,23 +7,6 @@ use AppBundle\Setting\MigrationSetting;
 
 class ClientRepository extends BaseRepository {
 
-
-    public function getByToken($token)
-    {
-
-        $em = $this->getEntityManager();
-        $client = $em->getRepository(Constant::CLIENT_REPOSITORY)->findOneBy(array('accessToken' => $token));
-
-        return $client;
-    }
-
-    public function getRelationNumberKeeper($token)
-    {
-        $client = $this->getByToken($token);
-
-        return $client->getRelationNumberKeeper();
-    }
-
     public function getByRelationNumberKeeper($relationNumberKeeper)
     {
         $repository = $this->getEntityManager()->getRepository(Constant::CLIENT_REPOSITORY);
