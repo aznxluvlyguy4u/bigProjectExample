66d1a00171095a62b203811b1bfee16426f2e0fb
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-07-13 10:05:02 +0200

    Create ghost_token constant

--- src/AppBundle/Constant/Constant.php
+++ src/AppBundle/Constant/Constant.php
@@ -54,6 +54,7 @@ class Constant
     const CODE_NAMESPACE = 'code';
     const ERRORS_NAMESPACE = 'errors';
     const TYPE_NAMESPACE = 'type';
+    const GHOST_TOKEN_NAMESPACE = 'ghost_token';
 
     //Animal identification
     const ULN_NAMESPACE = 'uln';
