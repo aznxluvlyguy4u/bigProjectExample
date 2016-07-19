016d45030f46bb2740a23871749e16dd74972b46
Steven <steven@jongensvantechniek.nl>
2016-07-19 13:50:58 +0200

    Update AWS queueService to reflect addition of staging QueueId

--- src/AppBundle/Service/AWSQueueService.php
+++ src/AppBundle/Service/AWSQueueService.php
@@ -72,21 +72,26 @@ class AWSQueueService
      * Get current environment, set queueId based on environment.
      *
      * 0 = (prod)uction
-     * 1 = (dev)elopment
-     * 2 = test
+     * 1 = (stage)ing
+     * 2 = (dev)elopment
+     * 3 = test
+     * 4 = local
      */
     switch($currentEnvironment) {
       case 'prod':
-        $queueId = $queueIds[0];
-        break;
-      case 'dev':
         $queueId = $queueIds[1];
         break;
-      case 'test':
+      case 'stage':
+        $queueId = $queueIds[1];
+        break;
+      case 'dev':
         $queueId = $queueIds[2];
         break;
-      case 'local':
+      case 'test':
         $queueId = $queueIds[3];
+        break;
+      case 'local':
+        $queueId = $queueIds[4];
         break;
       default; //dev
         $queueId = $queueIds[1];
