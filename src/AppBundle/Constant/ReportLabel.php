b272dc7dd9a1d09e349cc6a3a483221424ef7d04
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-07-25 07:21:48 +0200

    Update report constants

--- src/AppBundle/Constant/ReportLabel.php
+++ src/AppBundle/Constant/ReportLabel.php
@@ -6,72 +6,74 @@ namespace AppBundle\Constant;
 class ReportLabel
 {
     //General
-    const DATE = 'date';
     const ENTITY = 'entity';
 
     //Person
-    const FIRST_NAME = "first_name";
-    const LAST_NAME = "last_name";
-    const OWNER_NAME = "owner_name";
+    const OWNER_NAME = "ownerName";
     const OWNER = 'owner';
 
     //Breeder
     const BREEDER = 'breeder';
-    const BREEDER_NAME = "breeder_name";
-    const BREEDER_NAME_CROPPED = "breeder_name_cropped";
-    const BREEDER_NUMBER = 'breeder_number';
+    const BREEDER_NAME = "breederName";
+    const BREEDER_NAME_CROPPED = "breederNameCropped";
+    const BREEDER_NUMBER = 'breederNumber';
 
     //Address
     const ADDRESS = 'address';
-    const ADDRESS_BREEDER = 'address_breeder';
+    const ADDRESS_BREEDER = 'addressBreeder';
     const UBN = 'ubn';
 
     //Animal
     const ANIMALS = 'animals';
     const GENDER = 'gender';
-    const ULN_COUNTRY_CODE = 'uln_country_code';
-    const ULN_NUMBER = 'uln_number';
-    const ULN_CODE_FULL = 'uln_code_full';
-    const PEDIGREE_COUNTRY_CODE = 'pedrigree_country_code';
-    const PEDIGREE_NUMBER = 'pedrigree_number';
-    const PEDIGREE_CODE_FULL = 'pedrigree_code_full';
-    const DATE_OF_BIRTH = 'date_of_birth';
-    const LITTER_SIZE = 'litter_size';
-    const LITTER_GROUP = 'litter_group';
-    const SCRAPIE_GENE = 'scrapie_gene';
-    const PRODUCTION_CODE = 'production_code';
-    const BREED_STATUS = 'breed_status';
-    const INSPECTION_DATE = 'inspection_date';
+    const ULN_COUNTRY_CODE = 'ulnCountryCode';
+    const ULN_NUMBER = 'ulnNumber';
+    const ULN = 'uln';
+    const PEDIGREE_COUNTRY_CODE = 'pedigreeCountryCode';
+    const PEDIGREE_NUMBER = 'pedigreeNumber';
+    const PEDIGREE = 'pedigree';
+    const NAME = 'name';
+    const DATE_OF_BIRTH = 'dateOfBirth';
+    const LITTER_SIZE = 'litterSize'; //litter animal was born in
+    const LITTER_GROUP = 'litterGroup'; //litter animal was born in
+    const LITTER_COUNT = 'litterCount'; //number of offspring litters
+    const OFFSPRING_COUNT = 'offspringCount'; //number of offspring
+    const N_LING = 'nLing';
+    const SCRAPIE = 'scrapie';
+    const PRODUCTION = 'production';
+    const BREED_TYPE = 'breedType';
+    const BREED_CODE = 'breedCode';
+    const INSPECTION_DATE = 'inspectionDate';
     const PREDICATE = 'predicate';
-    const LATEST_EXTERIOR = 'latest_exterior';
+    const BLINDNESS_FACTOR = 'blindnessFactor';
+
+    //Measurements
+    const EXTERIOR = 'exterior';
     const MEASUREMENT_DATE = 'measurement_date';
-    const LATEST_MUSCLE_THICKNESS = 'latest_muscle_thickness';
-    const LATEST_BODY_FAT = 'latest_body_fat';
-    const LATEST_TAIL_LENGTH = 'latest_tail_length';
+    const SKULL = 'skull';
+    const DEVELOPMENT = 'development';
+    const MUSCULARITY = 'muscularity';
+    const PROPORTION = 'proportion';
+    const TYPE = 'type';
+    const LEGWORK = 'legWork';
+    const FUR = 'fur';
+    const GENERAL_APPEARANCE = 'generalAppearance';
+    const HEIGHT = 'height';
+    const TORSO_LENGTH = 'torsoLength';
+    const BREAST_DEPTH = 'breastDepth';
+    const GROWTH = 'growth';
+    const VL = 'vl';
+    const SL = 'sl';
+    const MARKINGS = 'markings';
+
+
+    const MUSCLE_THICKNESS = 'muscleThickness';
+    const BODY_FAT = 'bodyFat';
+    const TAIL_LENGTH = 'tailLength';
 
     //Pedrigree / Bloodline
-    const GENERATION = 'generation';
-    const CHILD = 'child';
-    const FATHER = 'father';
-    const MOTHER = 'mother';
-    const _S_FATHER = '_s_father';
-    const _S_MOTHER = '_s_mother';
+    const CHILD_KEY = 'c';
+    const FATHER_KEY = 'f';
+    const MOTHER_KEY = 'm';
     
-    const FATHER_S_FATHER = 'father_s_father';
-    const FATHER_S_FATHER_S_FATHER = 'father_s_father_s_father';
-    const FATHER_S_FATHER_S_MOTHER = 'father_s_father_s_mother';
-
-    const FATHER_S_MOTHER = 'father_s_mother';
-    const FATHER_S_MOTHER_S_FATHER = 'father_s_mother_s_father';
-    const FATHER_S_MOTHER_S_MOTHER = 'father_s_mother_s_mother';
-
-    const MOTHER_S_FATHER = 'mother_s_father';
-    const MOTHER_S_FATHER_S_FATHER = 'mother_s_father_s_father';
-    const MOTHER_S_FATHER_S_MOTHER = 'mother_s_father_s_mother';
-
-    const MOTHER_S_MOTHER = 'mother_s_mother';
-    const MOTHER_S_MOTHER_S_FATHER = 'mother_s_mother_s_father';
-    const MOTHER_S_MOTHER_S_MOTHER = 'mother_s_mother_s_mother';
-
-    //TODO Animal Measurements. Wait until the animal import is complete to see the real fields that are used.
 }
