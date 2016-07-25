c2ee6e4a480ce8daaa06fb23292924870f66f78c
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-07-25 07:17:19 +0200

    Create getters for body measurement values in their repositories

--- src/AppBundle/Entity/MuscleThicknessRepository.php
+++ src/AppBundle/Entity/MuscleThicknessRepository.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace AppBundle\Entity;
+use Doctrine\Common\Collections\Criteria;
 
 /**
  * Class MuscleThicknessRepository
@@ -8,4 +9,30 @@ namespace AppBundle\Entity;
  */
 class MuscleThicknessRepository extends BaseRepository {
 
+    /**
+     * @param Animal $animal
+     * @return float
+     */
+    public function getLatestMuscleThickness(Animal $animal)
+    {
+        //Measurement Criteria
+        $criteria = Criteria::create()
+            ->where(Criteria::expr()->eq('animal', $animal))
+            ->orderBy(['measurementDate' => Criteria::DESC])
+            ->setMaxResults(1);
+
+        //MuscleThickness
+        $latestMuscleThickness = $this->getEntityManager()->getRepository(MuscleThickness::class)
+            ->matching($criteria);
+
+        if(sizeof($latestMuscleThickness) > 0) {
+            $latestMuscleThickness = $latestMuscleThickness->get(0);
+            $latestMuscleThickness = $latestMuscleThickness->getMuscleThickness();
+        } else {
+            $latestMuscleThickness = 0.0;
+        }
+
+        return $latestMuscleThickness;
+    }
+    
 }
