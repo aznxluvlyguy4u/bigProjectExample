653ebab2c89f1369e3e3700230ceba21813e1add
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-07-25 07:18:38 +0200

    Create repository function to get Litters of given Ram or Ewe

--- src/AppBundle/Entity/LitterRepository.php
+++ src/AppBundle/Entity/LitterRepository.php
@@ -1,11 +1,36 @@
 <?php
 
 namespace AppBundle\Entity;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Criteria;
 
 /**
  * Class LitterRepository
  * @package AppBundle\Entity
  */
 class LitterRepository extends BaseRepository {
+
+    /**
+     * @param Ram|Ewe $animal
+     * @return ArrayCollection
+     */
+    public function getLitters($animal)
+    {
+        if($animal instanceof Ewe) {
+            $criteria = Criteria::create()
+                ->where(Criteria::expr()->eq('animalMother', $animal));
+            return $this->getEntityManager()->getRepository(Litter::class)
+                ->matching($criteria);
+
+        } elseif ($animal instanceof Ram) {
+            $criteria = Criteria::create()
+                ->where(Criteria::expr()->eq('animalFather', $animal));
+            return $this->getEntityManager()->getRepository(Litter::class)
+                ->matching($criteria);
+
+        } else {
+            return new ArrayCollection(); //empty ArrayCollection
+        }
+    }
 
 }
