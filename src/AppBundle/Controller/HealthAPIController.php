1e70bc11e1e5a049ae7b598b92d25e43a4697245
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-08-05 11:20:04 +0200

    Create EDIT LocationHealth endpoint
    Former-commit-id: ced3e3bfb1de87c58065a71f3647c31cb25055c1

--- src/AppBundle/Controller/HealthAPIController.php
+++ src/AppBundle/Controller/HealthAPIController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use AppBundle\Constant\Constant;
 use AppBundle\Entity\Location;
 use AppBundle\Enumerator\AccessLevelType;
+use AppBundle\FormInput\LocationHealth;
 use AppBundle\Output\HealthOutput;
 use AppBundle\Validation\AdminValidator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -63,5 +64,38 @@ class HealthAPIController extends APIController implements HealthAPIControllerIn
     return new JsonResponse(array(Constant::RESULT_NAMESPACE => $outputArray), 200);
   }
 
+  /**
+   *
+   * Edit Health statuses of the given ubn.
+   *
+   * @param Request $request the request object
+   * @param String $ubn
+   * @return JsonResponse
+   * @Route("/{ubn}/health")
+   * @Method("PUT")
+   */
+  public function updateHealthStatus(Request $request, $ubn) {
 
+    $admin = $this->getAuthenticatedEmployee($request);
+    $adminValidator = new AdminValidator($admin, AccessLevelType::SUPER_ADMIN);
+    if(!$adminValidator->getIsAccessGranted()) { //validate if user is at least a SUPER_ADMIN
+      return $adminValidator->createJsonErrorResponse();
+    }
+
+    $em = $this->getDoctrine()->getEntityManager();
+    $location = $em->getRepository(Location::class)->findByUbn($ubn);
+
+    if($location == null) {
+      $errorMessage = "No Location found with ubn: " . $ubn;
+      return new JsonResponse(array('code'=>428, "message" => $errorMessage), 428);
+    }
+    
+    $content = $this->getContentAsArray($request);    
+    $location = LocationHealth::update($em, $location, $content); //includes persisting changes
+    
+    $outputArray = HealthOutput::create($em, $location);
+
+    return new JsonResponse(array(Constant::RESULT_NAMESPACE => $outputArray), 200);
+    
+  }
 }
