1738710a6a158c6d9d4a0d7d45f858ab7d3cec1c
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-08-07 22:21:58 +0200

    Create Symfony Command util functions

---
+++ src/AppBundle/Util/CommandUtil.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace AppBundle\Util;
+
+
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\Question;
+
+class CommandUtil
+{
+    /** @var InputInterface */
+    private $inputInterface;
+
+    /** @var OutputInterface */
+    private $outputInterface;
+
+    /** @var QuestionHelper */
+    private $helper;
+
+    /**
+     * CommandUtil constructor.
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @param QuestionHelper $helper
+     */
+    public function __construct(InputInterface $input, OutputInterface $output, QuestionHelper $helper)
+    {
+        $this->inputInterface = $input;
+        $this->outputInterface = $output;
+        $this->helper = $helper;
+    }
+
+    /**
+     * @param string $question
+     * @param string $defaultAnswer
+     * @param bool $isCleanupString
+     * @return string
+     */
+    public function generateQuestion($question, $defaultAnswer, $isCleanupString = true)
+    {
+        $question = new Question([  ' ',
+                                    $question,
+                ':   ']
+            , $defaultAnswer);
+        $answer = $this->helper->ask($this->inputInterface, $this->outputInterface, $question);
+
+        if($isCleanupString) {
+            $answer = str_replace(array(' ', "\n", "\t", "\r", "'"), '', $answer);
+        }
+
+        return $answer;
+    }
+
+    /**
+     * @return bool
+     */
+    public function generateConfirmationQuestion($confirmationQuestion = 'Continue with this action?')
+    {
+        $question = new ConfirmationQuestion(
+            $confirmationQuestion,
+            false,
+            '/^(y|j)/i' //Anything starting with y or j is accepted
+        );
+
+        if (!$this->helper->ask($this->inputInterface, $this->outputInterface, $question)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * @param string $heading
+     * @return array
+     */
+    public static function generateTitle($heading)
+    {
+        return [' ',
+            $heading,
+            $line = str_repeat('=', strlen($heading)),
+            '',
+        ];
+    }
+}
