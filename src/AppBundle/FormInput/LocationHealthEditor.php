fcbfae56886b42fd436fbcc8c918461d41d9555c
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-08-10 02:32:40 +0200

    Implement LocationHealthEdit including hide obsolete illnesses logic
    Former-commit-id: 36cca33db5a832205fd3d58a9f475e28d2eab279

--- src/AppBundle/FormInput/LocationHealthEditor.php
+++ src/AppBundle/FormInput/LocationHealthEditor.php
@@ -11,6 +11,7 @@ use AppBundle\Entity\Scrapie;
 use AppBundle\Enumerator\MaediVisnaStatus;
 use AppBundle\Enumerator\ScrapieStatus;
 use AppBundle\Util\Finder;
+use AppBundle\Util\LocationHealthUpdater;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\Validator\Constraints\DateTime;
@@ -47,39 +48,53 @@ class LocationHealthEditor
         //Get the latest locationHealth status to use as a benchmark
         $latestIllnessStatuses = Finder::findLatestActiveIllnessStatusesOfLocation($location, $em);
 
+        
         /* Scrapie values */
         
         $lastScrapieStatus = $latestIllnessStatuses->get(JsonInputConstant::SCRAPIE_STATUS);
         $lastScrapieStatus = Utils::fillNullOrEmptyString($lastScrapieStatus, self::defaultScrapieStatus);
-        $lastScrapieEndDate = $latestIllnessStatuses->get(JsonInputConstant::SCRAPIE_END_DATE);
         $lastScrapieCheckDate = $latestIllnessStatuses->get(JsonInputConstant::SCRAPIE_CHECK_DATE);
         
         $newScrapieStatus = Utils::getNullCheckedArrayCollectionValue(JsonInputConstant::SCRAPIE_STATUS, $content);
         $newScrapieCheckDate = Utils::getNullCheckedArrayCollectionDateValue(JsonInputConstant::SCRAPIE_CHECK_DATE, $content);
-        $newScrapieEndDate = Utils::getNullCheckedArrayCollectionDateValue(JsonInputConstant::SCRAPIE_END_DATE, $content);
+
+        if($newScrapieCheckDate == null) {
+            $newScrapieCheckDate = new \DateTime('now');
+            $scrapieDatesChanged = false; //ignore this check if no date is given
+        } else {
+            $scrapieDatesChanged = $newScrapieCheckDate != $lastScrapieCheckDate;
+        }
 
         $scrapieStatusChanged = $newScrapieStatus != $lastScrapieStatus;
-        $scrapieDatesChanged = !($newScrapieCheckDate == $lastScrapieCheckDate && $newScrapieEndDate == $lastScrapieEndDate);
 
+        
         /* MaediVisna values */
 
         $lastMaediVisnaStatus = $latestIllnessStatuses->get(JsonInputConstant::MAEDI_VISNA_STATUS);
         $lastMaediVisnaStatus = Utils::fillNullOrEmptyString($lastMaediVisnaStatus, self::defaultMaediVisnaStatus);
-        $lastMaediVisnaEndDate = $latestIllnessStatuses->get(JsonInputConstant::MAEDI_VISNA_END_DATE);
         $lastMaediVisnaCheckDate = $latestIllnessStatuses->get(JsonInputConstant::MAEDI_VISNA_CHECK_DATE);
         
         $newMaediVisnaStatus = Utils::getNullCheckedArrayCollectionValue(JsonInputConstant::MAEDI_VISNA_STATUS, $content);
         $newMaediVisnaCheckDate = Utils::getNullCheckedArrayCollectionDateValue(JsonInputConstant::MAEDI_VISNA_CHECK_DATE, $content);
-        $newMaediVisnaEndDate = Utils::getNullCheckedArrayCollectionDateValue(JsonInputConstant::MAEDI_VISNA_END_DATE, $content);
+
+        if($newMaediVisnaCheckDate == null) {
+            $newMaediVisnaCheckDate = new \DateTime('now');
+            $maediVisnaDatesChanged = false; //ignore this check if no date is given
+        } else {
+            $maediVisnaDatesChanged = $newMaediVisnaCheckDate != $lastMaediVisnaCheckDate;
+        }
 
         $maediVisnaStatusChanged = $newMaediVisnaStatus != $lastMaediVisnaStatus;
-        $maediVisnaDatesChanged = !($newMaediVisnaCheckDate == $lastMaediVisnaCheckDate && $newMaediVisnaEndDate == $lastMaediVisnaEndDate);
 
+        
         /* LocationHealth Entity */
 
         //Only create a new Scrapie if there was any change in the values
         if($scrapieStatusChanged || $scrapieDatesChanged) {
-            $scrapie = new Scrapie($newScrapieStatus, $newScrapieEndDate);
+            //First hide the obsolete scrapies
+            LocationHealthUpdater::hideAllFollowingScrapies($em, $location, $newMaediVisnaCheckDate);
+
+            $scrapie = new Scrapie($newScrapieStatus);
             $scrapie->setCheckDate($newScrapieCheckDate);
             $locationHealth->addScrapie($scrapie);
             $scrapie->setLocationHealth($locationHealth);
@@ -88,7 +103,10 @@ class LocationHealthEditor
 
         //Only create a new MaediVisna if there was any change in the values
         if($maediVisnaStatusChanged || $maediVisnaDatesChanged) {
-            $maediVisna = new MaediVisna($newMaediVisnaStatus, $newMaediVisnaEndDate);
+            //First hide the obsolete maedi visnas
+            LocationHealthUpdater::hideAllFollowingMaediVisnas($em, $location, $newMaediVisnaCheckDate);
+
+            $maediVisna = new MaediVisna($newMaediVisnaStatus, $newMaediVisnaCheckDate);
             $maediVisna->setCheckDate($newMaediVisnaCheckDate);
             $locationHealth->addMaediVisna($maediVisna);
             $maediVisna->setLocationHealth($locationHealth);
