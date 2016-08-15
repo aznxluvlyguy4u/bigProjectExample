cb2824054154ed0072ca1679ded288cfb9ae2ede
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-08-15 14:59:56 +0200

    Create BreedCodyType enumerator

---
+++ src/AppBundle/Enumerator/BreedCodeType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppBundle\Enumerator;
+
+class BreedCodeType
+{
+  const TE = "TE";
+  const BT = "BT";
+  const DK = "DK";
+  const CF = "CF";
+  const NH = "NH";
+  const OV = "OV";
+}
