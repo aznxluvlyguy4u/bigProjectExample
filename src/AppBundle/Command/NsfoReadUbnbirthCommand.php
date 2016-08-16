c38eb4ee2edaaab2d708c2a9d0c5d15c6401162d
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-08-16 21:27:46 +0200

    Complete function to read ubnOfBirth from textfile

--- src/AppBundle/Command/NsfoReadUbnbirthCommand.php
+++ src/AppBundle/Command/NsfoReadUbnbirthCommand.php
@@ -7,6 +7,7 @@ use AppBundle\Entity\Person;
 use AppBundle\Util\CommandUtil;
 use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Query;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -17,8 +18,9 @@ class NsfoReadUbnbirthCommand extends ContainerAwareCommand
 {
     const TITLE = 'Read UBN of birth from text file and save it to animal';
     const DEFAULT_INPUT_FOLDER_PATH = '/home/data/JVT/projects/NSFO/DocsVanNSFO/DierBedrijf.txt';
-    const PERSIST_BATCH_SIZE = 250;
+    const PERSIST_BATCH_SIZE = 1000;
     const MAX_ROWS_TO_PROCESS = 0; //set 0 for no limit
+    const DEFAULT_START_ROW = 0;
 
     /** @var EntityManager $em */
     private $em;
@@ -47,44 +49,14 @@ class NsfoReadUbnbirthCommand extends ContainerAwareCommand
         $em = $this->getContainer()->get('doctrine.orm.entity_manager');
         $this->em = $em;
 
-        //Output folder input
-        $inputFolder = $cmdUtil->generateQuestion('Please enter input folder path', self::DEFAULT_INPUT_FOLDER_PATH);
+        //input
+        $inputFile = $cmdUtil->generateQuestion('Please enter input file path', self::DEFAULT_INPUT_FOLDER_PATH);
+        $startRow = $cmdUtil->generateQuestion('Choose start row (0 = default)', self::DEFAULT_START_ROW);
 
-        $fileContents = file_get_contents($inputFolder);
+        $fileContents = file_get_contents($inputFile);
         $dataInRows = explode("\r\n", $fileContents);
 
-
-        $count = 0;
-        foreach ($dataInRows as $row) {
-            $row = str_replace(array('  '), ' ', $row); //remove double spaces
-            $rowData = explode(" ", $row);
-
-            $vsmAnimalPrimaryKey = $rowData[0];
-            $dateOfBirth = $rowData[1];
-            $ubnOfBirth = $rowData[2];
-            if($ubnOfBirth == 'Onbekend') {
-                $ubnOfBirth = null;
-            }
-
-            $this->getAnimalByVsmPrimaryKey($vsmAnimalPrimaryKey);
-
-            $animal = $this->getAnimalByVsmPrimaryKey($vsmAnimalPrimaryKey);
-            if($animal != null && $animal->getUbnOfBirth() == null) {
-                $animal->setUbnOfBirth($ubnOfBirth);
-                $em->persist($animal);
-            }
-
-            $count++;
-            if($count%self::PERSIST_BATCH_SIZE) {
-                $em->flush();
-            }
-
-            if($count == self::MAX_ROWS_TO_PROCESS) {
-                break;
-            }
-        }
-        $em->flush();
-
+        $this->readFileFindAnimalInDbAndPersistUbnOfBirth($startRow, $dataInRows, $output);
 
         //Final Results
         $endTime = new \DateTime();
@@ -99,20 +71,53 @@ class NsfoReadUbnbirthCommand extends ContainerAwareCommand
     }
 
 
-    public function getAnimalByVsmPrimaryKey($vsmPrimaryKey)
+    /**
+     * @param int $startRow
+     * @param array $dataInRows
+     * @param OutputInterface $output
+     */
+    private function readFileFindAnimalInDbAndPersistUbnOfBirth($startRow, $dataInRows, OutputInterface $output)
     {
-        $criteria = Criteria::create()
-            ->where(Criteria::expr()->eq('name', $vsmPrimaryKey))
-        ;
+        $em = $this->em;
+        $em->getConnection()->getConfiguration()->setSQLLogger(null);
 
-        /** @var Animal $animal */
-        $animal = $this->em->getRepository(Animal::class)
-            ->matching($criteria)->first();
+        $maxRowCount = sizeof($dataInRows);
 
-        if(!$animal) {
-            return null;
-        } else {
-            return $animal;
+        for($i = $startRow; $i < $maxRowCount; $i++) {
+            $row = $dataInRows[$i];
+//            $row = str_replace(array('  '), ' ', $row); //remove double spaces
+            $rowData = explode(" ", $row);
+
+            if(sizeof($rowData) >= 3) {
+
+                $vsmAnimalPrimaryKey = $rowData[0];
+                $dateOfBirth = $rowData[1];
+                $ubnOfBirth = $rowData[2];
+                if ($ubnOfBirth == 'Onbekend') {
+                    $ubnOfBirth = null;
+                }
+
+                /** @var Animal $animal */
+                $animal = $this->em->getRepository(Animal::class)->findOneByName($vsmAnimalPrimaryKey);
+                if($animal != null && $animal->getUbnOfBirth() == null) {
+                    $animal->setUbnOfBirth($ubnOfBirth);
+                    $em->persist($animal);
+                }
+
+                if ($i%self::PERSIST_BATCH_SIZE == 0) {
+                    $em->flush();
+                    $em->clear();
+                    gc_collect_cycles();
+                    $output->writeln('Now at row: '.$i.' of '.$maxRowCount);
+                }
+
+                if ($i-$startRow+1 == self::MAX_ROWS_TO_PROCESS) {
+                    break;
+                }
+            }
         }
+        $em->flush();
+        $em->clear();
+        gc_collect_cycles();
     }
 }
