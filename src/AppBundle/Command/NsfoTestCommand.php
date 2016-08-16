16e716f7118d0d2c99350966e73193a9cae8c578
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-08-16 09:51:59 +0200

    Add timelogging to TestCommand as example for other commands

--- src/AppBundle/Command/NsfoTestCommand.php
+++ src/AppBundle/Command/NsfoTestCommand.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 class NsfoTestCommand extends ContainerAwareCommand
 {
     const TITLE = 'TESTING';
-    const INPUT_PATH = '/home/data/JVT/projects/NSFO/Migratie/fixed_machtigingenRVO.csv';
+    const INPUT_PATH = '/path/to/file.txt';
 
     /** @var EntityManager $em */
     private $em;
@@ -34,13 +34,28 @@ class NsfoTestCommand extends ContainerAwareCommand
 
         //Print intro
         $output->writeln(CommandUtil::generateTitle(self::TITLE));
+
+        //Timestamp
+        $startTime = new \DateTime();
+        $output->writeln(['Start time: '.date_format($startTime, 'Y-m-d h:m:s'),'']);
+
+        /** @var EntityManager $em */
         $em = $this->getContainer()->get('doctrine.orm.entity_manager');
         $this->em = $em;
 
-        $fileContents = file_get_contents(self::INPUT_PATH);
+//        $fileContents = file_get_contents(self::INPUT_PATH);
+
+
+
+
+        //Final Results
+        $endTime = new \DateTime();
+        $elapsedTime = gmdate("H:i:s", $endTime->getTimestamp() - $startTime->getTimestamp());
 
         $output->writeln([
             '=== PROCESS FINISHED ===',
+            'End Time: '.date_format($endTime, 'Y-m-d h:m:s'),
+            'Elapsed Time (h:m:s): '.$elapsedTime,
             '',
             '']);
     }
