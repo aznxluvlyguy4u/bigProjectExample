308927adf09d86cbc4b283713a57df22e05093e3
D S <Dw1LS@outlook.com>
2016-08-22 18:43:43 +0200

    Implement admin management (Create/Update/Hide)
    Former-commit-id: 5152a5eeb9adedcbc5abe1962f06c9ed3b8dbc61

--- src/AppBundle/Controller/AdminAPIControllerInterface.php
+++ src/AppBundle/Controller/AdminAPIControllerInterface.php
@@ -6,9 +6,9 @@ use Symfony\Component\HttpFoundation\Request;
 
 interface AdminAPIControllerInterface {
     function getAdmins(Request $request);
-    function createAdmins(Request $request);
-    function editAdmins(Request $request);
-    function deactivateAdmins(Request $request);
+    function createAdmin(Request $request);
+    function editAdmin(Request $request);
+    function deactivateAdmin(Request $request);
     function getTemporaryGhostToken(Request $request);
     function verifyGhostToken(Request $request);
     function getAccessLevelTypes(Request $request);
