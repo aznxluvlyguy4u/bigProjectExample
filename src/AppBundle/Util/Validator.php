9e78bf0ce256a36ed22d15a33b0c978c5bf3a0ce
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-08-24 12:53:59 +0200

    Block user login with deactivated companies
    (cherry picked from commit 710be6514a035656f2c4436defe8b486f08045bf [formerly 63bd15467faddc0514418a64f5805e4963b2f6be])
    
    
    Former-commit-id: 156d92cb8bf27a6d9f453d35e6c695dcc23fe499

--- src/AppBundle/Util/Validator.php
+++ src/AppBundle/Util/Validator.php
@@ -13,6 +13,9 @@ use AppBundle\Entity\Company;
 use AppBundle\Entity\Location;
 use AppBundle\Entity\Neuter;
 use AppBundle\Entity\Ram;
+use AppBundle\Entity\Person;
+use AppBundle\Entity\Employee;
+use AppBundle\Entity\Token;
 use AppBundle\Enumerator\GenderType;
 use AppBundle\Util\NullChecker;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -195,5 +198,82 @@ class Validator
         //Validate pedigree if it exists
         return self::verifyPedigreeCodeInAnimalArray($manager, $animalArray, false);
     }
-    
+
+
+    /**
+     * @param Person $user
+     * @param null $ghostToken
+     * @return bool
+     */
+    public static function isUserLoginWithActiveCompany($user, $ghostToken = null)
+    {
+        if($user instanceof Client) {
+            return self::isCompanyActiveOfClient($user);
+        } else if($user instanceof Employee && $ghostToken instanceof Token) {
+            return self::isCompanyActiveOfGhostToken($ghostToken);
+        } else {
+            //only Clients and Employees with GhostTokens are able to login
+            return false;
+        }
+    }
+
+    /**
+     * TODO At the moment any Client(user) can only own one Company OR be an employee a one company. When this changes, this validation check has to be updated.
+     * @param Client $client
+     * @return bool
+     */
+    private static function isCompanyActiveOfClient($client)
+    {
+        //null check
+        if(!($client instanceof Client)) { return false; }
+
+        if($client->hasEmployer()) {
+
+            //is user employee at the company
+            $isActive = $client->getEmployer()->isActive();
+            if($isActive) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } else {
+            //is owner at at least one of owner's companies
+            $companies = $client->getCompanies();
+            $deactivatedCompanies = 0;
+            /** @var Company $company */
+            foreach ($companies as $company) {
+                if(!$company->isActive()) {
+                    $deactivatedCompanies++;
+                }
+            }
+
+            if($deactivatedCompanies == $companies->count()) {
+                //has no active companies
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+
+    /**
+     * @param Token $ghostToken
+     * @return bool
+     */
+    private static function isCompanyActiveOfGhostToken($ghostToken)
+    {
+        //null check
+        if(!($ghostToken instanceof Token)) { return false; }
+
+        $tokenOwner = $ghostToken->getOwner();
+        if($tokenOwner instanceof Client) {
+            return self::isCompanyActiveOfClient($tokenOwner);
+        } else {
+            //not a client, so cannot even have any companies
+            return false;
+        }
+
+    }
 }
