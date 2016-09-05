a3c071c6da3ab38e5d2a2d0939b16aa6046764e6
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-09-05 22:20:10 +0200

    FIX getLocation function to return a Location instead of an array and refactor custom repository name to decrease confusion
    Former-commit-id: c4e5ed439a217a6cd85dcdf337855e69e6ba3ca8

--- src/AppBundle/Command/NsfoMigrateLittersCommand.php
+++ src/AppBundle/Command/NsfoMigrateLittersCommand.php
@@ -14,6 +14,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
 
 class NsfoMigrateLittersCommand extends ContainerAwareCommand
 {
@@ -21,6 +22,15 @@ class NsfoMigrateLittersCommand extends ContainerAwareCommand
     const DEFAULT_INPUT_PATH = '/home/data/JVT/projects/NSFO/Migratie/Animal/animal_litters_20160307_1349.csv';
     const BATCH_SIZE = 1000;
     const DEFAULT_MIN_VSM_ID = 1;
+
+    const DEFAULT_START_ROW = 0;
+
+
+    private $csvParsingOptions = array(
+        'finder_in' => 'app/Resources/imports/',
+        'finder_name' => 'animal_litters_20160307_1349.csv',
+        'ignoreFirstLine' => true
+    );
 
     /** @var ArrayCollection $litterSets */
     private $litterSets;
@@ -55,7 +65,7 @@ class NsfoMigrateLittersCommand extends ContainerAwareCommand
 
         $dataWithoutHeader = $cmdUtil::getRowsFromCsvFileWithoutHeader($inputFolderPath);
 
-        $cmdUtil->setStartTimeAndPrintIt(count($dataWithoutHeader), $minVsmId);
+        $cmdUtil->setStartTimeAndPrintIt(count($dataWithoutHeader)*2, $minVsmId);
 
         $rowCount = 0;
         $this->litterSets = new ArrayCollection();
@@ -64,11 +74,8 @@ class NsfoMigrateLittersCommand extends ContainerAwareCommand
 
             $this->groupLittersByEweVsmId($row);
             $rowCount++;
-            if($rowCount%100000 == 0) {
-                $output->writeln('Litters processed into array: '.$rowCount);
-            }
+            $cmdUtil->setProgressBarMessage('Litters grouped into array: '.$rowCount);
         }
-        $output->writeln('Litters processed into array: '.$rowCount);
 
         $output->writeln('Removing litters of Ewes with vsmId below given minimum');
         $eweVsmIds = $this->litterSets->getKeys();
@@ -160,6 +167,132 @@ class NsfoMigrateLittersCommand extends ContainerAwareCommand
 
         $this->litterSets->get($vsmId)
             ->set($litterDate, [$bornAliveCount, $stillbornCount]);
+    }
+
+
+    private function parseCSV() {
+        $ignoreFirstLine = $this->csvParsingOptions['ignoreFirstLine'];
+
+        $finder = new Finder();
+        $finder->files()
+            ->in($this->csvParsingOptions['finder_in'])
+            ->name($this->csvParsingOptions['finder_name'])
+        ;
+        foreach ($finder as $file) { $csv = $file; }
+
+        $rows = array();
+        if (($handle = fopen($csv->getRealPath(), "r")) !== FALSE) {
+            $i = 0;
+            while (($data = fgetcsv($handle, 30, ";")) !== FALSE) {
+                $i++;
+                if ($ignoreFirstLine && $i == 1) { continue; }
+                $rows[] = $data;
+                gc_collect_cycles();
+            }
+            fclose($handle);
+        }
+
+        return $rows;
+    }
+
+
+    protected function executeOld(InputInterface $input, OutputInterface $output)
+    {
+        /** @var ObjectManager $em */
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $this->em = $em;
+        $helper = $this->getHelper('question');
+        $cmdUtil = new CommandUtil($input, $output, $helper);
+
+        //Print intro
+        $output->writeln(CommandUtil::generateTitle(self::TITLE));
+
+        //Input folder input
+        $inputFolderPath = $cmdUtil->generateQuestion('Please enter input folder path: ', self::DEFAULT_INPUT_PATH);
+        $minVsmId = $cmdUtil->generateQuestion('Please enter minimum vsmId (default = 1): ', self::DEFAULT_MIN_VSM_ID);
+
+        $this->litterSets = new ArrayCollection();
+
+        $dataWithoutHeader = $cmdUtil::getRowsFromCsvFileWithoutHeader($inputFolderPath);
+
+        $cmdUtil->setStartTimeAndPrintIt(count($dataWithoutHeader)*2, $minVsmId);
+
+        $rowCount = 0;
+        $this->litterSets = new ArrayCollection();
+        //First save pedigree date per vsmId in an ArrayCollection
+        foreach ($dataWithoutHeader as $row) {
+
+            $this->groupLittersByEweVsmId($row);
+            $rowCount++;
+            $cmdUtil->setProgressBarMessage('Litters grouped into array: '.$rowCount);
+        }
+
+        $output->writeln('Removing litters of Ewes with vsmId below given minimum');
+        $eweVsmIds = $this->litterSets->getKeys();
+        $output->writeln('Ewes before filtering on min vsmId: '.sizeof($eweVsmIds));
+        $removeVsmIds = array();
+        foreach($eweVsmIds as $eweVsmId) {
+            if($eweVsmId < $minVsmId) {
+                $removeVsmIds[] = $eweVsmId;
+            }
+        }
+        $eweVsmIds = array_diff($eweVsmIds, $removeVsmIds);
+        $output->writeln('Ewes to process: '.sizeof($eweVsmIds));
+
+        $output->writeln('Creating new litters...');
+
+        $litterCount = 0;
+        $eweCount = 0;
+        foreach ($eweVsmIds as $eweVsmId)
+        {
+            /** @var Ewe $ewe */
+            $ewe = $em->getRepository(Ewe::class)->findOneByName($eweVsmId);
+
+            if($ewe != null) {
+                /** @var ArrayCollection $littersDataSet */
+                $littersDataSet = $this->litterSets->get($eweVsmId);
+                $litterDates = $littersDataSet->getKeys();
+
+                foreach ($litterDates as $litterDateString) {
+                    $children = $littersDataSet->get($litterDateString);
+
+                    $litterDate = new \DateTime($litterDateString);
+                    $bornAliveCount = $children[0];
+                    $stillbornCount = $children[1];
+
+                    $foundLitter = $em->getRepository(Litter::class)->findOneBy([
+                        'animalMother' => $ewe,
+                        'litterDate' => $litterDate
+                    ]);
+
+                    if($foundLitter == null) {
+//                        Litter data has not been migrated yet, so persist a new litter
+                        $litter = new Litter();
+                        $litter->setAnimalMother($ewe);
+                        $litter->setLitterDate($litterDate);
+                        $litter->setBornAliveCount($bornAliveCount);
+                        $litter->setStillbornCount($stillbornCount);
+
+                        $em->persist($litter);
+                        $litterCount++;
+                    }
+
+                    $cmdUtil->advanceProgressBar(1,'Checked LitterCount: '.$litterCount.' |  EweCount: '.$eweCount.' |  last vsmId: '.$eweVsmId);
+                }
+                $eweCount++;
+
+                if($eweCount%self::BATCH_SIZE == 0) {
+                    DoctrineUtil::flushClearAndGarbageCollect($em);
+                }
+            }
+        }
+        DoctrineUtil::flushClearAndGarbageCollect($em);
+
+        $output->writeln([  'Ewes processed: '.$eweCount,
+            'Litters processed: '.$litterCount
+        ]);
+
+        $cmdUtil->setEndTimeAndPrintFinalOverview();
     }
 
 }
