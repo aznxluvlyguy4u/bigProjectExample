3c494a00f12b0bf520107e4845529f8e39de47d7
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-09-08 18:18:46 +0200

    Deactivate old unit tests and move them outside the Tests folder
    Former-commit-id: 5b1a8e171df1aa487e131411601a55d1d2739369

---
+++ src/AppBundle/TestsDeactivated/CountryAPIControllerTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use AppBundle\Service\IRSerializer;
+use AppBundle\DataFixtures\ORM\MockedCountries;
+use AppBundle\DataFixtures\ORM\MockedClient;
+use Doctrine\ORM\EntityManager;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Client as RequestClient;
+use AppBundle\Entity\Country;
+use AppBundle\Entity\Client;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Class CountryAPIControllerTest
+ * @package AppBundle\Tests\Controller
+ * @group country
+ */
+class CountryAPIControllerTest extends WebTestCase {
+
+  const COUNTRIES_ENDPOINT = "/api/v1/countries";
+
+  /**
+   * @var RequestClient
+   */
+  private $client;
+
+  /**
+   * @var EntityManager
+   */
+  static private $entityManager;
+
+  /**
+   * @var Client
+   */
+  static private $mockedClient;
+
+  /**
+   * @var array
+   */
+  static private $mockedCountries;
+
+  /**
+   * @var array
+   */
+  private $defaultHeaders;
+
+//  /**
+//   * Runs on each testcase
+//   */
+//  public function setUp()
+//  {
+//    $this->client = parent::createClient();
+//
+//    //Load fixture class
+//    $fixtures = array('AppBundle\DataFixtures\ORM\MockedClient',
+//      'AppBundle\DataFixtures\ORM\MockedCountries');
+//    $this->loadFixtures($fixtures);
+//
+//    //Get mocked Client
+//    self::$mockedClient = MockedClient::getMockedClient();
+//    $this->accessToken = self::$mockedClient->getAccessToken();
+//
+//    //Get mocked Country
+//    self::$mockedCountries  = MockedCountries::getMockedCountriesList();
+//
+//    $this->defaultHeaders = array(
+//      'CONTENT_TYPE' => 'application/json',
+//      'HTTP_ACCESSTOKEN' => self::$mockedClient->getAccessToken(),
+//    );
+//  }
+//
+//  /**
+//   * Runs before class setup
+//   */
+//  public static function setUpBeforeClass()
+//  {
+//    //start the symfony kernel
+//    $kernel = static::createKernel();
+//    $kernel->boot();
+//
+//    static::$kernel = static::createKernel();
+//    static::$kernel->boot();
+//
+//    //Get the DI container
+//    $container = $kernel->getContainer();
+//
+//    //Get service classes
+//    self::$entityManager = $container->get('doctrine.orm.entity_manager');
+//  }
+//
+//  /**
+//   * @group get
+//   * @group country-get
+//   * Test retrieving Countries list
+//   */
+//  public function testGetCountries()
+//  {
+//    $this->client->request('GET',
+//      $this::COUNTRIES_ENDPOINT,
+//      array(),
+//      array(),
+//      $this->defaultHeaders
+//    );
+//
+//    $response = $this->client->getResponse();
+//    $data = json_decode($response->getContent(), true);
+//
+//    $this->assertEquals(sizeof(self::$mockedCountries), sizeof($data['result']));
+//  }
+//
+//  public function tearDown()
+//  {
+//    parent::tearDown();
+//  }
+//
+//  /*
+//   * Runs after all testcases ran and teardown
+//   */
+//  public static function tearDownAfterClass()
+//  {
+//
+//  }
+}
