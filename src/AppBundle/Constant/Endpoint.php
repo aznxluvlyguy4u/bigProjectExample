dc9a9538ca2dacad7d4a8314fc5a9ad2b2214017
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-09-22 15:20:15 +0200

    Create loss unit test
    Former-commit-id: 1387d8263d0d55c3653603db974eca9bbee7ea0f

--- src/AppBundle/Constant/Endpoint.php
+++ src/AppBundle/Constant/Endpoint.php
@@ -7,6 +7,7 @@ class Endpoint
 {
     const DECLARE_ARRIVAL_ENDPOINT = "/api/v1/arrivals";
     const DECLARE_IMPORT_ENDPOINT = "/api/v1/imports";
+    const DECLARE_LOSSES_ENDPOINT = "/api/v1/losses";
     
     const REVOKE_ENDPOINT = "/api/v1/revokes";
 }
