afb300fa2417ed4af870b3dbe1fdeaae92174f9b
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-09-23 11:08:27 +0200

    Refactor validator general functionality into new BaseValidator abstract class
    Former-commit-id: b07742dd43210a5125e6c6f9d8d0ef8b09085ff5

---
+++ src/AppBundle/Validation/BaseValidator.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace AppBundle\Validation;
+
+
+use AppBundle\Component\HttpFoundation\JsonResponse;
+use AppBundle\Entity\AnimalRepository;
+use AppBundle\Util\Validator;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/**
+ * Class BaseValidator
+ * @package AppBundle\Validation
+ */
+abstract class BaseValidator
+{
+    const ERROR_CODE = 428;
+    const ERROR_MESSAGE = 'INVALID INPUT';
+    const VALID_CODE = 200;
+    const VALID_MESSAGE = 'OK';
+
+    /** @var boolean */
+    protected $isInputValid;
+
+    /** @var ObjectManager */
+    protected $manager;
+
+    /** @var array */
+    protected $errors;
+
+    public function __construct(ObjectManager $manager, ArrayCollection $content)
+    {
+        $this->manager = $manager;
+        $this->isInputValid = false;
+        $this->errors = array();
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function getIsInputValid()
+    {
+        return $this->isInputValid;
+    }
+
+
+
+    /**
+     * @return JsonResponse
+     */
+    public function createJsonResponse()
+    {
+        if($this->isInputValid){
+            return Validator::createJsonResponse(self::VALID_MESSAGE, self::VALID_CODE);
+        } else {
+            return Validator::createJsonResponse(self::ERROR_MESSAGE, self::ERROR_CODE, $this->errors);
+        }
+    }
+
+}
