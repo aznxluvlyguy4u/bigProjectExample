ae70a90a286c270d34ea2a0492123ab6b26cbee6
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-09-28 12:27:29 +0200

    Setup structure for Migrating Measurements2016v2 command
    Former-commit-id: cf6e63dcc2b154310d301efe09ef65df68a8172f

---
+++ src/AppBundle/Command/NsfoMigrateMeasurements2016v2Command.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Component\Utils;
+use AppBundle\Entity\Animal;
+use AppBundle\Entity\AnimalRepository;
+use AppBundle\Entity\BodyFat;
+use AppBundle\Entity\BodyFatRepository;
+use AppBundle\Entity\ExteriorRepository;
+use AppBundle\Entity\MuscleThickness;
+use AppBundle\Entity\MuscleThicknessRepository;
+use AppBundle\Entity\TailLengthRepository;
+use AppBundle\Entity\Weight;
+use AppBundle\Entity\WeightRepository;
+use AppBundle\Util\CommandUtil;
+use AppBundle\Util\NullChecker;
+use AppBundle\Util\TimeUtil;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+
+class NsfoMigrateMeasurements2016v2Command extends ContainerAwareCommand
+{
+    const TITLE = 'MigrateMeasurements2016v2';
+    const INPUT_PATH = '/path/to/file.txt';
+    const IS_GROUPED_BY_ANIMAL_AND_DATE = true;
+    const FOLDER_NAME = 'migration';
+    const FILE_NAME_VSM_IDS_NOT_IN_DB = 'vsmId_van_metingen_waarvan_er_geen_dieren_in_de_database_zitten.txt';
+
+    /** @var ObjectManager $em */
+    private $em;
+
+    /** @var AnimalRepository $animalRepository */
+    private $animalRepository;
+
+    /** @var WeightRepository $weightRepository */
+    private $weightRepository;
+
+    /** @var BodyFatRepository $bodyFatRepository */
+    private $bodyFatRepository;
+
+    /** @var MuscleThicknessRepository $muscleThicknessRepository */
+    private $muscleThicknessRepository;
+
+    /** @var array */
+    private $idByAiindArray;
+
+    /** @var array */
+    private $weightsInDb;
+
+    /** @var array */
+    private $bodyFatsInDb;
+
+    /** @var array */
+    private $muscleThicknessesInDb;
+
+    /** @var string */
+    private $outputFolder;
+
+    private $csvParsingOptions = array(
+        'finder_in' => 'app/Resources/imports/',
+        'finder_out' => 'app/Resources/outputs/',
+        'finder_name' => '20160905_1040_Dierprestatietabel.csv',
+        'ignoreFirstLine' => true
+    );
+
+    protected function configure()
+    {
+        $this
+            ->setName('nsfo:migrate:measurements2016v2')
+            ->setDescription(self::TITLE)
+        ;
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** @var ObjectManager $em */
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $this->em = $em;
+        $helper = $this->getHelper('question');
+        $cmdUtil = new CommandUtil($input, $output, $helper);
+
+        //Print intro
+        $output->writeln(CommandUtil::generateTitle(self::TITLE));
+
+        $csv = $this->parseCSV();
+        $totalNumberOfRows = sizeof($csv);
+
+        $startUnit = 1;
+        $startMessage = 'Retrieving search arrays...';
+        $cmdUtil->setStartTimeAndPrintIt($totalNumberOfRows, $startUnit, $startMessage);
+
+        //Create folders
+        $this->outputFolder = $this->getContainer()->get('kernel')->getRootDir().'/Resources/outputs/'.self::FOLDER_NAME;
+        NullChecker::createFolderPathIfNull($this->outputFolder);
+
+        //Set repositories
+        $this->animalRepository = $this->em->getRepository(Animal::class);
+        $this->weightRepository = $this->em->getRepository(Weight::class);
+        $this->bodyFatRepository = $this->em->getRepository(BodyFat::class);
+        $this->muscleThicknessRepository = $this->em->getRepository(MuscleThickness::class);
+
+        //Generate search arrays
+        $this->idByAiindArray = $this->animalRepository->getAnimalPrimaryKeysByVsmIdArray();
+        $this->weightsInDb = $this->weightRepository->getAllWeightsBySql(self::IS_GROUPED_BY_ANIMAL_AND_DATE);
+        $this->bodyFatsInDb = $this->bodyFatRepository->getAllBodyFatsBySql(self::IS_GROUPED_BY_ANIMAL_AND_DATE);
+        $this->muscleThicknessesInDb = $this->muscleThicknessRepository->getAllMuscleThicknessesBySql(self::IS_GROUPED_BY_ANIMAL_AND_DATE);
+
+        //Result arrays
+        $vsmIdsNotInDatabase = array();
+
+        //Counters
+        $rowCount = 0;
+        $weightsNew = 0;
+        $weightsFixed = 0;
+        $weightsSkipped = 0;
+
+        foreach ($csv as $row)
+        {
+            $vsmId = $row[0];
+            $measurementDateString = TimeUtil::flipDateStringOrder($row[1]);
+            $inspector = $row[2];
+
+            $weight = $row[7];
+            $fat1 = $row[8];
+            $fat2 = $row[9];
+            $fat3 = $row[10];
+            $muscleThickness = $row[11];
+
+            $animalIdAndDate = $this->writeAnimalIdAndDate($vsmId, $measurementDateString);
+
+            //First null check animal
+            if($animalIdAndDate == null) {
+                $vsmIdsNotInDatabase[$vsmId] = $vsmId;
+
+            } else {
+                //Check weights
+                if(NullChecker::isNotNull($weight)) {
+
+//                Block Birth Weights > 10 kg TODO
+
+                }
+
+                //Check BodyFats
+                if(NullChecker::isNotNull($fat1) && NullChecker::isNotNull($fat2) && NullChecker::isNotNull($fat3)) {
+
+                }
+
+                //Check MuscleThicknesses
+                if(NullChecker::isNotNull($muscleThickness)) {
+
+                }
+            }
+
+            $message = $rowCount; //FIXME
+            $cmdUtil->advanceProgressBar(1, $message);
+        }
+
+        //Printing Errors
+        foreach ($vsmIdsNotInDatabase as $vsmId) {
+            file_put_contents($this->outputFolder.'/'.self::FILE_NAME_VSM_IDS_NOT_IN_DB, $vsmId."\n", FILE_APPEND);
+        }
+
+        $cmdUtil->setEndTimeAndPrintFinalOverview();
+    }
+
+    /**
+     * @param int $vsmId
+     * @param string $measurementDateString
+     * @return string
+     */
+    private function writeAnimalIdAndDate($vsmId, $measurementDateString)
+    {
+        $id = Utils::getNullCheckedArrayValue($vsmId, $this->idByAiindArray);
+        
+        if($id != null) {
+            return $id.'_'.$measurementDateString;
+        } else {
+            return null;
+        }
+    }
+
+
+
+    
+    
+    
+
+    private function parseCSV() {
+        $ignoreFirstLine = $this->csvParsingOptions['ignoreFirstLine'];
+
+        $finder = new Finder();
+        $finder->files()
+            ->in($this->csvParsingOptions['finder_in'])
+            ->name($this->csvParsingOptions['finder_name'])
+        ;
+        foreach ($finder as $file) { $csv = $file; }
+
+        $rows = array();
+        if (($handle = fopen($csv->getRealPath(), "r")) !== FALSE) {
+            $i = 0;
+            while (($data = fgetcsv($handle, null, ";")) !== FALSE) {
+                $i++;
+                if ($ignoreFirstLine && $i == 1) { continue; }
+                $rows[] = $data;
+                gc_collect_cycles();
+            }
+            fclose($handle);
+        }
+
+        return $rows;
+    }
+}
