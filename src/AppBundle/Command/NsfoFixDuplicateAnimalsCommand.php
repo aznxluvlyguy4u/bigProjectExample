5b0e6914d29bc6ef585345d2e1a01d95a297f581
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-10-03 07:04:56 +0200

    WIP Create command to delete duplicate animals
    Former-commit-id: 53065fbf29283b9b0dfbc9f06aa3c3a3383e3ce7

--- src/AppBundle/Command/NsfoFixDuplicateAnimalsCommand.php
+++ src/AppBundle/Command/NsfoFixDuplicateAnimalsCommand.php
@@ -4,7 +4,11 @@ namespace AppBundle\Command;
 
 use AppBundle\Entity\Animal;
 use AppBundle\Entity\AnimalRepository;
+use AppBundle\Entity\AnimalResidence;
+use AppBundle\Entity\BreedCodes;
+use AppBundle\Entity\Weight;
 use AppBundle\Enumerator\GenderType;
+use AppBundle\Migration\BreedCodeReformatter;
 use AppBundle\Util\CommandUtil;
 use AppBundle\Util\GenderChanger;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -16,6 +20,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 class NsfoFixDuplicateAnimalsCommand extends ContainerAwareCommand
 {
     const TITLE = 'Fix Duplicate Animals: Due to Animal Import after AnimalSync around July-Aug 2016';
+    const ID_LIMIT = 611040;
+    const OLD_DATE = '2016-09-22 11';
 
     /** @var ObjectManager $em */
     private $em;
@@ -31,6 +37,12 @@ class NsfoFixDuplicateAnimalsCommand extends ContainerAwareCommand
 
     /** @var GenderChanger */
     private $genderChanger;
+
+    /** @var BreedCodeReformatter */
+    private $breedCodeReformatter;
+
+    /** @var ArrayCollection */
+    private $animalsGroupedByUln;
     
     protected function configure()
     {
@@ -48,6 +60,9 @@ class NsfoFixDuplicateAnimalsCommand extends ContainerAwareCommand
         $this->output = $output;
         $helper = $this->getHelper('question');
         $this->cmdUtil = new CommandUtil($input, $output, $helper);
+
+        /** @var BreedCodeReformatter $breedCodeReformatter */
+        $this->breedCodeReformatter = new BreedCodeReformatter($this->em, false, new ArrayCollection());
 
         /** @var GenderChanger genderChanger */
         $this->genderChanger = new GenderChanger($this->em);
@@ -77,13 +92,13 @@ class NsfoFixDuplicateAnimalsCommand extends ContainerAwareCommand
                 ) d ON d.uln_number = a.uln_number AND d.uln_country_code = a.uln_country_code";
         $ulnResults = $this->em->getConnection()->query($sql)->fetchAll();
 
-        $animalsGroupedByUln = new ArrayCollection();
+        $this->animalsGroupedByUln = new ArrayCollection();
         foreach ($ulnResults as $ulnResult)
         {
             $uln = $ulnResult['uln'];
             $animalId = $ulnResult['id'];
 
-            $animalIds = $animalsGroupedByUln->get($uln);
+            $animalIds = $this->animalsGroupedByUln->get($uln);
 
             if($animalIds == null) {
                 $animalIds = array();
@@ -95,10 +110,8 @@ class NsfoFixDuplicateAnimalsCommand extends ContainerAwareCommand
                 $animalIds[] = $animalId;
             }
 
-            $animalsGroupedByUln->set($uln, $animalIds);
+            $this->animalsGroupedByUln->set($uln, $animalIds);
         }
-
-        return $animalsGroupedByUln;
     }
 
 
@@ -108,16 +121,15 @@ class NsfoFixDuplicateAnimalsCommand extends ContainerAwareCommand
     private function fixDuplicateAnimals()
     {
         $isGetAnimalEntities = true;
-        $animalsGroups = $this->findDuplicateAnimals($isGetAnimalEntities);
+        $this->findDuplicateAnimals($isGetAnimalEntities);
 
         $startUnit = 1;
-        $totalNumberOfUnits = $animalsGroups->count() + $startUnit;
+        $totalNumberOfUnits = $this->animalsGroupedByUln->count() + $startUnit;
         $startMessage = 'Fixing Duplicate Animals';
 
         $this->cmdUtil->setStartTimeAndPrintIt($totalNumberOfUnits, $startUnit, $startMessage);
 
-
-        foreach ($animalsGroups as $animalsGroup)
+        foreach ($this->animalsGroupedByUln as $animalsGroup)
         {
             /** @var Animal $animal1 */
             $animal1 = $animalsGroup[0];
@@ -130,8 +142,15 @@ class NsfoFixDuplicateAnimalsCommand extends ContainerAwareCommand
              * And copy the missing data from the imported animal to the synced animal
              */
 
+            //TODO ONLY PASS animal ID and find/set the animals here
             /* 1. Identify primary animal */
-            if($animal1->getId() < $animal2->getId()) {
+            if($animal1->getId() > self::ID_LIMIT) {
+                $primaryAnimal = $animal1;
+                $secondaryAnimal = $animal2;
+            } elseif($animal2->getId() > self::ID_LIMIT) {
+                $primaryAnimal = $animal2;
+                $secondaryAnimal = $animal1;
+            } elseif($animal1->getId() < $animal2->getId()) {
                 $primaryAnimal = $animal1;
                 $secondaryAnimal = $animal2;
             } else {
@@ -154,14 +173,17 @@ class NsfoFixDuplicateAnimalsCommand extends ContainerAwareCommand
                 
                 */
 
-                if($genderSecondaryAnimal == GenderType::FEMALE) { $this->genderChanger->makeFemale($primaryAnimal); }
-                elseif($genderSecondaryAnimal == GenderType::MALE) { $this->genderChanger->makeMale($primaryAnimal); }
+                if($genderSecondaryAnimal == GenderType::FEMALE) { $primaryAnimal = $this->genderChanger->makeFemale($primaryAnimal); }
+                elseif($genderSecondaryAnimal == GenderType::MALE) { $primaryAnimal = $this->genderChanger->makeMale($primaryAnimal); }
             }
 
             /* 3. */
             $this->mergeValues($primaryAnimal, $secondaryAnimal);
 
-            $this->em->remove($secondaryAnimal);
+            /* 4 Remove unnecessary duplicate */
+            /** @var Animal $secondaryAnimal */
+            $this->removeAnimal($secondaryAnimal);
+
             $this->em->persist($primaryAnimal);
             $this->em->flush();
 
@@ -169,6 +191,44 @@ class NsfoFixDuplicateAnimalsCommand extends ContainerAwareCommand
         }
 
         $this->cmdUtil->setEndTimeAndPrintFinalOverview();
+    }
+
+
+    /**
+     * @param Animal $animal
+     */
+    private function removeAnimal($animal)
+    {
+        /** @var Animal $animal */
+        $animal = $this->em->merge($animal);
+        /** @var BreedCodes $breedCodes */
+        $breedCodes = $animal->getBreedCodes();
+        if($breedCodes != null) {
+            foreach ($breedCodes->getCodes() as $code)
+            {
+                $this->em->remove($code);
+            }
+            $breedCodes->removeAllCodes();
+            $breedCodes->setAnimal(null);
+            $animal->setBreedCodes(null);
+            $this->em->remove($breedCodes);
+        }
+        $sql = "SELECT id FROM breed_codes WHERE animal_id = ".$animal->getId();
+        $breedCodesId = $this->em->getConnection()->query($sql)->fetch()['id'];
+        if($breedCodesId != null) {
+            $sql = "DELETE FROM breed_code WHERE breed_codes_id = ".$breedCodesId;
+            $this->em->getConnection()->exec($sql);
+        }
+
+        $sql = "UPDATE animal SET breed_codes_id = NULL WHERE id = ".$animal->getId();
+        $this->em->getConnection()->exec($sql);
+
+        $sql = "DELETE FROM breed_codes WHERE animal_id = ".$animal->getId();
+        $this->em->getConnection()->exec($sql);
+
+        /** @var Animal $animal */
+        $animal = $this->em->merge($animal);
+        $this->em->remove($animal);
     }
 
 
@@ -210,21 +270,67 @@ class NsfoFixDuplicateAnimalsCommand extends ContainerAwareCommand
             $primaryAnimal->setScrapieGenotype($secondaryAnimal->getScrapieGenotype());
         }
 
-        if($primaryAnimal->getBreedCodes() == null) {
-            $primaryAnimal->setBreedCodes($secondaryAnimal->getBreedCodes());
-            $secondaryAnimal->setBreedCodes(null);
-        }
-
         if($primaryAnimal->getUbnOfBirth() == null) {
             $primaryAnimal->setUbnOfBirth($secondaryAnimal->getUbnOfBirth());
         }
 
-        if($primaryAnimal->getPedigreeRegister() == null) {
+        if($primaryAnimal->getPedigreeRegister() == null && $secondaryAnimal->getPedigreeRegister() != null) {
             $primaryAnimal->setPedigreeRegister($secondaryAnimal->getPedigreeRegister());
         }
 
-        if($primaryAnimal->getMixblupBlock() == null) {
+        if($primaryAnimal->getMixblupBlock() == 2 || $primaryAnimal->getMixblupBlock() == null) {
             $primaryAnimal->setMixblupBlock($secondaryAnimal->getMixblupBlock());
+        }
+
+        /* Generate breed codes */
+        if($primaryAnimal->getBreedCodes() == null) {
+            $animalArray = new ArrayCollection();
+            $animalArray->add($primaryAnimal);
+            $this->breedCodeReformatter->setAnimals($animalArray);
+            $this->breedCodeReformatter->migrate();
+        }
+
+        if($secondaryAnimal->getWeightMeasurements()->count() > 0) {
+            /** @var Weight $weight */
+            foreach ($secondaryAnimal->getWeightMeasurements() as $weight)
+            {
+                $weight->setAnimal($primaryAnimal);
+            }
+        }
+
+        if($secondaryAnimal->getDepartures()->count() > 0) {
+            /** @var Weight $weight */
+            foreach ($secondaryAnimal->getDepartures() as $departure)
+            {
+                $departure->setAnimal($primaryAnimal);
+            }
+        }
+
+        if($secondaryAnimal->getDeaths()->count() > 0) {
+            /** @var Weight $weight */
+            foreach ($secondaryAnimal->getDeaths() as $loss)
+            {
+                $loss->setAnimal($primaryAnimal);
+            }
+        }
+
+
+        if($primaryAnimal->getAnimalResidenceHistory()->count() == 1 && $secondaryAnimal->getAnimalResidenceHistory()->count() > 0) {
+            /** @var AnimalResidence $primaryAnimalResidence */
+            $primaryAnimalResidence = $primaryAnimal->getAnimalResidenceHistory()->first();
+            if($primaryAnimalResidence->getLogDate()->format('Y-m-d_H') == self::OLD_DATE
+            && $primaryAnimalResidence->getStartDate()->format('Y-m-d_H') == self::OLD_DATE
+            && $primaryAnimalResidence->getEndDate() == null
+            ) {
+                $this->em->remove($primaryAnimalResidence);
+            }
+            
+            /** @var AnimalResidence $animalResidence */
+            foreach ($secondaryAnimal->getAnimalResidenceHistory() as $animalResidence) {
+                $animalResidence->setAnimal($primaryAnimal);
+            }
+
+            $primaryAnimal->setAnimalResidenceHistory($secondaryAnimal->getAnimalResidenceHistory());
         }
     }
 
