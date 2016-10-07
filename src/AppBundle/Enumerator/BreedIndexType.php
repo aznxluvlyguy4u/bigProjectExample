945794a100ede5eb6de5e51dc059e59fbc7b32dd
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-10-07 13:33:37 +0200

    Create enumerators for BreedValueIndex values
    Former-commit-id: 350594a6bdc4976ea341c6689fd5be0ae8c118cb

---
+++ src/AppBundle/Enumerator/BreedIndexType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AppBundle\Enumerator;
+
+
+class BreedIndexType
+{
+    const LAMB_MEAT_INDEX = 'LAMB_MEAT_INDEX'; //vleeslamindex
+}
---
+++ src/AppBundle/Enumerator/BreedTrait.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\Enumerator;
+
+
+class BreedTrait
+{
+    const FAT = 'FAT';
+    const GROWTH = 'GROWTH';
+    const MUSCLE_THICKNESS = 'MUSCLE_THICKNESS';
+}
