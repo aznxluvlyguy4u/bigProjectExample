c7675574c07593a0365bc63754cb8cbe382a9ffc
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-10-07 13:19:00 +0200

    ENTITY CHANGE create BreedValueCoefficient
    Former-commit-id: f4ad2f6f6da10071f9178e63f21a87efcaceec40

---
+++ src/AppBundle/Entity/BreedValueCoefficientRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppBundle\Entity;
+use AppBundle\Constant\Constant;
+
+/**
+ * Class BreedValueCoefficient
+ * @package AppBundle\Entity
+ */
+class BreedValueCoefficientRepository extends BaseRepository {
+
+}
