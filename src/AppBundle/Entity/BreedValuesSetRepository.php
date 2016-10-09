05ad7f5df7239acaa8ec4249e0abeba7cafe45f1
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-10-09 02:21:13 +0200

    Change breedValues to correctedBreedValues in repository function
    Former-commit-id: 01b2e219c2b66961315a896c5d35f41bac021642

--- src/AppBundle/Entity/BreedValuesSetRepository.php
+++ src/AppBundle/Entity/BreedValuesSetRepository.php
@@ -13,15 +13,30 @@ use AppBundle\Util\NullChecker;
 class BreedValuesSetRepository extends BaseRepository {
 
     /**
+     * If the breedValues for multiple animals is required,
+     * it is advised to retrieve the geneticBase once and pass the values as a variable,
+     * to minimize calls to the database.
+     * 
      * @param int $animalId
+     * @param int $year
+     * @param GeneticBase $geneticBases
      * @return array
      */
-    public function getBreedValuesWithAccuracies($animalId)
+    public function getBreedValuesCorrectedByGeneticBaseWithAccuracies($animalId, $year, $geneticBases = null)
     {
         $em = $this->getManager();
     
-        $sql = "SELECT * FROM breed_values_set WHERE animal_id = ".$animalId;
+        $sql = "SELECT * FROM breed_values_set WHERE animal_id = ".$animalId." AND EXTRACT(YEAR FROM generation_date) = ".$year;
         $results = $em->getConnection()->query($sql)->fetch();
+
+        if($geneticBases == null) {
+            /** @var GeneticBaseRepository $geneticBaseRepository */
+            $geneticBaseRepository = $this->getManager()->getRepository(GeneticBase::class);
+            $geneticBases = $geneticBaseRepository->getNullCheckedGeneticBases($year);
+        }
+
+        //If there are no genetic bases, then the corrected values cannot be calculated
+        if($geneticBases == null) { return null; }
 
         //Default values
         $muscleThicknessValue = null;
@@ -35,17 +50,17 @@ class BreedValuesSetRepository extends BaseRepository {
         $isGetFormattedAccuracies = false;
 
         if(NullChecker::floatIsNotZero($results['muscle_thickness_reliability'])) {
-            $muscleThicknessValue = $results['muscle_thickness'];
+            $muscleThicknessValue = $results['muscle_thickness'] - $geneticBases->getMuscleThickness();
             $muscleThicknessAccuracy = BreedValueUtil::getAccuracyFromReliability($results['muscle_thickness_reliability'], $isGetFormattedAccuracies);
         }
 
         if(NullChecker::floatIsNotZero($results['growth_reliability'])) {
-            $growthValue = $results['growth'];
+            $growthValue = $results['growth'] - $geneticBases->getGrowth();
             $growthAccuracy = BreedValueUtil::getAccuracyFromReliability($results['growth_reliability'], $isGetFormattedAccuracies);
         }
 
         if(NullChecker::floatIsNotZero($results['fat_reliability'])) {
-            $fatValue = $results['fat'];
+            $fatValue = $results['fat'] - $geneticBases->getFat();
             $fatAccuracy = BreedValueUtil::getAccuracyFromReliability($results['fat_reliability'], $isGetFormattedAccuracies);
         }
 
