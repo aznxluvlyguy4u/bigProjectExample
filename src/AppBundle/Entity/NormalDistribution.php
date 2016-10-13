060d181635d7c5f6dcdf4c78fc73aee359d8d67d
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-10-13 13:31:39 +0200

    Fix NormalDistribution imports
    Former-commit-id: d14211cf705dbc53acab3a6f768c8d59ebfdbc4e

--- src/AppBundle/Entity/NormalDistribution.php
+++ src/AppBundle/Entity/NormalDistribution.php
@@ -3,6 +3,16 @@
 
 namespace AppBundle\Entity;
 
+
+use Doctrine\Common\Collections\ArrayCollection;
+use JMS\Serializer\Annotation\ExclusionPolicy;
+use JMS\Serializer\Annotation\Expose;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\DependencyInjection\Tests\Compiler\A;
+use Symfony\Component\Validator\Constraints as Assert;
+use JMS\Serializer\Annotation as JMS;
+
+
 /**
  * Class NormalDistribution
  * @ORM\Entity(repositoryClass="AppBundle\Entity\NormalDistributionRepository")
