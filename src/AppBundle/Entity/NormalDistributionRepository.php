d8a0991885ccc44bb3089fdac149d593ca8725c7
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-10-13 12:51:42 +0200

    ENTITY CHANGE create NormalDistribution entity
    Former-commit-id: e26c7dc2a1134eaa6e8b381ffb1f07288049f9c2

---
+++ src/AppBundle/Entity/NormalDistributionRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Entity;
+use AppBundle\Util\MathUtil;
+
+/**
+ * Class NormalDistributionRepository
+ * @package AppBundle\Entity
+ */
+class NormalDistributionRepository extends BaseRepository {
+
+
+    /**
+     * NormalDistribution constructor.
+     * @param string $type
+     * @param int $year
+     * @param array $values
+     * @param boolean $isIncludingOnlyAliveAnimals
+     */
+    public function persistFromArray($type, $year, $values, $isIncludingOnlyAliveAnimals)
+    {
+        $mean = array_sum($values) / count($values);
+        $standardDeviation = MathUtil::standardDeviation($values, $mean);
+
+        $normalDistribution = new NormalDistribution($type, $year, $standardDeviation, $mean , $isIncludingOnlyAliveAnimals);
+
+        $this->getManager()->persist($normalDistribution);
+        $this->getManager()->flush();
+    }
+    
+}
