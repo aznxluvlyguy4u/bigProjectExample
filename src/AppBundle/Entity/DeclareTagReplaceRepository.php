880581218a913c19c540b4a2232c325f41fd6509
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-10-18 10:50:18 +0200

    Printout missing ulns in relani-solani files and find their new replacement ulns
    Former-commit-id: 6e5277aaf657e9ca4cede34b295ad63369015bf7

--- src/AppBundle/Entity/DeclareTagReplaceRepository.php
+++ src/AppBundle/Entity/DeclareTagReplaceRepository.php
@@ -7,5 +7,20 @@ namespace AppBundle\Entity;
  * @package AppBundle\Entity
  */
 class DeclareTagReplaceRepository extends BaseRepository {
+    
+    /**
+     * @param $oldReplacedUln
+     */
+    public function getNewReplacementUln($oldReplacedUln)
+    {
+        $sql = "SELECT uln_number_replacement, replace_date
+            FROM declare_tag_replace d
+            INNER JOIN declare_base b ON d.id = b.id
+            WHERE (b.request_state = 'FINISHED' OR b.request_state = 'FINISHED_WITH_WARNING') AND uln_number_to_replace = '".$oldReplacedUln."'
+            ORDER BY replace_date DESC";
+        $result = $this->getManager()->getConnection()->query($sql)->fetch();
 
+        return $result['uln_number_replacement'];
+    }
+    
 }
