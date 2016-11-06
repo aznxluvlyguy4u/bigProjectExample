54b11dad0e7fe4e26d4cdd6a4943f53953a5566d
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-11-07 00:16:25 +0100

    Remove obsolete count functions for Declares in Dashboard
    Former-commit-id: 3383a792aebeb118b9a5b636612be422f8706806

--- src/AppBundle/Component/Count.php
+++ src/AppBundle/Component/Count.php
@@ -6,35 +6,21 @@ namespace AppBundle\Component;
 use AppBundle\Entity\Client;
 use AppBundle\Entity\Company;
 use AppBundle\Entity\Animal;
-use AppBundle\Entity\DeclarationDetail;
-use AppBundle\Entity\DeclareAnimalFlag;
 use AppBundle\Entity\DeclareArrival;
 use AppBundle\Entity\DeclareBirth;
 use AppBundle\Entity\DeclareDepart;
 use AppBundle\Entity\DeclareExport;
 use AppBundle\Entity\DeclareImport;
 use AppBundle\Entity\DeclareLoss;
-use AppBundle\Entity\DeclareMateRepository;
-use AppBundle\Entity\DeclareNsfoBase;
-use AppBundle\Entity\DeclareTagsTransfer;
 use AppBundle\Entity\Location;
 use AppBundle\Entity\Mate;
-use AppBundle\Entity\RetrieveAnimals;
-use AppBundle\Entity\RetrieveCountries;
-use AppBundle\Entity\RetrieveTags;
-use AppBundle\Entity\RetrieveAnimalDetails;
-use AppBundle\Entity\RetrieveUbnDetails;
-use AppBundle\Entity\RevokeDeclaration;
 use AppBundle\Enumerator\AnimalTransferStatus;
 use AppBundle\Enumerator\LiveStockType;
-use AppBundle\Enumerator\RequestStateType;
 use AppBundle\Enumerator\RequestType;
 use AppBundle\Enumerator\RequestTypeNonIR;
-use AppBundle\Enumerator\TagStateType;
 use AppBundle\Util\StringUtil;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\Validator\Constraints\DateTime;
 
 
 /**
@@ -43,21 +29,6 @@ use Symfony\Component\Validator\Constraints\DateTime;
  */
 class Count
 {
-    /**
-     * @param Client $client
-     * @return ArrayCollection
-     */
-    public static function getErrorCountDeclarations(Client $client)
-    {
-        $errorCounts = new ArrayCollection();
-        $errorCounts->set(RequestType::DECLARE_ARRIVAL, Count::getErrorCountArrivalsAndImports($client));
-        $errorCounts->set(RequestType::DECLARE_DEPART, Count::getErrorCountDepartsAndExports($client));
-        $errorCounts->set(RequestType::DECLARE_LOSS, Count::getErrorCountLosses($client));
-        $errorCounts->set(RequestType::DECLARE_BIRTH, Count::getErrorCountBirths($client));
-
-        return $errorCounts;
-    }
-
     /**
      * @param Location $location
      * @return ArrayCollection
@@ -165,283 +136,6 @@ class Count
             return 0;
         }
     }
-        
-
-    /**
-     * @param Client $client
-     * @return int
-     */
-    public static function getErrorCountArrivalsAndImports(Client $client)
-    {
-        return self::getErrorCountArrivals($client) + self::getErrorCountImports($client);
-    }
-
-    /**
-     * @param Location $location
-     * @return int
-     */
-    public static function getErrorCountArrivalsAndImportsPerLocation(Location $location)
-    {
-        return self::getErrorCountArrivalsLocation($location) + self::getErrorCountImportsLocation($location);
-    }
-
-    /**
-     * @param Client $client
-     * @return int
-     */
-    public static function getErrorCountDepartsAndExports(Client $client)
-    {
-        return self::getErrorCountDeparts($client) + self::getErrorCountExports($client);
-    }
-
-    /**
-     * @param Location $location
-     * @return int
-     */
-    public static function getErrorCountDepartsAndExportsPerLocation(Location $location)
-    {
-        return self::getErrorCountDepartsLocation($location) + self::getErrorCountExportsLocation($location);
-    }
-
-
-    /**
-     * @param Client $client
-     * @return int
-     */
-    public static function getErrorCountArrivals(Client $client)
-    {
-        $count = 0;
-
-        foreach($client->getCompanies() as $company){
-            foreach($company->getLocations() as $location){
-                self::getErrorCountArrivalsLocation($location);
-            }
-        }
-
-        return $count;
-    }
-
-    /**
-     * @param Location $location
-     * @return int
-     */
-    public static function getErrorCountArrivalsLocation(Location $location)
-    {
-        $count = 0;
-
-        foreach($location->getArrivals() as $arrival){
-            if(self::countAsErrorResponse($arrival)) {
-                $count++;
-            }
-        }
-
-        return $count;
-    }
-
-    /**
-     * @param Client $client
-     * @return int
-     */
-    public static function getErrorCountImports(Client $client)
-    {
-        $count = 0;
-
-        foreach($client->getCompanies() as $company){
-            foreach($company->getLocations() as $location){
-                self::getErrorCountImportsLocation($location);
-            }
-        }
-
-        return $count;
-    }
-
-    /**
-     * @param Location $location
-     * @return int
-     */
-    public static function getErrorCountImportsLocation(Location $location)
-    {
-        $count = 0;
-
-        foreach($location->getImports() as $import){
-            if(self::countAsErrorResponse($import)) {
-                $count++;
-            }
-        }
-
-        return $count;
-    }
-
-    /**
-     * @param Client $client
-     * @return int
-     */
-    public static function getErrorCountDeparts(Client $client)
-    {
-        $count = 0;
-
-        foreach($client->getCompanies() as $company){
-            foreach($company->getLocations() as $location){
-                self::getErrorCountDepartsLocation($location);
-            }
-        }
-
-        return $count;
-    }
-
-    /**
-     * @param Location $location
-     * @return int
-     */
-    public static function getErrorCountDepartsLocation(Location $location)
-    {
-        $count = 0;
-
-        foreach($location->getDepartures() as $departure){
-            if(self::countAsErrorResponse($departure)) {
-                $count++;
-            }
-        }
-
-        return $count;
-    }
-
-    /**
-     * @param Client $client
-     * @return int
-     */
-    public static function getErrorCountExports(Client $client)
-    {
-        $count = 0;
-
-        foreach($client->getCompanies() as $company){
-            foreach($company->getLocations() as $location){
-                self::getErrorCountExportsLocation($location);
-            }
-        }
-
-        return $count;
-    }
-
-    /**
-     * @param Location $location
-     * @return int
-     */
-    public static function getErrorCountExportsLocation(Location $location)
-    {
-        $count = 0;
-
-        foreach($location->getExports() as $export){
-            if(self::countAsErrorResponse($export)) {
-                $count++;
-            }
-        }
-
-        return $count;
-    }
-
-    /**
-     * @param Client $client
-     * @return int
-     */
-    public static function getErrorCountLosses(Client $client)
-    {
-        $count = 0;
-
-        foreach($client->getCompanies() as $company){
-            foreach($company->getLocations() as $location){
-                self::getErrorCountLossesLocation($location);
-            }
-        }
-
-        return $count;
-    }
-
-    /**
-     * @param Location $location
-     * @return int
-     */
-    public static function getErrorCountLossesLocation(Location $location)
-    {
-        $count = 0;
-
-        foreach($location->getLosses() as $loss){
-            if(self::countAsErrorResponse($loss)) {
-                $count++;
-            }
-        }
-
-        return $count;
-    }
-
-    /**
-     * @param Client $client
-     * @return int
-     */
-    public static function getErrorCountBirths(Client $client)
-    {
-        $count = 0;
-
-        foreach($client->getCompanies() as $company){
-            foreach($company->getLocations() as $location){
-                self::getErrorCountBirthsLocation($location);
-            }
-        }
-
-        return $count;
-    }
-
-    /**
-     * @param Location $location
-     * @return int
-     */
-    public static function getErrorCountBirthsLocation(Location $location)
-    {
-        $count = 0;
-
-        foreach($location->getBirths() as $birth){
-            if(self::countAsErrorResponse($birth)) {
-                $count++;
-            }
-        }
-
-        return $count;
-    }
-
-
-    /**
-     * @param Client $client
-     * @return int
-     */
-    public static function getErrorCountMatings(Client $client)
-    {
-        $count = 0;
-
-        foreach($client->getCompanies() as $company){
-            foreach($company->getLocations() as $location){
-                self::getErrorCountMatingsLocation($location);
-            }
-        }
-
-        return $count;
-    }
-
-    /**
-     * @param Location $location
-     * @return int
-     */
-    public static function getErrorCountMatingsLocation(Location $location)
-    {
-        $count = 0;
-
-        foreach($location->getMatings() as $mate){
-            if(self::countNsfoAsErrorResponse($mate)) {
-                $count++;
-            }
-        }
-
-        return $count;
-    }
     
 
     /**
@@ -456,44 +150,6 @@ class Count
         $sql = "SELECT COUNT(*) FROM tag WHERE owner_id = ".$clientId." AND tag.location_id = ".$locationId." AND tag_status = 'UNASSIGNED'";
         $result = $em->getConnection()->query($sql)->fetch();
         return $result == false || $result == null ? 0 : $result['count'];
-    }
-    
-    
-    /**
-     * @param DeclareArrival|DeclareImport|DeclareExport|DeclareDepart|DeclareBirth|DeclareLoss|DeclareAnimalFlag|DeclarationDetail|DeclareTagsTransfer|RetrieveTags|RevokeDeclaration|RetrieveAnimals|RetrieveAnimals|RetrieveCountries|RetrieveUBNDetails|RetrieveAnimalDetails $declaration
-     * @return bool
-     */
-    private static function countAsErrorResponse($declaration)
-    {
-        if($declaration->getRequestState() == RequestStateType::FAILED) {
-
-            $responses = $declaration->getResponses();
-
-            if(sizeof($responses) > 0) {
-                $lastResponse = $responses[sizeof($responses)-1];
-
-                if($lastResponse->getIsRemovedByUser() == false) {
-                    return true;
-                }
-            }
-
-        }
-
-        return false;
-    }
-
-
-    /**
-     * @param Mate|DeclareNsfoBase $declaration
-     * @return bool
-     */
-    private static function countNsfoAsErrorResponse($declaration)
-    {
-        if($declaration->getRequestState() == RequestStateType::FAILED && !$declaration->getIsHidden()) {
-            return true;
-        } else {
-            return false;
-        }
     }
     
 
