178b1804bc8dbf13cabe727cdfe48e808cc043dd
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-11-09 11:35:53 +0100

    Create BirthType enumerators
    Former-commit-id: d117a7c8a41257210fe6c9d1deb8881b6912ee55

---
+++ src/AppBundle/Enumerator/BirthType.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace AppBundle\Enumerator;
+
+
+class BirthType
+{
+    const NO_HELP = 'NO HELP';
+    const LIGHT_WITH_HELP = 'LIGHT WITH HELP';
+    const NORMAL_WITH_HELP = 'NORMAL WITH HELP';
+    const HEAVY_WITH_HELP = 'HEAVY WITH HELP';
+    const CAESARIAN_LAMB_TOO_BIG = 'CAESARIAN (LAMB TOO BIG)';
+    const CAESARIAN_INSUFFICIENT_ACCESS = 'CAESARIAN (INSUFFICIENT ACCESS)';
+}
