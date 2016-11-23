ef463d2117a0b14303e92bac5a7e8ef926a0bd84
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-11-24 00:05:16 +0100

    Show occupied ulns, incorrect animalOrderNumbers and missing breederNrs in console output
    Former-commit-id: 5577a3f0e52b2247d93f47ce78ebfebae886d9a4

--- src/AppBundle/Migration/AnimalTableMigrator.php
+++ src/AppBundle/Migration/AnimalTableMigrator.php
@@ -587,6 +587,8 @@ class AnimalTableMigrator extends MigratorBase
 
 		$ulnsUpdated = 0;
 		$animalOrderNumbersUpdated = 0;
+		$occupiedUlns = 0;
+		$incorrectOrderNumbers = 0;
 		foreach ($results as $result) {
 			$id = $result['id'];
 			$pedigreeNumber = $result['pedigree_number'];
@@ -612,10 +614,10 @@ class AnimalTableMigrator extends MigratorBase
 					$this->conn->exec($sql);
 					$ulnsUpdated++;
 					$usedUlnNumbers[$ulnNumber] = $ulnNumber;
-				}
-			}
+				} else { $occupiedUlns++; }
+			} else { $incorrectOrderNumbers++; }
 			
-			$this->cmdUtil->advanceProgressBar(1,'ulns updated: '.$ulnsUpdated.' | animalOrderNumbers updated: '.$animalOrderNumbersUpdated);
+			$this->cmdUtil->advanceProgressBar(1,'updated ulns|animalOrderNumbers: '.$ulnsUpdated.'|'.$animalOrderNumbersUpdated.' | ulns occupied|incorrect aOrderNrs: '.$occupiedUlns.'|'.$incorrectOrderNumbers);
 		}
 		$this->cmdUtil->setEndTimeAndPrintFinalOverview();
 		return $usedUlnNumbers;
@@ -646,6 +648,9 @@ class AnimalTableMigrator extends MigratorBase
 		$ulnsUpdated = 0;
 		$animalOrderNumbersUpdated = 0;
 		$ubnsOfBirthUpdated = 0;
+		$occupiedUlns = 0;
+		$missingBreederNumbers = 0;
+		$incorrectOrderNumbers = 0;
 		foreach ($results as $result) {
 			$id = $result['id'];
 			$pedigreeNumber = $result['pedigree_number'];
@@ -668,6 +673,8 @@ class AnimalTableMigrator extends MigratorBase
 						$this->conn->exec($sql);
 						$ulnsUpdated++;
 						$usedUlnNumbers[$ulnNumber] = $ulnNumber;
+					} else {
+						$occupiedUlns++;
 					}
 
 					if($animalOrderNumberInDb == null || $animalOrderNumberInDb == '') {
@@ -676,9 +683,9 @@ class AnimalTableMigrator extends MigratorBase
 						$this->conn->exec($sql);
 						$animalOrderNumbersUpdated++;
 					}
-				}
-			}
-			$this->cmdUtil->advanceProgressBar(1,'ubnsOfBirth updated: '.$ubnsOfBirthUpdated.' | ulns updated: '.$ulnsUpdated.' | animalOrderNumbers updated: '.$animalOrderNumbersUpdated);
+				} else { $incorrectOrderNumbers++; }
+			} else { $missingBreederNumbers++; }
+			$this->cmdUtil->advanceProgressBar(1,'updated ubnsOfBirth|ulns|animalOrderNumbers: '.$ubnsOfBirthUpdated.'|'.$ulnsUpdated.'|'.$animalOrderNumbersUpdated.' | ulns occupied|incorrect aOrderNrs|missingBreederNrs: '.$occupiedUlns.'|'.$incorrectOrderNumbers.'|'.$missingBreederNumbers);
 		}
 		$this->cmdUtil->setEndTimeAndPrintFinalOverview();
 		return $usedUlnNumbers;
