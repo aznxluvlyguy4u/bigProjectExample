d1408dcbd6bb2bc458f33f316cd62817ce4cc77e
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-11-28 23:59:49 +0100

    ENTITY CHANGE create vsmIdGroup entity
    Former-commit-id: 13366df2f109fa4f0b6af9c93d4f12b7811b7425

---
+++ src/AppBundle/Entity/VsmIdGroup.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * Class VsmIdGroup
+ * @package AppBundle\Entity
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\VsmIdGroupRepository")
+ */
+class VsmIdGroup
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank
+     * @ORM\Column(type="string", nullable=false)
+     * @JMS\Type("string")
+     */
+    private $primaryVsmId;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank
+     * @ORM\Column(type="string", nullable=false, unique=true)
+     * @JMS\Type("string")
+     */
+    private $secondaryVsmId;
+
+    public function __construct($primaryVsmId = null, $secondaryVsmId = null)
+    {
+        $this->primaryVsmId = $primaryVsmId;
+        $this->secondaryVsmId = $secondaryVsmId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrimaryVsmId()
+    {
+        return $this->primaryVsmId;
+    }
+
+    /**
+     * @param string $primaryVsmId
+     */
+    public function setPrimaryVsmId($primaryVsmId)
+    {
+        $this->primaryVsmId = $primaryVsmId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSecondaryVsmId()
+    {
+        return $this->secondaryVsmId;
+    }
+
+    /**
+     * @param string $secondaryVsmId
+     */
+    public function setSecondaryVsmId($secondaryVsmId)
+    {
+        $this->secondaryVsmId = $secondaryVsmId;
+    }
+
+
+
+
+
+}
