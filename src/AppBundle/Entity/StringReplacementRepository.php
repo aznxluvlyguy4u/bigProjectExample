88e78b3d4c32bef964a14d1ed2e093d8ce9b04d3
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2016-12-14 13:16:34 +0100

    ENTITY CHANGE create StringReplacement entity
    (cherry picked from commit d02a1f36f30af1cd11692446a1bf8fb4642c3f4b [formerly f899f20f377b62b30bfeba667ab418a0174b88d0])
    
    
    Former-commit-id: 9c71c185165af0de63494b002426dbeedfeaa5a1

---
+++ src/AppBundle/Entity/StringReplacementRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Class StringReplacementRepository
+ * @package AppBundle\Entity
+ */
+class StringReplacementRepository extends BaseRepository {
+
+}
