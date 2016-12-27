fb590c9832e77cb037bcd023afeeae2b4c30b4af
Steven <steven@jongensvantechniek.nl>
2016-12-27 09:24:18 +0100

    Refactor getAnimalByULN, accept either a ULN String or Array with uln_country and uln_number key-values
    Former-commit-id: 2f3a896d777049fb5acd8d36a82e7d2954b8f4fa

--- src/AppBundle/Entity/AnimalRepository.php
+++ src/AppBundle/Entity/AnimalRepository.php
@@ -377,7 +377,7 @@ class AnimalRepository extends BaseRepository
               a.gender, a.date_of_birth, a.is_alive, a.date_of_death, l.ubn, a.id,
               c.is_reveal_historic_animals as is_public, false as current_livestock
             FROM animal_residence r
-              INNER JOIN animal a ON r.animal_id = a.id
+              INNER JOINq animal a ON r.animal_id = a.id
               LEFT JOIN location l ON a.location_id = l.id
               LEFT JOIN company c ON c.id = l.company_id
             WHERE r.location_id = ".$idCurrentLocation.
@@ -455,31 +455,54 @@ class AnimalRepository extends BaseRepository
    * @param string $ulnString
    * @return null|Animal
    */
-  public function getAnimalByUlnString(Client $client, $ulnString)
+  public function getAnimalByUlnString(Client $client, $uln)
   {
-    $uln = Utils::getUlnFromString($ulnString);
+    $ulnCountryCode = null;
+    $ulnNumber = null;
 
-    if ($uln == null) { //invalid input for $ulnString
+    if(!$uln instanceof ArrayCollection) {
+      $uln = Utils::getUlnFromString($uln);
+    }
+
+    $ulnCountryCode = $uln[Constant::ULN_COUNTRY_CODE_NAMESPACE];
+    $ulnNumber = $uln[Constant::ULN_NUMBER_NAMESPACE];
+
+    if(!$ulnCountryCode && !$ulnNumber) {
       return null;
     }
+
+    $locationIds = array();
 
     foreach ($client->getCompanies() as $company) {
       /** @var Location $location */
       foreach ($company->getLocations() as $location) {
-        foreach ($location->getAnimals() as $animal) {
 
-          $showAnimal = $animal->getUlnCountryCode() == $uln[Constant::ULN_COUNTRY_CODE_NAMESPACE]
-              && $animal->getUlnNumber() == $uln[Constant::ULN_NUMBER_NAMESPACE];
-
-          if ($showAnimal) {
-            return $animal;
-          }
-
-        }
+        $locationIds[] = $location->getLocationId();
       }
     }
 
-    return null;
+    $sql = "SELECT a.id, 
+                   a.uln_country_code, 
+                   a.uln_number, 
+                   a.pedigree_country_code, 
+                   a.pedigree_number, 
+                   a.animal_order_number as work_number,
+                   a.gender, 
+                   a.date_of_birth, 
+                   a.is_alive, 
+                   a.is_departed_animal, 
+                   c.last_weight as weight, 
+                   c.weight_measurement_date      
+              FROM animal a
+              LEFT JOIN animal_cache c 
+              ON a.id = c.animal_id
+              WHERE a.is_alive = TRUE 
+              AND (a.transfer_state ISNULL) 
+              AND a.uln_number = '".$ulnNumber."'";
+
+    $result = $this->getManager()->getConnection()->query($sql)->fetchAll();
+
+    return $result;
   }
 
 
@@ -545,8 +568,7 @@ class AnimalRepository extends BaseRepository
       $criteria = Criteria::create()
           ->where(Criteria::expr()->gte('id', $startId))
           ->andWhere(Criteria::expr()->lte('id', $endId))
-          ->orderBy(['id' => Criteria::ASC])
-      ;
+          ->orderBy(['id' => Criteria::ASC]);
 
       return $this->getManager()->getRepository(Animal::class)
                   ->matching($criteria);
