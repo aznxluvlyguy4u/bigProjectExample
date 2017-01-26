32dcaea9a85016bb556d7333b7cf21bcb13c85e2
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-01-26 15:31:30 +0100

    Filter out deactivated Employee/Admin accounts in AdminAuthController endpoints
    Former-commit-id: cf13d076d5005e9cbf26fd4dd0cf1f78d5232ed3

--- src/AppBundle/Entity/EmployeeRepository.php
+++ src/AppBundle/Entity/EmployeeRepository.php
@@ -4,4 +4,16 @@ namespace AppBundle\Entity;
 
 class EmployeeRepository extends BaseRepository {
 
+
+    /**
+     * @param string $emailAddress
+     * @return null|Employee
+     */
+    public function findActiveOneByEmailAddress($emailAddress)
+    {
+        /** @var EmployeeRepository $employeeRepository */
+        $employeeRepository = $this->getManager()->getRepository(Employee::class);
+        return $employeeRepository->findOneBy(["emailAddress"=>$emailAddress, "isActive" => TRUE]);
+    }
+
 }
