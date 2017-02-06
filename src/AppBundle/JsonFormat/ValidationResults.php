6a5427952b32ae384fed8495101fb3cc0a7cb223
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-02-06 20:02:38 +0100

    Create ValidationResults class to store validation results in
    (cherry picked from commit fde4fc1bcf78add0ac1c96102fc04b81e60c27c6 [formerly 5f6341417bba1f2e9fd3e6a0459d3bd635e98128])
    
    
    Former-commit-id: b22d96480e6f96a6ec03afc84e0c80dfd6980795

---
+++ src/AppBundle/JsonFormat/ValidationResults.php
@@ -0,0 +1,102 @@
+<?php
+
+
+namespace AppBundle\JsonFormat;
+
+
+use AppBundle\Util\Validator;
+
+class ValidationResults
+{
+    const VALID_CODE = 200;
+    const ERROR_CODE = 428;
+    const VALID_MESSAGE = 'OK';
+    const ERROR_MESSAGE = 'INVALID INPUT';
+    
+    /** @var boolean */
+    private $isValid;
+
+    /** @var array */
+    private $errors;
+
+    /** @var object */
+    private $validResultObject;
+
+    /**
+     * ValidationResults constructor.
+     * @param bool $isValid
+     */
+    public function __construct($isValid)
+    {
+        $this->isValid = $isValid;
+        $this->errors = [];
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isValid()
+    {
+        return $this->isValid;
+    }
+
+    /**
+     * @param boolean $isValid
+     */
+    public function setIsValid($isValid)
+    {
+        $this->isValid = $isValid;
+    }
+
+    /**
+     * @return array
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    /**
+     * @param array $errors
+     */
+    public function setErrors($errors)
+    {
+        $this->errors = $errors;
+    }
+
+    /**
+     * @param string $errorMessage
+     */
+    public function addError($errorMessage)
+    {
+        $this->errors[] = $errorMessage;
+    }
+
+    /**
+     * @return object
+     */
+    public function getValidResultObject()
+    {
+        return $this->validResultObject;
+    }
+
+    /**
+     * @param object $validResultObject
+     */
+    public function setValidResultObject($validResultObject)
+    {
+        $this->validResultObject = $validResultObject;
+    }
+
+    /**
+     * @return \AppBundle\Component\HttpFoundation\JsonResponse
+     */
+    public function getJsonResponse()
+    {
+        if($this->isValid()){
+            return Validator::createJsonResponse(self::VALID_MESSAGE, self::VALID_CODE);
+        } else {
+            return Validator::createJsonResponse(self::ERROR_MESSAGE, self::ERROR_CODE, $this->errors);
+        }
+    }
+}
