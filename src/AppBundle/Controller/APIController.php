939e86578c26d086c0702ad7009c3de611541837
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-02-15 12:39:30 +0100

    Create APIController function to getDecodedJson from object
    Former-commit-id: d913933facffbb7490c9f7e700e27c1c13c19f59

--- src/AppBundle/Controller/APIController.php
+++ src/AppBundle/Controller/APIController.php
@@ -125,6 +125,19 @@ class APIController extends Controller implements APIControllerInterface
     return $this->serializer;
   }
 
+
+  /**
+   * @param $object
+   * @param $type
+   * @return mixed|array
+   */
+  public function getDecodedJson($object, $type)
+  {
+    $jsonMessage = $this->getSerializer()->serializeToJSON($object, $type);
+    return json_decode($jsonMessage, true);
+  }
+
+
   /**
    * @return RequestMessageBuilder
    */
