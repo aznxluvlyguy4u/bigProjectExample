f16cafebf4c9accd14a296223865aa0930d44105
werner <werner@jongensvantechniek.nl>
2017-04-05 16:58:32 +0200

    Add skeleton functions to InvoiceRuleAPIController
    Former-commit-id: f4de403bcdcef414d6b516fc50e2cd5c7a8b82e7

--- src/AppBundle/Controller/InvoiceRuleAPIController.php
+++ src/AppBundle/Controller/InvoiceRuleAPIController.php
@@ -9,7 +9,66 @@
 namespace AppBundle\Controller;
 
 
-class InvoiceRuleAPIController
+use AppBundle\Component\HttpFoundation\JsonResponse;
+use AppBundle\Constant\Constant;
+use AppBundle\Entity\Invoice;
+use AppBundle\Entity\InvoiceRule;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class InvoiceRuleAPIController extends APIController implements InvoiceRuleAPIControllerInterface
 {
+    /**
+     * @return JsonResponse
+     * @Method("GET")
+     * @Route("")
+     */
+    public function getInvoiceRules()
+    {
+        // TODO: Implement deleteInvoiceRule() method.
+    }
+
+    /**
+     * @param Request $request
+     * @param InvoiceRule $invoiceRule
+     * @Method("PUT")
+     * @Route("/{invoiceRule}")
+     */
+    public function updateInvoiceRule(Request $request, InvoiceRule $invoiceRule)
+    {
+        // TODO: Implement deleteInvoiceRule() method.
+    }
+
+    /**
+     * @param Request $request
+     * @Method("POST")
+     * @Route("")
+     */
+    public function createInvoiceRule(Request $request)
+    {
+        // TODO: Implement deleteInvoiceRule() method.
+    }
+
+    /**
+     * @param InvoiceRule $invoiceRule
+     * @Method("DELETE")
+     * @Route("/{invoiceRule}")
+     */
+    public function deleteInvoiceRule(InvoiceRule $invoiceRule)
+    {
+        // TODO: Implement deleteInvoiceRule() method.
+    }
+
+    /**
+     * @param InvoiceRule $invoiceRule
+     * @param Invoice $invoice
+     * @Method("PUT")
+     * @Route("{invoice}/{invoiceRule}")
+     */
+    public function linkInvoiceRuleToInvoice(InvoiceRule $invoiceRule, Invoice $invoice)
+    {
+        // TODO: Implement linkInvoiceRuleToInvoice() method.
+    }
 
 }
--- src/AppBundle/Controller/InvoiceRuleAPIControllerInterface.php
+++ src/AppBundle/Controller/InvoiceRuleAPIControllerInterface.php
@@ -9,7 +9,16 @@
 namespace AppBundle\Controller;
 
 
-class InvoiceRuleAPIControllerInterface
+use AppBundle\Entity\Invoice;
+use AppBundle\Entity\InvoiceRule;
+use Symfony\Component\HttpFoundation\Request;
+
+interface InvoiceRuleAPIControllerInterface
 {
+    function getInvoiceRules();
+    function updateInvoiceRule(Request $request, InvoiceRule $invoiceRule);
+    function createInvoiceRule(Request $request);
+    function deleteInvoiceRule(InvoiceRule $invoiceRule);
+    function linkInvoiceRuleToInvoice(InvoiceRule $invoiceRule, Invoice $invoice);
 
 }
