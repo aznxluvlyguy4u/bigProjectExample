1319866307b9bdfc7344e3b4736bb0baf8e8902f
werner <werner@jongensvantechniek.nl>
2017-04-07 11:06:00 +0200

    Add the groups INVOICE and INVOICE_SENDER_DETAILS to JMSGroups enumerator class
    Former-commit-id: 91bb8e8bbe43eaf9966f4be2b790a94d1a1dca41

--- src/AppBundle/Enumerator/JMSGroups.php
+++ src/AppBundle/Enumerator/JMSGroups.php
@@ -5,6 +5,8 @@ namespace AppBundle\Enumerator;
 
 class JMSGroups
 {
+    const INVOICE = 'INVOICE';
     const INVOICE_RULE = 'INVOICE_RULE';
     const INVOICE_RULE_TEMPLATE = 'INVOICE_RULE_TEMPLATE';
+    const INVOICE_SENDER_DETAILS = 'INVOICE_SENDER_DETAILS';
 }
