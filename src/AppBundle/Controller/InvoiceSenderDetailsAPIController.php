b526c2d9d5bbd0a01a379460d29fafa1ab641d33
werner <werner@jongensvantechniek.nl>
2017-04-07 11:00:50 +0200

    Create the entity InvoiceSenderDetails, along with it's controller, repository and interface
    Former-commit-id: b3d7771b8506782013918cda24370d6d923abd2f

---
+++ src/AppBundle/Controller/InvoiceSenderDetailsAPIController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: werner
+ * Date: 7-4-17
+ * Time: 10:24
+ */
+
+namespace AppBundle\Controller;
+
+
+class InvoiceSenderDetailsAPIController
+{
+
+}
---
+++ src/AppBundle/Entity/InvoiceSenderDetailsRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: werner
+ * Date: 7-4-17
+ * Time: 10:25
+ */
+
+namespace AppBundle\Entity;
+
+
+class InvoiceSenderDetailsRepository
+{
+
+}
