51646afe3dce2acf5e53487c22513332982b3f0d
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-04-11 01:50:23 +0200

    Create functions to return all breedTypes in English and Dutch
    Former-commit-id: 48a51190e2c5303a628b5f71f795b3c89863b925

--- src/AppBundle/Enumerator/BreedType.php
+++ src/AppBundle/Enumerator/BreedType.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Enumerator;
 
+use AppBundle\Util\Translation;
+
 class BreedType
 {
   const BLIND_FACTOR = "BLIND_FACTOR";
@@ -14,4 +16,36 @@ class BreedType
   const UNDETERMINED = "UNDETERMINED";
   const EN_MANAGEMENT = "EN_MANAGEMENT";
   const EN_BASIS = "EN_BASIS";
+
+  /**
+   * @return array
+   */
+  public static function getAll()
+  {
+    return [
+      self::BLIND_FACTOR => self::BLIND_FACTOR,
+      self::MEAT_LAMB_FATHER => self::MEAT_LAMB_FATHER,
+      self::MEAT_LAMB_MOTHER => self::MEAT_LAMB_MOTHER,
+      self::PARENT_ANIMAL => self::PARENT_ANIMAL,
+      self::PURE_BRED => self::PURE_BRED,
+      self::REGISTER => self::REGISTER,
+      self::SECONDARY_REGISTER => self::SECONDARY_REGISTER,
+      self::UNDETERMINED => self::UNDETERMINED,
+      self::EN_MANAGEMENT => self::EN_MANAGEMENT,
+      self::EN_BASIS => self::EN_BASIS,
+    ];
+  }
+
+
+  /**
+   * @return array
+   */
+  public static function getAllInDutch()
+  {
+    $results = [];
+    foreach (self::getAll() as $key => $item) {
+      $results[$key] = Translation::getDutch($item);
+    }
+    return $results;
+  }
 }
