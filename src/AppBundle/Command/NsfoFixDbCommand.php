a4df22046427a24c472df394c1250b05518cf457
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-04-18 13:16:58 +0200

    Create command to update maxId of all sequences
    Former-commit-id: 5ca4ea850e94129b0d7f51a80db58174b72f4160

--- src/AppBundle/Command/NsfoFixDbCommand.php
+++ src/AppBundle/Command/NsfoFixDbCommand.php
@@ -6,6 +6,7 @@ use AppBundle\Util\CommandUtil;
 use AppBundle\Util\DatabaseDataFixer;
 use AppBundle\Util\DoctrineUtil;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -25,6 +26,9 @@ class NsfoFixDbCommand extends ContainerAwareCommand
 
     /** @var ObjectManager */
     private $em;
+
+    /** @var Connection */
+    private $conn;
     
     protected function configure()
     {
@@ -38,6 +42,7 @@ class NsfoFixDbCommand extends ContainerAwareCommand
     {
         /** @var ObjectManager $em */
         $this->em = $this->getContainer()->get('doctrine')->getManager();
+        $this->conn = $this->em->getConnection();
         $helper = $this->getHelper('question');
         $this->cmdUtil = new CommandUtil($input, $output, $helper);
         $this->output = $output;
@@ -48,19 +53,27 @@ class NsfoFixDbCommand extends ContainerAwareCommand
 
         $option = $this->cmdUtil->generateMultiLineQuestion([
             'Choose option: ', "\n",
-            '1: Fill missing animalOrderNumbers', "\n",
-            '2: Fix incongruent animalOrderNumbers', "\n",
+            '=====================================', "\n",
+            '1: Update MaxId of all sequences', "\n",
+            '=====================================', "\n",
+            '2: Fill missing animalOrderNumbers', "\n",
+            '3: Fix incongruent animalOrderNumbers', "\n",
             'abort (other)', "\n"
         ], self::DEFAULT_OPTION);
 
         switch ($option) {
             case 1:
-                DatabaseDataFixer::fillMissingAnimalOrderNumbers($this->em, $this->cmdUtil);
+                DatabaseDataFixer::updateMaxIdOfAllSequences($this->conn, $this->cmdUtil);
                 $output->writeln('Done!');
                 break;
 
             case 2:
-                DatabaseDataFixer::fixIncongruentAnimalOrderNumbers($this->em, $this->cmdUtil);
+                DatabaseDataFixer::fillMissingAnimalOrderNumbers($this->conn, $this->cmdUtil);
+                $output->writeln('Done!');
+                break;
+
+            case 3:
+                DatabaseDataFixer::fixIncongruentAnimalOrderNumbers($this->conn, $this->cmdUtil);
                 $output->writeln('Done!');
                 break;
 
