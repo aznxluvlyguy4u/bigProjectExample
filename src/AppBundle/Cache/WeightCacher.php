164eeb4c582075f7bb47de28a625aa80a5860c7f
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-04-20 12:18:20 +0200

    Update 8weeks and 20weeks weight animal_cache values
    Former-commit-id: 8d16334150b77e1b4192dc700baa633c9702a52e

--- src/AppBundle/Cache/WeightCacher.php
+++ src/AppBundle/Cache/WeightCacher.php
@@ -3,6 +3,7 @@
 
 namespace AppBundle\Cache;
 use AppBundle\Constant\MeasurementConstant;
+use AppBundle\Enumerator\WeightType;
 use AppBundle\Util\SqlUtil;
 use AppBundle\Util\TimeUtil;
 use Doctrine\DBAL\Connection;
@@ -31,8 +32,10 @@ class WeightCacher
      */
     public static function updateWeights(Connection $conn, $animalIds){
         return
-            self::updateLastWeights($conn, $animalIds)
+              self::updateLastWeights($conn, $animalIds)
             + self::updateBirthWeights($conn, $animalIds)
+            + self::update8WeekWeights($conn, $animalIds)
+            + self::update20WeekWeights($conn, $animalIds)
             ;
     }
 
@@ -165,6 +168,140 @@ class WeightCacher
                             AND animal_cache.animal_id NOT IN (
                                 SELECT w.animal_id
                                 ".$sqlBase."
+                            )
+                            RETURNING 1
+                        )
+                        SELECT COUNT(*) AS count FROM rows;";
+        $updateCount += $conn->query($sqlMakeBlank)->fetch()['count'];
+
+        return $updateCount;
+    }
+
+
+    /**
+     * @param Connection $conn
+     * @param array $animalIds
+     * @return int
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public static function update8WeekWeights(Connection $conn, $animalIds)
+    {
+        return self::updateXWeekWeights($conn, $animalIds, WeightType::EIGHT_WEEKS);
+    }
+
+
+    /**
+     * @param Connection $conn
+     * @param array $animalIds
+     * @return int
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public static function update20WeekWeights(Connection $conn, $animalIds)
+    {
+        return self::updateXWeekWeights($conn, $animalIds, WeightType::TWENTY_WEEKS);
+    }
+
+
+    /**
+     * @param Connection $conn
+     * @param array $animalIds
+     * @param string $weightType
+     * @return int
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    private static function updateXWeekWeights(Connection $conn, $animalIds, $weightType)
+    {
+        $updateCount = 0;
+
+        $animalIdFilterString = "";
+        $animalIdFilterString2 = "";
+        if(is_array($animalIds)) {
+            if(count($animalIds) == 0) {
+                return $updateCount;
+            }
+            else {
+                $animalIdFilterString = " AND (".SqlUtil::getFilterStringByIdsArray($animalIds,'ww.animal_id').")";
+                $animalIdFilterString2 = " AND (".SqlUtil::getFilterStringByIdsArray($animalIds,'animal_id').")";
+            }
+        } elseif($animalIds != null) {
+            return $updateCount;
+        }
+
+
+        switch ($weightType) {
+            case WeightType::EIGHT_WEEKS:
+                $columnName = 'weight_at8weeks';
+                $minWeightValue = MeasurementConstant::WEIGHT_AT_8_WEEKS_MIN_VALUE;
+                $maxWeightValue = MeasurementConstant::WEIGHT_AT_8_WEEKS_MAX_VALUE;
+                $minAgeInDays = MeasurementConstant::WEIGHT_AT_8_WEEKS_MIN_AGE;
+                $maxAgeInDays = MeasurementConstant::WEIGHT_AT_8_WEEKS_MAX_AGE;
+                break;
+
+            case WeightType::TWENTY_WEEKS:
+                $columnName = 'weight_at20weeks';
+                $minWeightValue = MeasurementConstant::WEIGHT_AT_20_WEEKS_MIN_VALUE;
+                $maxWeightValue = MeasurementConstant::WEIGHT_AT_20_WEEKS_MAX_VALUE;
+                $minAgeInDays = MeasurementConstant::WEIGHT_AT_20_WEEKS_MIN_AGE;
+                $maxAgeInDays = MeasurementConstant::WEIGHT_AT_20_WEEKS_MAX_AGE;
+                break;
+
+            default:
+                return $updateCount;
+        }
+
+
+        $sqlBase = "FROM weight w
+             INNER JOIN animal_cache c ON c.animal_id = w.animal_id
+             INNER JOIN (
+                 -- A double GROUP BY + INNER JOIN is used here because there are cases where
+                 -- two weights have equal ABS(measurementDate-targetDate) values.
+                 -- One being ABS(-x) and the other ABS(x).
+                 -- In that case pick the higher weight
+                 SELECT MAX(w.weight) as max_weight, w.animal_id, g.min_days as days_deviation_from_target_days
+                 FROM animal a
+                   INNER JOIN animal_cache c ON c.animal_id = a.id
+                   INNER JOIN weight w ON w.animal_id = a.id
+                   INNER JOIN measurement m ON w.id = m.id
+                   INNER JOIN (
+                                SELECT ww.animal_id,
+                                  MIN(ABS(DATE_PART('day', mm.measurement_date - aa.date_of_birth)-56)) as min_days
+                                FROM animal aa
+                                  INNER JOIN weight ww ON aa.id = ww.animal_id
+                                  INNER JOIN measurement mm ON ww.id = mm.id
+                                WHERE is_active AND ww.is_revoked = false AND ww.is_birth_weight = false
+                                      AND ".$minWeightValue." <= ww.weight
+                                      AND ww.weight <= ".$maxWeightValue."
+                                      AND ".$minAgeInDays." <= DATE_PART('day', mm.measurement_date - aa.date_of_birth)
+                                      AND DATE_PART('day', mm.measurement_date - aa.date_of_birth) <= ".$maxAgeInDays."
+                                      ".$animalIdFilterString."
+                                GROUP BY ww.animal_id
+                              )g ON g.animal_id = a.id
+                 WHERE ABS(DATE_PART('day', m.measurement_date - a.date_of_birth)-56) = g.min_days
+                       AND m.is_active AND w.is_revoked = false AND w.is_birth_weight = false
+                 GROUP BY w.animal_id, min_days
+                 )gg ON max_weight = w.weight AND gg.animal_id = w.animal_id";
+
+        $sqlUpdateToNonBlank = "WITH rows AS (
+                                    UPDATE animal_cache SET $columnName = v.$columnName
+                                    FROM (
+                                             SELECT w.weight as $columnName, w.animal_id
+                                             ".$sqlBase."
+                                                 WHERE (c.$columnName ISNULL OR
+                                                        c.$columnName <> $columnName)
+                                         ) AS v($columnName, animal_id)
+                                    WHERE animal_cache.animal_id = v.animal_id
+                                    RETURNING 1
+                                )
+                                SELECT COUNT(*) AS count FROM rows;";
+        $updateCount = $conn->query($sqlUpdateToNonBlank)->fetch()['count'];
+
+        $sqlMakeBlank = "WITH rows AS (
+                            UPDATE animal_cache SET $columnName = NULL
+                            WHERE animal_cache.$columnName NOTNULL
+                                  ".$animalIdFilterString2."
+                                  AND animal_cache.animal_id NOT IN (
+                                  SELECT w.animal_id
+                                  ".$sqlBase."
                             )
                             RETURNING 1
                         )
