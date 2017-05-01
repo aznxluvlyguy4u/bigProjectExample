f9265a448c3778f72e0276c2c5036462b2e67acd
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-05-01 17:41:31 +0200

    Create BreedCodeUtil functions necessary to calculate the childBreedCode from the breedCodes of the parents
    Former-commit-id: a4884785c7c32e076857b9da1a304278a1e3dfaf

--- src/AppBundle/Util/BreedCodeUtil.php
+++ src/AppBundle/Util/BreedCodeUtil.php
@@ -32,4 +32,138 @@ class BreedCodeUtil
 
         return $result;
     }
+
+
+    /**
+     * @param string $fatherBreedCodeString
+     * @param string $motherBreedCodeString
+     * @param mixed $nullResponse
+     * @return string|null
+     */
+    public static function calculateBreedCodeFromParentBreedCodes($fatherBreedCodeString, $motherBreedCodeString, $nullResponse = null)
+    {
+        $fatherBreedCodeParts = self::getBreedCodePartsFromBreedCodeString($fatherBreedCodeString);
+        $motherBreedCodeParts = self::getBreedCodePartsFromBreedCodeString($motherBreedCodeString);
+
+        if(self::verifySumOfBreedCodeParts($fatherBreedCodeParts) && self::verifySumOfBreedCodeParts($motherBreedCodeParts)) {
+
+            $newBreedCodeParts = self::divideBreedCodeValuesInHalf($fatherBreedCodeParts, $motherBreedCodeParts);
+            $newBreedCode = self::implodeBreedCodeParts($newBreedCodeParts);
+            $childBreedCodeString = StringUtil::getNullAsStringOrWrapInQuotes($newBreedCode);
+
+            return $childBreedCodeString;
+        }
+
+        return $nullResponse;
+    }
+
+
+    /**
+     * @param array $breedCodeParts
+     * @return bool
+     */
+    public static function verifySumOfBreedCodeParts($breedCodeParts)
+    {
+        if(!is_array($breedCodeParts)) { return false; }
+
+        $count = count($breedCodeParts);
+        if($count < 2 || NumberUtil::isOdd($count)) { return false; }
+
+        return array_sum($breedCodeParts) == 100;
+    }
+
+
+    /**
+     * Both parents must have a known breedCode
+     *
+     * @param array $breedCodePartsParent1
+     * @param array $breedCodePartsParent2
+     * @param mixed $nullResponse
+     * @return array
+     */
+    public static function divideBreedCodeValuesInHalf($breedCodePartsParent1, $breedCodePartsParent2, $nullResponse = null)
+    {
+        if(!is_array($breedCodePartsParent1) || !is_array($breedCodePartsParent2)) { return $nullResponse; }
+        if(count($breedCodePartsParent1) == 0 || count($breedCodePartsParent1) == 0) { return $nullResponse; }
+
+        $totalParts = [];
+
+        //Merge values
+        foreach ([$breedCodePartsParent1, $breedCodePartsParent2] as $breedCodeParts) {
+            foreach ($breedCodeParts as $breedCode => $value) {
+                $breedCodeSum = ArrayUtil::get($breedCode, $totalParts, 0) + intval($value);
+                $totalParts[$breedCode] = $breedCodeSum;
+            }
+        }
+
+        //Divide values
+        ksort($totalParts); //Sort the codes alphabetically, secondary
+        arsort($totalParts);//Sort by number value, primary
+        $breedCodes = array_keys($totalParts);
+
+        $roundingError = 100;
+        foreach ($breedCodes as $breedCode) {
+            $number = $totalParts[$breedCode];
+            $newHalvedNumber = self::divideBreedCodeNumberInHalf($number);
+            $totalParts[$breedCode] = $newHalvedNumber;
+            $roundingError = $roundingError - $newHalvedNumber;
+        }
+
+        //Fix rounding errors
+        if($roundingError != 0) {
+            //Add the roundingError to the highest one in the set
+            $totalParts[$breedCodes[0]] = $totalParts[$breedCodes[0]] + $roundingError;
+            ksort($totalParts); //Sort the codes alphabetically, secondary
+            arsort($totalParts);//Sort by number value, primary
+        }
+
+        return $totalParts;
+    }
+
+
+    /**
+     * @param $integer
+     * @param int $partsBy
+     * @return int
+     */
+    public static function divideBreedCodeNumberInHalf($integer, $partsBy = 100)
+    {
+        $halfFloat = floatval(intval($integer))/2;
+        if(!NumberUtil::hasDecimals($halfFloat)) {
+            return intval($halfFloat);
+        } else {
+            return self::roundBreedCodeNumber($halfFloat, $partsBy);
+        }
+    }
+
+
+    /**
+     * @param float|int $number
+     * @param int $partsBy
+     * @return int
+     */
+    public static function roundBreedCodeNumber($number, $partsBy = 100)
+    {
+        if($number > $partsBy/2) {
+            return intval(round($number, 0, PHP_ROUND_HALF_UP));
+        } else {
+            return intval(round($number, 0, PHP_ROUND_HALF_DOWN));
+        }
+    }
+
+
+    /**
+     * @param array $totalParts
+     * @return string
+     */
+    public static function implodeBreedCodeParts($totalParts)
+    {
+        $breedCodes = array_keys($totalParts);
+        //Generate string
+        $breedCodeString = '';
+        foreach ($breedCodes as $breedCode) {
+            $breedCodeString = $breedCodeString.$breedCode.$totalParts[$breedCode];
+        }
+        return $breedCodeString;
+    }
 }
