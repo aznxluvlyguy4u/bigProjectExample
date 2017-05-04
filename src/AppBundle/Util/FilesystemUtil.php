fbbcef3fe88239f13890ba895901eaae90379585
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-05-04 17:50:19 +0200

    Create FileSystemUtil
    Former-commit-id: 37724f03ba07859c83fb760038d5064e3d26e6c6

---
+++ src/AppBundle/Util/FilesystemUtil.php
@@ -0,0 +1,93 @@
+<?php
+
+
+namespace AppBundle\Util;
+
+
+
+use AppBundle\Component\Builder\CsvOptions;
+use Symfony\Component\Filesystem\Filesystem;
+
+class FilesystemUtil
+{
+    /**
+     * @param string $rootFolder
+     * @param CsvOptions $csvOptions
+     * @return bool
+     */
+    public static function csvFileExists($rootFolder, CsvOptions $csvOptions)
+    {
+        return self::filesExist($rootFolder.ltrim($csvOptions->getInputFolder(),'app'), [$csvOptions->getFileName()]);
+    }
+
+    /**
+     * @param string $rootFolder
+     * @param array|string $fileNames
+     * @return bool
+     */
+    public static function filesExist($rootFolder, $fileNames)
+    {
+        $fullPathFileNames = [];
+        if(is_array($fileNames)) {
+            foreach ($fileNames as $fileName) {
+                $fullPathFileNames[] = rtrim($rootFolder,'/').'/'.$fileName;
+            }
+
+        } elseif (is_string($fileNames)) {
+            $fullPathFileNames[] = rtrim($rootFolder,'/').'/'.$fileNames;
+
+        } else {
+            return false;
+        }
+        $fs = new Filesystem();
+        return $fs->exists($fullPathFileNames);
+    }
+
+
+    /**
+     * @param string $folderPath
+     */
+    public static function createFolderPathIfNull($folderPath)
+    {
+        $fs = new Filesystem();
+        if(!$fs->exists($folderPath)) {
+            $fs->mkdir($folderPath);
+        }
+    }
+
+
+    /**
+     * @param string $rootDir
+     * @param CsvOptions $csvOptions
+     */
+    public static function createFolderPathsFromCsvOptionsIfNull($rootDir, $csvOptions)
+    {
+        if(!is_string($rootDir) OR !($csvOptions instanceof CsvOptions)) { return; }
+
+        //removing the app at the end
+        $pathStart = substr($rootDir, 0, strlen($rootDir)-3);
+
+        /* Setup folders */
+        FilesystemUtil::createFolderPathIfNull($pathStart.$csvOptions->getInputFolder());
+        FilesystemUtil::createFolderPathIfNull($pathStart.$csvOptions->getOutputFolder());
+    }
+
+
+    /**
+     * @param string $rootDir
+     * @return string
+     */
+    public static function getWebDirectory($rootDir) { return realpath($rootDir . '/../web'); }
+
+    /**
+     * @param string $rootDir
+     * @return string
+     */
+    public static function getAssetsDirectory($rootDir) { return self::getWebDirectory($rootDir).'/assets'; }
+
+    /**
+     * @param string $rootDir
+     * @return string
+     */
+    public static function getImagesDirectory($rootDir) { return self::getAssetsDirectory($rootDir).'/images'; }
+}
