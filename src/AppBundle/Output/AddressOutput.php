13669b02a0cd83b138516b1e766d7b3c183dec2b
werner <werner@jongensvantechniek.nl>
2017-05-09 16:06:59 +0200

    Add AddressOutput, which returns the array representation of an address object
    Former-commit-id: fc18dc01654392d86b4fd396179803c9392375f0

--- src/AppBundle/Output/AddressOutput.php
+++ src/AppBundle/Output/AddressOutput.php
@@ -9,7 +9,23 @@
 namespace AppBundle\Output;
 
 
+use AppBundle\Entity\Address;
+
 class AddressOutput
 {
-
+    /**
+     * @param Address $address
+     * @return array
+     */
+    public static function createAddressOutput($address) {
+        return array(
+            'id' => $address->getId(),
+            'street_name' => $address->getStreetName(),
+            'address_number' => $address->getAddressNumber(),
+            'address_number_suffix' => $address->getAddressNumberSuffix(),
+            'postal_code' => $address->getPostalCode(),
+            'city' => $address->getCity(),
+            'country' => $address->getCountry()
+        );
+    }
 }
