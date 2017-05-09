a30609a6480f7c9883f25ccce99cf339d368ec53
werner <werner@jongensvantechniek.nl>
2017-05-09 16:10:53 +0200

    Add InvoiceRuleOutput which returns the array representation of an InvoiceRuleTemplate
    Former-commit-id: 83ce732356cfed5b8713d43a9784f5a3e852053c

--- src/AppBundle/Output/InvoiceRuleOutput.php
+++ src/AppBundle/Output/InvoiceRuleOutput.php
@@ -9,7 +9,32 @@
 namespace AppBundle\Output;
 
 
+use AppBundle\Entity\InvoiceRuleTemplate;
+
 class InvoiceRuleOutput
 {
+    public static function createInvoiceRuleOutputList($invoiceRules) {
+        $results = array();
+        foreach ($invoiceRules as $invoiceRule) {
+            $results[] = self::createInvoiceRuleOutput($invoiceRule);
+        }
+        return $results;
+    }
 
+    /**
+     * @param InvoiceRuleTemplate $invoiceRule
+     * @return array
+     */
+    private static function createInvoiceRuleOutput($invoiceRule){
+        return array(
+            'id' => $invoiceRule->getId(),
+            'description' => $invoiceRule->getDescription(),
+            'vat_percentage_rate' => $invoiceRule->getVatPercentageRate(),
+            'price_excl_vat' => $invoiceRule->getPriceExclVat(),
+            'sort_order' => $invoiceRule->getSortOrder(),
+            'category' => $invoiceRule->getCategory(),
+            'locked_version' => $invoiceRule->getLockedVersion()->getId(),
+            'type' => $invoiceRule->getType()
+        );
+    }
 }
