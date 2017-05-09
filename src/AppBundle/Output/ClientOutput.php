d15ea345252cfb04c3e7c87aa6d8705874d93182
werner <werner@jongensvantechniek.nl>
2017-05-09 16:07:35 +0200

    Add ClientOutput which returns the array representation of a Client object
    Former-commit-id: 43de652367a781e07720fe06fbbe12c79b8ffceb

--- src/AppBundle/Output/ClientOutput.php
+++ src/AppBundle/Output/ClientOutput.php
@@ -9,7 +9,17 @@
 namespace AppBundle\Output;
 
 
+use AppBundle\Entity\Client;
+
 class ClientOutput
 {
-
+    public static function createOwnerOutput(Client $client){
+        return array(
+            'person_id' => $client->getPersonId(),
+            'prefix' => $client->getPrefix(),
+            'last_name' => $client->getLastName(),
+            'first_name' => $client->getFirstName(),
+            'email_address' => $client->getEmailAddress(),
+        );
+    }
 }
