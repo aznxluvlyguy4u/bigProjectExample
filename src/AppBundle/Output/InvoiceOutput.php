38211663aa9b244597de4c326348179dd6f936da
werner <werner@jongensvantechniek.nl>
2017-05-09 16:09:33 +0200

    Add InvoiceOutput which returns the array representation of an Invoice
    Former-commit-id: 7a673faea70f924baa1a72461f952ef5b0fe3421

--- src/AppBundle/Output/InvoiceOutput.php
+++ src/AppBundle/Output/InvoiceOutput.php
@@ -9,7 +9,41 @@
 namespace AppBundle\Output;
 
 
+use AppBundle\Entity\Invoice;
+use AppBundle\Entity\InvoiceRuleLocked;
+
 class InvoiceOutput
 {
+    /**
+     * @param $invoices
+     * @return array
+     */
+    public static function createInvoiceOutputList($invoices){
+        $results = array();
+        /** @var Invoice $invoice */
+        foreach ($invoices as $invoice) {
+            if (!$invoice->isDeleted()) {
+                $results[] = self::createInvoiceOutput($invoice);
+            }
+        }
+        return $results;
+    }
 
+    /**
+     * @param Invoice $invoice
+     * @return array
+     */
+    public static function createInvoiceOutput($invoice){
+        return array(
+            'id' => $invoice->getId(),
+            'company_name' => $invoice->getCompanyName(),
+            'company_vat_number' => $invoice->getCompanyVatNumber(),
+            'ubn' => $invoice->getUbn(),
+            'invoice_number' => $invoice->getInvoiceNumber(),
+            'invoice_date' => $invoice->getInvoiceDate(),
+            'company' => CompanyOutput::createCompanyOutputNoInvoices($invoice->getCompany()),
+            'invoice_rules' => InvoiceRuleOutput::createInvoiceRuleOutputList($invoice->getInvoiceRules()),
+            'invoice_rules_locked' => InvoiceRuleLockedOutput::createInvoiceRuleOutputList($invoice->getLockedInvoiceRules())
+        );
+    }
 }
