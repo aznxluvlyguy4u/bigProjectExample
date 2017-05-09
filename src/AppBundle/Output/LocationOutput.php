619c66de81a68efd155e1f5d52ef98ff564d23e3
werner <werner@jongensvantechniek.nl>
2017-05-09 16:11:37 +0200

    Add LocationOutput which returns the array representation of a location only containing data relevant to Invoices
    Former-commit-id: 57f2971ea42cf0a2d932271443a0e2a6936423ee

--- src/AppBundle/Output/LocationOutput.php
+++ src/AppBundle/Output/LocationOutput.php
@@ -9,7 +9,31 @@
 namespace AppBundle\Output;
 
 
+use AppBundle\Entity\Location;
+
 class LocationOutput
 {
+    public static function generateInvoiceLocationArrayList($locations){
+        $results = array();
+        /** @var Location $location */
+        foreach($locations as $location) {
+            if ($location->getIsActive()) {
+                $results[] = self::generateLocationInvoiceArray($location);
+            }
+        }
+        return $results;
+    }
 
+    /**
+     * @param Location $location
+     * @return array
+     */
+    private static function generateLocationInvoiceArray($location) {
+        return array(
+            'id' => $location->getId(),
+            'location_id' => $location->getLocationId(),
+            'ubn' => $location->getUbn(),
+            'location_address' => AddressOutput::createAddressOutput($location->getAddress())
+        );
+    }
 }
