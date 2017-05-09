3f47fb94bf1c701e42ec3abf4664804ca3d8c74b
werner <werner@jongensvantechniek.nl>
2017-05-09 16:10:15 +0200

    Add InvoiceRuleLockedOutput which returns the array representation of a locked invoice rule
    Former-commit-id: 4833249a329c641199626f9c15ebc2caa2e8e8e2

--- src/AppBundle/Output/InvoiceRuleLockedOutput.php
+++ src/AppBundle/Output/InvoiceRuleLockedOutput.php
@@ -2,14 +2,38 @@
 /**
  * Created by IntelliJ IDEA.
  * User: werner
- * Date: 8-5-17
- * Time: 11:45
+ * Date: 14-4-17
+ * Time: 11:33
  */
 
 namespace AppBundle\Output;
 
 
+use AppBundle\Entity\InvoiceRuleLocked;
+
 class InvoiceRuleLockedOutput
 {
+    public static function createInvoiceRuleOutputList($invoiceRules) {
+        $results = array();
+        foreach ($invoiceRules as $invoiceRule) {
+            $results[] = self::createInvoiceRuleOutput($invoiceRule);
+        }
+        return $results;
+    }
 
+    /**
+     * @param InvoiceRuleLocked $invoiceRule
+     * @return array
+     */
+    private static function createInvoiceRuleOutput($invoiceRule){
+        return array(
+            'id' => $invoiceRule->getId(),
+            'description' => $invoiceRule->getDescription(),
+            'vat_percentage_rate' => $invoiceRule->getVatPercentageRate(),
+            'price_excl_vat' => $invoiceRule->getPriceExclVat(),
+            'sort_order' => $invoiceRule->getSortOrder(),
+            'category' => $invoiceRule->getCategory(),
+            'type' => $invoiceRule->getType()
+        );
+    }
 }
