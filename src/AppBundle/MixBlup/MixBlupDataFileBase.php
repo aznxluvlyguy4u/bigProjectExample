d36695637f5c95d0a80f694c6bb707121a612112
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-05-10 17:38:10 +0200

    Generate instruction files for lambmeatindex
    Former-commit-id: c8da9d9364db5ab832954abf0b6557841903fd79

--- src/AppBundle/MixBlup/MixBlupDataFileBase.php
+++ src/AppBundle/MixBlup/MixBlupDataFileBase.php
@@ -3,6 +3,8 @@
 
 namespace AppBundle\MixBlup;
 
+use AppBundle\Enumerator\MixBlupNullFiller;
+use AppBundle\Setting\MixBlupSetting;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\DBAL\Connection;
 
@@ -27,5 +29,53 @@ class MixBlupDataFileBase
     {
         $this->em = $em;
         $this->conn = $em->getConnection();
+    }
+
+
+    /**
+     * @return array
+     */
+    protected function getInstructionFileDefaultEnding()
+    {
+        return [
+            'PEDFILE   '.MixBlupSetting::PEDIGREE_FILENAME.'.txt',
+            ' animal    A !missing '.MixBlupNullFiller::ULN.' #uln',
+            ' sire      A !missing '.MixBlupNullFiller::ULN.' #uln van vader',
+            ' dam       A !missing '.MixBlupNullFiller::ULN.' #uln van moeder',
+            ' block     I !BLOCK', //NOTE it is an integer here
+            ' gender    A !missing '.MixBlupNullFiller::GENDER,
+            ' gebjaar   A !missing '.MixBlupNullFiller::DATE.' #geboortedatum',
+            ' rascode   A !missing '.MixBlupNullFiller::CODE,
+
+            'PARFILE  '.MixBlupSetting::PARFILE_FILENAME,
+
+            'MODEL', //TODO check the MODEL & SOLVING settings
+            ' bw1    ~  herd sex !random comenv G(animal)',
+            ' bw2    ~  herd sex !random comenv G(animal)',
+
+            'SOLVING',
+            'TMPDiR .',
+            'END',
+        ];
+    }
+
+
+    /**
+     * @return array
+     */
+    protected function getStandardizedBreedCodePartsAndHetRecOfInstructionFile()
+    {
+        return [
+            ' CovTE      I !missing '.MixBlupNullFiller::COUNT,  //TE, BT, DK are genetically all the same
+            ' CovCF      I !missing '.MixBlupNullFiller::COUNT,  //Clun Forest
+            ' CovBM      I !missing '.MixBlupNullFiller::COUNT,  //Bleu du Maine
+            ' CovSW      I !missing '.MixBlupNullFiller::COUNT,  //Swifter
+            ' CovNH      I !missing '.MixBlupNullFiller::COUNT,  //Noordhollander
+            ' CovFL      I !missing '.MixBlupNullFiller::COUNT,  //Flevolander
+            ' CovHD      I !missing '.MixBlupNullFiller::COUNT,  //Hampshire Down
+            ' CovOV      I !missing '.MixBlupNullFiller::COUNT,  //other  (NN means unknown, also include it here)
+            ' CovHet     T !missing '.MixBlupNullFiller::HETEROSIS.' #Heterosis van het dier',
+            ' CovRec     T !missing '.MixBlupNullFiller::RECOMBINATION.' #Recombinatie van het dier',
+        ];
     }
 }
