b965c2cc657bbf164ef7138e04e4649a3ac75e67
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-05-11 14:36:48 +0200

    Create MixBlupInstructionFile constants class
    Former-commit-id: 5693965081631573ecb111f8790b665baf2d5e7e

---
+++ src/AppBundle/Setting/MixBlupInstructionFile.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace AppBundle\Setting;
+
+/**
+ * Class MixBlupInstructionFile
+ * @package AppBundle\Setting
+ */
+class MixBlupInstructionFile
+{
+    const EXTERIOR_LEG_WORK = 'InpExtBeenw.txt';
+    const EXTERIOR_MUSCULARITY = 'InpExtBesp.txt';
+    const EXTERIOR_PROPORTION = 'InpExtEvenr.txt';
+    const EXTERIOR_SKULL = 'InpExtKop.txt';
+    const EXTERIOR_PROGRESS = 'InpExtOntw.txt';
+    const EXTERIOR_TYPE = 'InpExtType.txt';
+    const BIRTH_PROGRESS = 'InpGeboorte.txt';
+    const TAIL_LENGTH = 'InpStaartLen.txt';
+    const LAMB_MEAT = 'InpVleeslam.txt';
+    const FERTILITY = 'InpVruchtb.txt';
+    const WORM_RESISTANCE = 'InpWormRes.txt';
+}
