33f8690ad573176ac798d89b8420e9864f7d4291
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-05-11 15:47:36 +0200

    Write lambMeatIndex and Reproduction instruction files based on correct formatting and filenames
    Former-commit-id: 859bc3220557ef2bfd726afaaec47ef6b2feb61f

--- src/AppBundle/MixBlup/LambMeatIndexDataFile.php
+++ src/AppBundle/MixBlup/LambMeatIndexDataFile.php
@@ -4,6 +4,7 @@
 namespace AppBundle\MixBlup;
 
 use AppBundle\Enumerator\MixBlupNullFiller;
+use AppBundle\Setting\MixBlupInstructionFile;
 use AppBundle\Setting\MixBlupSetting;
 use AppBundle\Util\ArrayUtil;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -21,17 +22,19 @@ class LambMeatIndexDataFile extends MixBlupDataFileBase implements MixBlupDataFi
      */
     public function __construct(ObjectManager $em, $outputFolderPath)
     {
-        parent::__construct($em, outputFolderPath);
+        parent::__construct($em, $outputFolderPath);
     }
 
 
     /**
+     * @param array $customRecords
+     * @param string $fileType
      * @return array
      */
-    private function generateTestAttributeInstructionFile()
+    private function generateTestAttributeInstructionFile(array $customRecords = [], $fileType = 'vleeslamkenmerken')
     {
         $start = [
-            'TITEL   schapen fokwaarde berekening vleeslamkenmerken',
+            'TITEL   schapen fokwaarde berekening '.$fileType,
             'DATAFILE  '.MixBlupSetting::DATA_FILENAME_PREFIX.MixBlupSetting::LAMB_MEAT_INDEX.'.txt',
             ' ID         A !missing '.MixBlupNullFiller::ULN.' #uln',  //uln
             ' IDM        A !missing '.MixBlupNullFiller::ULN.' #uln van moeder',  //uln of mother
@@ -45,19 +48,64 @@ class LambMeatIndexDataFile extends MixBlupDataFileBase implements MixBlupDataFi
             ' Zling      I !missing '.MixBlupNullFiller::COUNT.' #zooggrootte',
             ' LeeftScan  I !missing '.MixBlupNullFiller::COUNT.' #op moment van meting in dagen', //age of animal on measurementDate in days
             ' GewScan    T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE.' #gewicht bij scannen',   //weight at measurementDate
+        ];
+
+        if($customRecords == []) {
+            //Insert all records as default
+            $customRecords = [
+                ' GewGeb     T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE.' #geboortegewicht',   //weight at birth
+                ' StaartLen  T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE.' #staartlengte', //tailLength
+                ' Gew08wk    T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE.' #8 weken gewichtmeting', //weight measurement at 8 weeks
+                ' Gew20wk    T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE.' #20 weken gewichtmeting', //weight measurement at 20 weeks
+                ' Vetd01     T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE,
+                ' Vetd02     T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE,
+                ' Vetd03     T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE,
+                ' Spierd     T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE.' #spierdikte',
+            ];
+        }
+
+        $lastDataRecords = [
+            ' Bedrijf    I !missing '.MixBlupNullFiller::UBN.' #ubn van geboorte',
+        ];
+        
+        return ArrayUtil::concatArrayValues([
+            $start,
+            $this->getStandardizedBreedCodePartsAndHetRecOfInstructionFile(),
+            $middle,
+            $customRecords,
+            $lastDataRecords,
+            $this->getInstructionFileDefaultEnding()
+        ]);
+    }
+
+
+    /**
+     * @return array
+     */
+    private function generateLambMeatInstructionFile()
+    {
+        $customRecords = [
             ' GewGeb     T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE.' #geboortegewicht',   //weight at birth
-            ' StaartLen  T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE.' #staartlengte', //tailLength
             ' Gew08wk    T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE.' #8 weken gewichtmeting', //weight measurement at 8 weeks
             ' Gew20wk    T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE.' #20 weken gewichtmeting', //weight measurement at 20 weeks
             ' Vetd01     T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE,
             ' Vetd02     T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE,
             ' Vetd03     T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE,
             ' Spierd     T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE.' #spierdikte',
-            ' Bedrijf    I !missing '.MixBlupNullFiller::UBN.' #ubn van geboorte',
         ];
-        
-        return ArrayUtil::concatArrayValues([$start, $this->getStandardizedBreedCodePartsAndHetRecOfInstructionFile(),
-            $middle, $this->getInstructionFileDefaultEnding()]);
+        return $this->generateTestAttributeInstructionFile($customRecords, 'vleeslamkenmerken');
+    }
+
+
+    /**
+     * @return array
+     */
+    private function generateTailLengthInstructionFile()
+    {
+        $customRecords = [
+            ' StaartLen  T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE.' #staartlengte', //tailLength
+        ];
+        return $this->generateTestAttributeInstructionFile($customRecords, 'staartlengte');
     }
     
     
@@ -66,7 +114,10 @@ class LambMeatIndexDataFile extends MixBlupDataFileBase implements MixBlupDataFi
      */
     function generateInstructionFiles()
     {
-        return [$this->generateTestAttributeInstructionFile()];
+        return [
+            MixBlupInstructionFile::LAMB_MEAT => $this->generateLambMeatInstructionFile(),
+            MixBlupInstructionFile::TAIL_LENGTH => $this->generateTailLengthInstructionFile(),
+        ];
     }
 
     /**
@@ -85,13 +136,7 @@ class LambMeatIndexDataFile extends MixBlupDataFileBase implements MixBlupDataFi
         // TODO: Implement generatePedigreeFile() method.
     }
 
-    /**
-     * @inheritDoc
-     */
-    function write()
-    {
-        // TODO: Implement write() method.
-    }
+
 
 
 }
--- src/AppBundle/MixBlup/ReproductionDataFile.php
+++ src/AppBundle/MixBlup/ReproductionDataFile.php
@@ -27,19 +27,39 @@ class ReproductionDataFile extends MixBlupDataFileBase implements MixBlupDataFil
 
 
     /**
-     * @param array $middleRecords
+     * @param array $customRecords
+     * @param string $titleType
      * @return array
      */
-    private function reproductionInstructionFileBase(array $middleRecords = [])
+    private function reproductionInstructionFileBase(array $customRecords = [], $titleType = 'reproductiekenmerken')
     {
         $start = [
-            'TITEL   schapen fokwaarde berekening exterieurkenmerken',
+            'TITEL   schapen fokwaarde berekening '.$titleType,
             'DATAFILE  '.MixBlupSetting::DATA_FILENAME_PREFIX.MixBlupSetting::EXTERIOR.'.txt',
             ' ID         A !missing '.MixBlupNullFiller::ULN.' #uln van ooi of moeder',  //uln
             ' Leeft      I !missing '.MixBlupNullFiller::COUNT.' #leeftijd ooi in jaren', //age of ewe in years
             ' Sekse      A !missing '.MixBlupNullFiller::GENDER,  //ram/ooi/N_B
             ' JaarBedr   A !missing '.MixBlupNullFiller::GROUP.' #jaar en ubn van geboorte', //year and ubn of birth
         ];
+
+        if($customRecords == []) {
+            //By default include all records
+            $customRecords = [
+                ' HetLam     T !missing '.MixBlupNullFiller::HETEROSIS.' #Heterosis lam of worp', //Heterosis of offspring/litter
+                ' RecLam     T !missing '.MixBlupNullFiller::RECOMBINATION.' #Recombinatie lam of worp', //Recombination of offspring/litter
+                ' CovTE_M    I !missing '.MixBlupNullFiller::COUNT.' #Rasdeel TE van moeder', //BreedCode part TE of mother //TODO definition still unclear
+                ' Inductie   I !missing '.MixBlupNullFiller::PMSG.' #Bronstinduction 0=Ja, 1=Nee', //pmsg value in mate 0=FALSE, 1=TRUE
+                ' PermMil    A !missing '.MixBlupNullFiller::ULN.' #Permanent milieu is identiek aan uln van ooi of moeder',
+                ' IDM        A !missing '.MixBlupNullFiller::ULN.' #Het unieke diernummer van de moeder', //TODO definition still unclear
+                ' TotGeb     I !missing '.MixBlupNullFiller::COUNT.' #Totaal geboren lammeren in de worp', //bornAliveCount in litter
+                ' DoodGeb    I !missing '.MixBlupNullFiller::COUNT.' #Doodgeboren lammeren in de worp', //stillbornCount in litter
+                ' GewGeb     T !missing '.MixBlupNullFiller::MEASUREMENT_VALUE.' #Geboortegewicht', //birthWeight
+                ' GebGemak   I !missing '.MixBlupNullFiller::BIRTH_PROGRESS.' #Geboortegemak', //birthProgress from 0 to 4
+                ' DrTijd     I !missing '.MixBlupNullFiller::AGE.' #Draagtijd', //gestationPeriod in litter
+                ' TusLamT    I !missing '.MixBlupNullFiller::AGE.' #Tussenlamtijd', //birthInterval in litter
+                ' Vroeg      I !missing '.MixBlupNullFiller::PRECOCIOUS.' #Geworpen in eerste levensjaar', //GaveBirthAsOneYearOld 0=FALSE, 1=TRUE
+            ];
+        }
 
         $lastDataRecords = [
             ' Bedrijf    I !missing '.MixBlupNullFiller::UBN.' #ubn van geboorte',
@@ -48,7 +68,7 @@ class ReproductionDataFile extends MixBlupDataFileBase implements MixBlupDataFil
         return ArrayUtil::concatArrayValues([
             $start,
             $this->getStandardizedBreedCodePartsAndHetRecOfInstructionFile(),
-            $middleRecords,
+            $customRecords,
             $lastDataRecords,
             $this->getInstructionFileDefaultEnding()
         ]);
@@ -69,7 +89,7 @@ class ReproductionDataFile extends MixBlupDataFileBase implements MixBlupDataFil
             ' DoodGeb    I !missing '.MixBlupNullFiller::COUNT.' #Doodgeboren lammeren in de worp', //stillbornCount in litter
             ' TusLamT    I !missing '.MixBlupNullFiller::AGE.' #Tussenlamtijd', //birthInterval in litter
         ];
-        return $this->reproductionInstructionFileBase($middleRecords);
+        return $this->reproductionInstructionFileBase($middleRecords, 'worpgrootte');
     }
 
 
@@ -87,7 +107,7 @@ class ReproductionDataFile extends MixBlupDataFileBase implements MixBlupDataFil
             ' GebGemak   I !missing '.MixBlupNullFiller::BIRTH_PROGRESS.' #Geboortegemak', //birthProgress from 0 to 4
             ' DrTijd     I !missing '.MixBlupNullFiller::AGE.' #Draagtijd', //gestationPeriod in litter
         ];
-        return $this->reproductionInstructionFileBase($middleRecords);
+        return $this->reproductionInstructionFileBase($middleRecords, 'geboorteverloop');
     }
 
 
@@ -99,7 +119,7 @@ class ReproductionDataFile extends MixBlupDataFileBase implements MixBlupDataFil
         $middleRecords = [
             ' Vroeg      I !missing '.MixBlupNullFiller::PRECOCIOUS.' #Geworpen in eerste levensjaar', //GaveBirthAsOneYearOld 0=FALSE, 1=TRUE
         ];
-        return $this->reproductionInstructionFileBase($middleRecords);
+        return $this->reproductionInstructionFileBase($middleRecords, 'vroegrijpheid');
     }
 
 
