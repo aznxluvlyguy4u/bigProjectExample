e6d54abdfd40fae040c7342b4496326635b1dc89
werner <werner@jongensvantechniek.nl>
2017-05-12 14:59:03 +0200

    Add the mollie API as a dependency
    Former-commit-id: 41a3b40410f43598689f331b0e0a9167e2214671

---
+++ src/AppBundle/Controller/ClientInvoiceAPIController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: werner
+ * Date: 11-5-17
+ * Time: 14:06
+ */
+
+namespace AppBundle\Controller;
+
+
+class ClientInvoiceAPIController
+{
+
+}
---
+++ src/AppBundle/Controller/MollieAPIController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: werner
+ * Date: 12-5-17
+ * Time: 14:58
+ */
+
+namespace AppBundle\Controller;
+
+
+class MollieAPIController
+{
+
+}
