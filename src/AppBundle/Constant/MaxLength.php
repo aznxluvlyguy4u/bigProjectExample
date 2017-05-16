fc653df7d46926bdaef026083cced86fd505bca8
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-05-16 16:43:14 +0200

    Create MaxLength constant class
    Former-commit-id: 1a1b1b585d8b93bd1af48a7ea89779dc772f4ddf

---
+++ src/AppBundle/Constant/MaxLength.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace AppBundle\Constant;
+
+
+class MaxLength
+{
+    const ULN = 14;
+    const DATE = 10;
+    const EXTERIOR_VALUE = 3; //includes null value
+    const EXTERIOR_DECIMAL_VALUE = 4; //includes comma and one decimal
+}
