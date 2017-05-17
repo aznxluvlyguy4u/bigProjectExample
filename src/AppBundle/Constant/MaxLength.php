a7ba3f1452699a5ae871bb5524ec87be151cac2a
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-05-17 11:47:04 +0200

    Add MaxLength columnWidth constants necesary for ExteriorDataFile and DataFileBase
    Former-commit-id: 8569e444c22cb47b36ce80a0c36ed3abc55b4e99

--- src/AppBundle/Constant/MaxLength.php
+++ src/AppBundle/Constant/MaxLength.php
@@ -10,4 +10,10 @@ class MaxLength
     const DATE = 10;
     const EXTERIOR_VALUE = 3; //includes null value
     const EXTERIOR_DECIMAL_VALUE = 4; //includes comma and one decimal
+    const BREED_CODE_PART_BY_8_PARTS = 1;
+    const HETEROSIS_AND_RECOMBINATION = 5;
+    const NSFO_INSPECTOR = 7;
+    const LITTER_GROUP = 17;
+    const YEAR = 4;
+    const VALID_GENDER = 3; //Ram/Ewe
 }
