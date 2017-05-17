8ac3af021c8e659e5af57f26501aac1a4aa18c41
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-05-17 11:52:59 +0200

    Generate data for ExteriorDataFile
    Former-commit-id: 81ad0dab22312663eb6d391e2622184b282e0e58

--- src/AppBundle/MixBlup/ExteriorDataFile.php
+++ src/AppBundle/MixBlup/ExteriorDataFile.php
@@ -3,6 +3,14 @@
 
 namespace AppBundle\MixBlup;
 
+use AppBundle\Component\Utils;
+use AppBundle\Constant\JsonInputConstant;
+use AppBundle\Constant\MaxLength;
+use AppBundle\Enumerator\ExteriorKind;
+use AppBundle\Enumerator\GenderType;
+use AppBundle\Setting\MixBlupSetting;
+use AppBundle\Util\CsvWriterUtil;
+use AppBundle\Util\Translation;
 use Doctrine\DBAL\Connection;
 
 /**
@@ -17,10 +25,264 @@ class ExteriorDataFile extends MixBlupDataFileBase implements MixBlupDataFileInt
      */
     static function generateDataFile(Connection $conn)
     {
-        // TODO: Implement generateDataFile() method.
-        return [];
+        $dynamicColumnWidths = self::dynamicColumnWidths($conn);
+
+        $records = [];
+        foreach (self::getDataBySql($conn) as $data)
+        {
+            /*
+             * null check validation for these values is done in the sql query
+             * uln, gender, litter_group, year_and_ubn_of_birth, breed_code, ubn_of_birth, kind
+             */
+
+            $parsedBreedCode = self::parseBreedCode($data);
+            //Only allow valid breedCodes
+            if(!$parsedBreedCode) { continue; }
+
+            if($data[JsonInputConstant::HETEROSIS] == null
+                || $data[JsonInputConstant::RECOMBINATION] == null) {
+                //TODO
+            }
+
+            $record =
+            self::getFormattedUln($data).
+            self::getFormattedGenderFromType($data).
+            self::getFormattedYearAndUbnOfBirth($data, $dynamicColumnWidths[JsonInputConstant::YEAR_AND_UBN_OF_BIRTH]).
+            self::getFormattedLitterGroup($data).
+            self::getFormattedNsfoInspectorCode($data).
+            $parsedBreedCode.
+            self::getFormattedHeterosis($data).
+            self::getFormattedRecombination($data).
+            self::formatMuscularityFemaleVG($data).
+            self::formatMaleVGExteriorValues($data).
+            self::formatDFExteriorValues($data).
+            self::formatLinearExteriorValues().
+            self::getUbnOfBirthAsLastColumnValue($data);
+
+            $records[] = $record;
+        }
+
+        return $records;
     }
-    
-    
-    
+
+
+    /**
+     * @param array $data
+     * @return string
+     */
+    private static function formatMuscularityFemaleVG(array $data)
+    {
+        //null check validation for these values is done in the sql query
+        $kind = $data[JsonInputConstant::KIND];
+        $gender = self::getTranslatedGenderFromType($data);
+        if($kind == ExteriorKind::VG_ && $gender == Translation::NL_EWE) {
+            $muscularity = self::formatExteriorValue($data, JsonInputConstant::MUSCULARITY);
+        } else {
+            $muscularity = self::formattedNullExteriorValue();
+        }
+        return $muscularity;
+    }
+
+
+    /**
+     * @param array $data
+     * @return string
+     */
+    private static function formatMaleVGExteriorValues(array $data)
+    {
+        return self::formatExteriorValuesSet($data, [ExteriorKind::VG_], true);
+    }
+
+
+    /**
+     * @param array $data
+     * @return string
+     */
+    private static function formatDFExteriorValues(array $data)
+    {
+        return self::formatExteriorValuesSet($data, [ExteriorKind::DF_, ExteriorKind::DD_, ExteriorKind::HK_], false);
+    }
+
+
+    /**
+     * @param array $data
+     * @param array $allowedKinds
+     * @param bool $onlyMale
+     * @return string
+     */
+    private static function formatExteriorValuesSet(array $data, array $allowedKinds, $onlyMale = false)
+    {
+        //null check validation for these values is done in the sql query
+        $kind = $data[JsonInputConstant::KIND];
+
+        $isGenderAllowed = true;
+        if($onlyMale) {
+            $gender = self::getTranslatedGenderFromType($data);
+            $isGenderAllowed = $gender == Translation::NL_RAM;
+        }
+
+        if(in_array($kind, $allowedKinds) && $isGenderAllowed) {
+            $skull = self::formatExteriorValue($data, "skull");
+            $progress = self::formatExteriorValue($data, "progress");
+            $muscularity =self::formatExteriorValue($data, "muscularity");
+            $proportion = self::formatExteriorValue($data, "proportion");
+            $exteriorType = self::formatExteriorValue($data, "exterior_type");
+            $legWork = self::formatExteriorValue($data, "leg_work");
+            $fur = self::formatExteriorValue($data, "fur");
+            $generalAppearance = self::formatExteriorValue($data, "general_appearance");
+            $height = self::formatExteriorDecimalValue($data, "height");
+            $breastDepth = self::formatExteriorDecimalValue($data, "breast_depth");
+            $torsoLength = self::formatExteriorDecimalValue($data, "torso_length");
+        } else {
+            $skull = self::formattedNullExteriorValue();
+            $progress = self::formattedNullExteriorValue();
+            $muscularity = self::formattedNullExteriorValue();
+            $proportion = self::formattedNullExteriorValue();
+            $exteriorType = self::formattedNullExteriorValue();
+            $legWork = self::formattedNullExteriorValue();
+            $fur = self::formattedNullExteriorValue();
+            $generalAppearance = self::formattedNullExteriorValue();
+            $height = self::formattedNullExteriorDecimalValue();
+            $breastDepth = self::formattedNullExteriorDecimalValue();
+            $torsoLength = self::formattedNullExteriorDecimalValue();
+        }
+
+        return $skull.$progress.$muscularity.$proportion.$exteriorType.
+        $legWork.$fur.$generalAppearance.$height.$breastDepth.$torsoLength;
+    }
+
+
+    /**
+     * @return string
+     */
+    private static function formatLinearExteriorValues()
+    {
+        //At the moment all linearExteriorValues are null
+        return
+            self::formattedNullExteriorValue()
+            .self::formattedNullExteriorValue()
+            .self::formattedNullExteriorValue()
+            .self::formattedNullExteriorValue()
+            .self::formattedNullExteriorValue()
+            .self::formattedNullExteriorValue()
+            .self::formattedNullExteriorValue()
+            .self::formattedNullExteriorValue()
+            .self::formattedNullExteriorValue()
+            .self::formattedNullExteriorValue()
+            .self::getFormattedLinearNsfoInspectorCode([JsonInputConstant::LINEAR_INSPECTOR_CODE => null]);
+    }
+
+
+    /**
+     * @param Connection $conn
+     * @return array
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    private static function getDataBySql(Connection $conn)
+    {
+        $sql = "SELECT
+                  CONCAT(a.uln_country_code, a.uln_number) as uln,
+                  a.type,
+                  CONCAT(DATE_PART('year', a.date_of_birth),'_', a.ubn_of_birth) as year_and_ubn_of_birth,
+                  CONCAT(mom.uln_country_code, mom.uln_number,'_', LPAD(CAST(l.litter_ordinal AS TEXT), 2, '0')) as litter_group,
+                  a.breed_code,
+                  i.inspector_code,
+                  a.ubn_of_birth,
+                  a.heterosis,
+                  a.recombination,
+                  skull, muscularity, proportion, progress, exterior_type, leg_work, fur,
+                  general_appearance, height, breast_depth, torso_length, kind
+                FROM exterior x
+                  INNER JOIN measurement m ON m.id = x.id
+                  INNER JOIN animal a ON a.id = x.animal_id
+                  INNER JOIN animal mom ON mom.id = a.parent_mother_id
+                  LEFT JOIN inspector i ON i.id = m.inspector_id
+                  LEFT JOIN litter l ON l.id = a.litter_id
+                WHERE m.is_active AND DATE_PART('year', NOW()) - DATE_PART('year', measurement_date) <= 
+                  ".MixBlupSetting::MEASUREMENTS_FROM_LAST_AMOUNT_OF_YEARS."
+                  AND a.gender <> '".GenderType::NEUTER."'
+                  AND a.date_of_birth NOTNULL AND a.ubn_of_birth NOTNULL AND a.uln_number NOTNULL
+                  AND a.breed_code NOTNULL AND m.measurement_date NOTNULL
+                  --AND a.heterosis NOTNULL AND a.recombination NOTNULL --CHECK IF NULLCHECK NECESSARY OR NOT
+                  AND m.measurement_date <= NOW()
+                  AND (
+                        -- 1 VG FEMALE
+                        (
+                          a.gender = '".GenderType::FEMALE."' AND x.kind = '".ExteriorKind::VG_."'
+                          AND 69 <= x.muscularity OR x.muscularity <= 99
+                        )
+                        OR
+                        -- 2 VG MALE
+                        (
+                          a.gender = '".GenderType::MALE."' AND x.kind = '".ExteriorKind::VG_."'
+                        )
+                        OR
+                        -- 3 DF/DD/HK
+                        (
+                          x.kind = '".ExteriorKind::DD_."' OR x.kind = '".ExteriorKind::DF_."' OR x.kind = '".ExteriorKind::HK_."'
+                        )
+                      )";
+        return $conn->query($sql)->fetchAll();
+    }
+
+
+    /**
+     * @param Connection $conn
+     * @return array
+     */
+    public static function dynamicColumnWidths(Connection $conn)
+    {
+        $columns = [
+            'ubn' => 'location',
+        ];
+
+        $maxColumnWidths = CsvWriterUtil::maxStringLenghts($conn, $columns);
+        
+        return [
+                  "year_and_ubn_of_birth" => MaxLength::YEAR+strlen('_')+$maxColumnWidths['ubn'],
+        ];
+    }
+
+
+    /**
+     * @param $array
+     * @param $key
+     * @param int $maxLength
+     * @return string
+     */
+    private static function formatExteriorValue($array, $key, $maxLength = MaxLength::EXTERIOR_VALUE)
+    {
+        $filledExteriorValue = Utils::fillZero($array[$key], ExteriorInstructionFiles::MISSING_REPLACEMENT);
+        return CsvWriterUtil::pad($filledExteriorValue, $maxLength);
+    }
+
+
+    /**
+     * @param int $maxLength
+     * @return string
+     */
+    private static function formattedNullExteriorValue($maxLength = MaxLength::EXTERIOR_VALUE)
+    {
+        return CsvWriterUtil::pad(ExteriorInstructionFiles::MISSING_REPLACEMENT, $maxLength);
+    }
+
+
+    /**
+     * @param $array
+     * @param $key
+     * @return string
+     */
+    private static function formatExteriorDecimalValue($array, $key)
+    {
+        return self::formatExteriorValue($array, $key, MaxLength::EXTERIOR_DECIMAL_VALUE);
+    }
+
+
+    /**
+     * @return string
+     */
+    private static function formattedNullExteriorDecimalValue()
+    {
+        return self::formattedNullExteriorValue(MaxLength::EXTERIOR_DECIMAL_VALUE);
+    }
 }
