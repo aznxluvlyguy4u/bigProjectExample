a7652178b56111d0a939e8e575f642cb67b227d1
werner <werner@jongensvantechniek.nl>
2017-05-22 17:11:47 +0200

    Add functionality to get all invoices for a client and to get all invoices from a year
    Former-commit-id: 69d63a2c9d6db96934f0ea1638ae198ccd638227

--- src/AppBundle/Entity/InvoiceRepository.php
+++ src/AppBundle/Entity/InvoiceRepository.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace AppBundle\Entity;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\QueryBuilder;
 
 /**
  * Class InvoiceRepository
@@ -8,5 +10,37 @@ namespace AppBundle\Entity;
  */
 class InvoiceRepository extends BaseRepository
 {
+    /**
+     * @param $ubn
+     * @return ArrayCollection
+     */
+    public function findClientAvailableInvoices($ubn) {
+        /** @var QueryBuilder $qb */
+        $qb = $this->getEntityManager()->createQueryBuilder();
+                $qb->select('i')
+                    ->from('AppBundle:Invoice','i')
+                    ->where($qb->expr()->andX(
+                        $qb->expr()->eq('i.ubn', ':ubn'),
+                        $qb->expr()->orX(
+                            $qb->expr()->eq('i.status', "'UNPAID'"),
+                            $qb->expr()->eq('i.status', "'PAID'"),
+                            $qb->expr()->eq('i.status', "'CANCELLED'")
+                            )
+                    ))
+                    ->setParameter('ubn', $ubn);
+        $result = new ArrayCollection($qb->getQuery()->getResult());
+        return $result;
+    }
 
+    public function getInvoicesOfCurrentYear($year){
+        $qb = $this->getManager()->getRepository(Invoice::class)->createQueryBuilder('qb')
+            ->where('qb.invoiceNumber LIKE :year')
+            ->orderBy('qb.invoiceNumber', 'DESC')
+            ->setMaxResults(1)
+            ->setParameter('year', $year."%")
+            ->getQuery();
+        /** @var Invoice $invoices */
+        $result = $qb->getResult();
+        return $result;
+    }
 }
