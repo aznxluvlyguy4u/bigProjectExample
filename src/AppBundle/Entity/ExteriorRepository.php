853b5b4d93cf2949a8ff1838a64e17c3bf7f23db
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-05-22 11:51:05 +0200

    Remove unused getLatestExterior() function that does not account for deleted measurements
    Former-commit-id: 4480feb2b0a10eea09a318f068b9a3ea3a95f364

--- src/AppBundle/Entity/ExteriorRepository.php
+++ src/AppBundle/Entity/ExteriorRepository.php
@@ -89,32 +89,6 @@ class ExteriorRepository extends MeasurementRepository {
 
 
     /**
-     * If no Exterior is found a blank Exterior entity is returned
-     * 
-     * @param Animal $animal
-     * @return Exterior
-     */
-    public function getLatestExterior(Animal $animal)
-    {
-        //Measurement Criteria
-        $criteria = Criteria::create()
-            ->where(Criteria::expr()->eq('animal', $animal))
-            ->orderBy(['measurementDate' => Criteria::DESC])
-            ->setMaxResults(1);
-        
-        $latestExterior = $this->getManager()->getRepository(Exterior::class)
-            ->matching($criteria);
-
-        if(sizeof($latestExterior) > 0) {
-            $latestExterior = $latestExterior->get(0);
-        } else { //create an empty default Exterior with default 0.0 values
-            $latestExterior = new Exterior();
-        }
-        return $latestExterior;
-    }
-
-
-    /**
      * NOTE! general_appearance is returned spelling corrected as general_appearance
      *
      * @param int $animalId
