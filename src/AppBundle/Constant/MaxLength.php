37ca65720fd86cfd0f7164d853d353e2433b8621
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-05-31 16:41:00 +0200

    Add Fat, MuscleThickness and TailLength to MaxLength constants
    Former-commit-id: b1003c34b05da86b27fd7b4ec69979ae2605200c

--- src/AppBundle/Constant/MaxLength.php
+++ src/AppBundle/Constant/MaxLength.php
@@ -11,11 +11,14 @@ class MaxLength
     const DATE = 10;
     const EXTERIOR_DECIMAL_VALUE = 4; //includes comma and one decimal
     const EXTERIOR_VALUE = 3; //includes null value
+    const FAT = 3; //9.9
     const HETEROSIS_AND_RECOMBINATION = 5;
     const LITTER_GROUP = 17;
+    const MUSCLE_THICKNESS = 5; //99.99
     const N_LING = 2;
     const NSFO_INSPECTOR = 7;
     const SUCKLE_COUNT = 2;
+    const TAIL_LENGTH = 5; //99.99
     const ULN = 14;
     const VALID_GENDER = 3; //Ram/Ewe
     const WEIGHT = 5;
