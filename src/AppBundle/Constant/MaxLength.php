400864b1b0743d69fd9ab63a1b5a75d449f53dd7
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-06-02 18:20:51 +0200

    Set all MaxLengths for Reproduction values
    Former-commit-id: d5cd82d34fdd3226a996c1ef313b7839c8296551

--- src/AppBundle/Constant/MaxLength.php
+++ src/AppBundle/Constant/MaxLength.php
@@ -8,17 +8,22 @@ class MaxLength
 {
     const AGE = 5;
     const ANIMAL_ID = 8;
+    const BIRTH_INTERVAL = 6;
+    const BIRTH_PROGRESS = 3; //includes -99
     const BREED_CODE_PART_BY_8_PARTS = 1;
+    const BOOL_AS_INT = 3; //includes -99
     const DATE = 10;
     const EXTERIOR_DECIMAL_VALUE = 4; //includes comma and one decimal
     const EXTERIOR_VALUE = 3; //includes null value
     const FAT = 3; //9.9
+    const GESTATION_PERIOD = 3;
     const HETEROSIS_AND_RECOMBINATION = 5;
     const LITTER_GROUP = 17;
     const MUSCLE_THICKNESS = 5; //99.99
     const N_LING = 2;
     const NSFO_INSPECTOR = 7;
-    const SUCKLE_COUNT = 2;
+    const PMSG = 3; //0/1 includes -99
+    const SUCKLE_COUNT = 3; //includes -99
     const TAIL_LENGTH = 5; //99.99
     const ULN = 14;
     const VALID_GENDER = 3; //Ram/Ewe
