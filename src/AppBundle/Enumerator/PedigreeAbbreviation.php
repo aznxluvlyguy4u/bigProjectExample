45ba5a9a7897e74010b7af4848df790fdae7daf8
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-06-07 17:59:50 +0200

    Create PedigreeAbbreviation enumerator
    Former-commit-id: eb064e66ae2df03bc46d7a226f3f2df1f4f960e6

---
+++ src/AppBundle/Enumerator/PedigreeAbbreviation.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace AppBundle\Enumerator;
+
+/**
+ * Class PedigreeAbbreviation
+ * @package AppBundle\Enumerator
+ */
+class PedigreeAbbreviation
+{
+    const BdM = 'BdM';
+    const BL = 'BL';
+    const BT = 'BT';
+    const CF = 'CF';
+    const DK = 'DK';
+    const HD = 'HD';
+    const KE = 'KE';
+    const LAX = 'LAX';
+    const NFS = 'NFS';
+    const NH = 'NH';
+    const NTS = 'NTS';
+    const RUI = 'RUI';
+    const RY = 'RY';
+    const Soay = 'Soay';
+    const TES = 'TES';
+    const TSNH = 'TSNH';
+}
