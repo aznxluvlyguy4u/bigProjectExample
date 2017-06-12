d39c111376f488e6baa5d5880d810abd2587ce38
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-06-12 16:15:12 +0200

    Create MixBlupBreedValueType enumerator
    Former-commit-id: 38cb654e199a794f8c4829f20f3d559e6264ecfa

---
+++ src/AppBundle/Enumerator/MixBlupBreedValueType.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace AppBundle\Enumerator;
+
+/**
+ * Class MixBlupBreedValueType
+ * @package AppBundle\Enumerator
+ */
+class MixBlupBreedValueType
+{
+    //Exterior
+    const EXTERIOR_LEG_WORK = 'ExtBeenw';
+    const EXTERIOR_MUSCULARITY = 'ExtBesp';
+    const EXTERIOR_PROPORTION = 'ExtEvenr';
+    const EXTERIOR_SKULL = 'ExtKop';
+    const EXTERIOR_PROGRESS = 'ExtOntw';
+    const EXTERIOR_TYPE = 'ExtType';
+
+    //Lamb Meat
+    const LAMB_MEAT = 'Vleeslam';
+    const TAIL_LENGTH = 'StaartLen';
+
+    //Reproduction
+    const BIRTH_PROGRESS = 'Geboorte';
+    const FERTILITY = 'Vruchtb';
+    const FERTILITY_1 = 'Vruchtb1';
+    const FERTILITY_2 = 'Vruchtb2';
+    const FERTILITY_3 = 'Vruchtb3';
+
+    //Worm Resistance
+    const WORM_RESISTANCE = 'WormRes';
+}
