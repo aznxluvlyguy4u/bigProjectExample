765bf6a1807fe5c0bcd9b14c65ba0876c0c0f0ab
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-06-12 18:48:24 +0200

    Create an overview of the MixBlupParseInstruction showing which relani and solani columns contain what
    Former-commit-id: 5edd1d89c02d38dd8146f9840cc385bd702e7d88

---
+++ src/AppBundle/Setting/MixBlupParseInstruction.php
@@ -0,0 +1,103 @@
+<?php
+
+
+namespace AppBundle\Setting;
+
+
+use AppBundle\Constant\JsonInputConstant;
+use AppBundle\Enumerator\MixBlupBreedValueType;
+
+
+/**
+ * Class MixBlupParseInstruction
+ * @package AppBundle\Setting
+ */
+class MixBlupParseInstruction
+{
+
+    /**
+     * NOTE! At least
+     *    JsonInputConstant::ANIMAL_ID
+     * && JsonInputConstant::SOLANI_1
+     * && JsonInputConstant::RELANI_1
+     * must be included!
+     *
+     * @param string $breedValueType
+     * @return array
+     */
+    public static function get($breedValueType)
+    {
+        switch ($breedValueType)
+        {
+            case MixBlupBreedValueType::EXTERIOR_MUSCULARITY:
+                return self::tripleSolaniAndRelaniColumn();
+
+            case MixBlupBreedValueType::FERTILITY_1:
+                return self::singleSolaniAndRelaniColumn();
+
+            case MixBlupBreedValueType::FERTILITY_2:
+                return self::singleSolaniAndRelaniColumn();
+
+            case MixBlupBreedValueType::FERTILITY_3:
+                return self::singleSolaniAndRelaniColumn();
+
+            case MixBlupBreedValueType::LAMB_MEAT: //TODO
+                return self::singleSolaniAndRelaniColumn();
+
+            case MixBlupBreedValueType::TAIL_LENGTH:
+                return self::singleSolaniAndRelaniColumn();
+
+            default:
+                /*
+                 * The default includes:
+                 * InpGeboorte  MixBlupBreedValueType::BIRTH_PROGRESS
+                 * InpExtOnt    MixBlupBreedValueType::EXTERIOR_PROGRESS
+                 * ExtBeenw     MixBlupBreedValueType::EXTERIOR_LEG_WORK
+                 * ExtEvenr     MixBlupBreedValueType::EXTERIOR_PROPORTION
+                 * ExtKop       MixBlupBreedValueType::EXTERIOR_SKULL
+                 * ExtType      MixBlupBreedValueType::EXTERIOR_TYPE
+                 */
+                return self::doubleSolaniAndRelaniColumn();
+        }
+    }
+
+
+    /**
+     * @return array
+     */
+    private static function singleSolaniAndRelaniColumn()
+    {
+        return [
+            JsonInputConstant::ANIMAL_ID => 1,
+            JsonInputConstant::SOLANI_1 => 4,
+            //JsonInputConstant::RELANI_1 => 4, //TODO verify the relani
+        ];
+    }
+
+
+    /**
+     * @return array
+     */
+    private static function doubleSolaniAndRelaniColumn()
+    {
+        return [
+            JsonInputConstant::ANIMAL_ID => 1,
+            JsonInputConstant::SOLANI_1 => 4,
+            JsonInputConstant::SOLANI_2 => 5, //TODO add relani
+        ];
+    }
+
+
+    /**
+     * @return array
+     */
+    private static function tripleSolaniAndRelaniColumn()
+    {
+        return [
+            JsonInputConstant::ANIMAL_ID => 1,
+            JsonInputConstant::SOLANI_1 => 4,
+            JsonInputConstant::SOLANI_2 => 5,
+            JsonInputConstant::SOLANI_3 => 6, //TODO add relani
+        ];
+    }
+}
