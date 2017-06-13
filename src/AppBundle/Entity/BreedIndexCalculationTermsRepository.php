72346913a3845adb376a827557e7105b81a3e343
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-06-13 14:10:38 +0200

    ENTITY CHANGE create BreedIndexCalculationTerms
    Former-commit-id: 22525095462b6f634014e9b33391a2dcfc4596b0

---
+++ src/AppBundle/Entity/BreedIndexCalculationTermsRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Class BreedIndexCalculationTermsRepository
+ * @package AppBundle\Entity
+ */
+class BreedIndexCalculationTermsRepository extends BaseRepository {
+
+}
