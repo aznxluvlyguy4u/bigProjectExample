e4cb260f58c756bb67ea5cc0562c95b37becfa4d
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-06-13 13:38:23 +0200

    Create BreedIndexTypeConstant
    Former-commit-id: 26e19d50d19d9436b6104b3bc675a58e0d785784

---
+++ src/AppBundle/Constant/BreedIndexTypeConstant.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace AppBundle\Constant;
+
+/**
+ * Class BreedIndexTypeConstant
+ * @package AppBundle\Constant
+ */
+class BreedIndexTypeConstant
+{
+    const LAMB_MEAT_INDEX = 'VleesLamIndex';
+    const EXTERIOR_INDEX = 'ExterieurIndex';
+    const FERTILITY_INDEX = 'VruchtbaarheidIndex';
+    const WORM_RESISTANCE_INDEX = 'WormResistentieIndex';
+}
