45dfb63f104f53863653e460216c58d755bc9bd1
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-06-14 12:46:03 +0200

    Create BreedGradingSetting class
    Former-commit-id: 335296430e2d5cbc6b393f606e7d54df7ba0a94e

---
+++ src/AppBundle/Setting/BreedGradingSetting.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace AppBundle\Setting;
+
+/**
+ * Class BreedGradingSetting
+ * @package AppBundle\Setting
+ */
+class BreedGradingSetting
+{
+    const MIN_RELIABILITY_FOR_GENETIC_BASE = 0.16;
+}
