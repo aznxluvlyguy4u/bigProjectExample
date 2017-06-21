16cff9d31e1e7bcb87b4f2be49077dd342856c61
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-06-21 04:07:04 +0200

    ENTITY CHANGE create ErrorLogAnimalPedigree
    Former-commit-id: 7d214ea630b4e926f577c3d7f3a2433cccb373e9

---
+++ src/AppBundle/Entity/ErrorLogAnimalPedigreeRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Class ErrorLogAnimalPedigreeRepository
+ * @package AppBundle\Entity
+ */
+class ErrorLogAnimalPedigreeRepository extends BaseRepository {
+}
