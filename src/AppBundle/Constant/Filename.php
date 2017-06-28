6f088295fb7d3c6dd96e248106b72dfd742779f0
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-06-28 13:23:35 +0200

    Create Relani direct and indirect filename constants
    Former-commit-id: 4fecd0c142fd768d055b0631e587c29f7b2ed854

--- src/AppBundle/Constant/Filename.php
+++ src/AppBundle/Constant/Filename.php
@@ -8,5 +8,7 @@ class Filename
 {
     const PAR_NSFO = 'ParNSFO.txt';
     const RELANI = 'Relani.txt';
+    const RELANI_DIRECT = 'Relani_direct.txt';
+    const RELANI_INDIRECT = 'Relani_indirect.txt';
     const SOLANI = 'Solani.txt';
 }
