57ea6de6a04ec094602d4ce1ba3508797d568d24
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-08-08 09:58:35 +0200

    Create ArrayUtil functions to get the firstKey and firstValue of an array
    Former-commit-id: 40415967c60ae0c632d3f111b1926b29f8e28041

---
+++ src/AppBundle/Service/CsvFromSqlResultsWriterService.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace AppBundle\Service;
+
+
+class CsvWriterService
+{
+    public function __construct()
+    {
+    }
+
+
+    public function writeFromSqlResults($results)
+    {
+        foreach ($results as $result)
+        {
+            $result[''];
+        }
+    }
+}
