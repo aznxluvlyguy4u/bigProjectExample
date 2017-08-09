34ac9e5d7d35a4c41e98c389d95bd60f568824c5
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-08-09 17:03:47 +0200

    Log login attempts for user login
    Former-commit-id: 781de9d86b9b109873515e2b1abd60185508d5ce

--- src/AppBundle/Controller/AuthAPIController.php
+++ src/AppBundle/Controller/AuthAPIController.php
@@ -196,12 +196,15 @@ class AuthAPIController extends APIController {
             "user" => MenuBarOutput::create($client)
         ];
 
+        ActionLogWriter::loginUser($this->getManager(), $client, $client, true);
+
         return new JsonResponse(array("access_token"=>$client->getAccessToken(),
                          Constant::RESULT_NAMESPACE => $result), 200);
 
         //TODO If Frontend is updated use the following JsonResponse
         //return new JsonResponse(array(Constant::RESULT_NAMESPACE => $result), 200);
-      }
+        }
+        ActionLogWriter::loginUser($this->getManager(), $client, null, false);
     }
 
     return new JsonResponse(array("errorCode" => 401, "errorMessage"=>"Unauthorized"), 401);
