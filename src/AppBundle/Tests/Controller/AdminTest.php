21c9b30a3b00a60d0b2da9ca5612492acd1f95cd
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-08-26 14:45:33 +0200

    Create unit tests for Create Update Deactivate for AdminService
    Former-commit-id: fa0ef0e72816923865fb0093e66a64fd66869650

--- src/AppBundle/Tests/Controller/AdminTest.php
+++ src/AppBundle/Tests/Controller/AdminTest.php
@@ -2,14 +2,21 @@
 
 namespace AppBundle\Tests\Controller;
 
+use AppBundle\Constant\Constant;
 use AppBundle\Constant\TestConstant;
+use AppBundle\Entity\Employee;
 use AppBundle\Enumerator\AccessLevelType;
 use AppBundle\Service\IRSerializer;
+use AppBundle\Util\ArrayUtil;
 use AppBundle\Util\DoctrineUtil;
+use AppBundle\Util\ResultUtil;
+use AppBundle\Util\UnitTestData;
 use AppBundle\Util\Validator;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Liip\FunctionalTestBundle\Test\WebTestCase;
 use Symfony\Bundle\FrameworkBundle\Client as RequestClient;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Class AdminTest
@@ -21,18 +28,35 @@ class AdminTest extends WebTestCase
 
     const DECLARE_ADMIN_ENDPOINT = "/api/v1/admins";
 
+    const GET_getAdmins = 'GET_getAdmins';
+    const GET_getAccessLevelTypes = 'GET_getAccessLevelTypes';
+    const POST_createAdmin = 'POST_createAdmin';
+    const PUT_editAdmin = 'PUT_editAdmin';
+    const PUT_deactivateAdmin = 'PUT_deactivateAdmin';
+    const POST_getTemporaryGhostToken = 'POST_getTemporaryGhostToken';
+    const PUT_verifyGhostToken = 'PUT_verifyGhostToken';
+
+    private $endpointSuffixes = [
+        self::GET_getAdmins => '',
+        self::GET_getAccessLevelTypes => '-access-levels',
+        self::POST_createAdmin => '',
+        self::PUT_editAdmin => '',
+        self::PUT_deactivateAdmin => '-deactivate',
+        self::POST_getTemporaryGhostToken => '/ghost',
+        self::PUT_verifyGhostToken => '/verify-ghost-token',
+    ];
+
+
     /** @var RequestClient */
     private $client;
-
     /** @var string */
     static private $accessTokenCode;
-
     /** @var IRSerializer */
     static private $serializer;
-
-    /** @var ObjectManager */
+    /** @var ObjectManager|EntityManagerInterface */
     static private $em;
-
+    /** @var Employee */
+    static private $employee;
     /** @var array */
     private $defaultHeaders;
 
@@ -64,6 +88,9 @@ class AdminTest extends WebTestCase
         }
 
         self::$accessTokenCode = DoctrineUtil::getRandomAdminAccessTokenCode(self::$em, AccessLevelType::SUPER_ADMIN);
+
+        $testEmail = $container->getParameter('test_email');
+        self::$employee = UnitTestData::getTestAdmin($testEmail, AccessLevelType::ADMIN);
     }
 
 
@@ -88,17 +115,142 @@ class AdminTest extends WebTestCase
     public function testAdminsGetters()
     {
         $this->client->request('GET',
-            $this::DECLARE_ADMIN_ENDPOINT,
+            $this::DECLARE_ADMIN_ENDPOINT . $this->endpointSuffixes[self::GET_getAdmins],
             array(), array(), $this->defaultHeaders
         );
         $this->assertStatusCode(200, $this->client);
 
 
         $this->client->request('GET',
-            $this::DECLARE_ADMIN_ENDPOINT.'-access-levels',
+            $this::DECLARE_ADMIN_ENDPOINT . $this->endpointSuffixes[self::GET_getAccessLevelTypes],
             array(), array(), $this->defaultHeaders
         );
         $this->assertStatusCode(200, $this->client);
+    }
+
+
+    /**
+     * @group cud
+     * @group admin-cud
+     * Test admin create, edit and delete endpoints
+     */
+    public function testCreateEditDeleteAdmin()
+    {
+
+        /* Create */
+
+        $createAdminJson =
+            json_encode(
+                [
+                    "first_name" => self::$employee->getFirstName(),
+                    "last_name" => self::$employee->getLastName(),
+                    "email_address" => self::$employee->getEmailAddress(),
+                    "access_level" => self::$employee->getAccessLevel(),
+                ]);
+
+        $this->client->request('POST',
+            $this::DECLARE_ADMIN_ENDPOINT . $this->endpointSuffixes[self::POST_createAdmin],
+            array(),
+            array(),
+            $this->defaultHeaders,
+            $createAdminJson
+        );
+
+        /** @var JsonResponse $response */
+        $response = $this->client->getResponse();
+        $data = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey(Constant::RESULT_NAMESPACE, $data);
+
+        $personId = ResultUtil::getFromResult('person_id', $response);
+
+        $this->assertEquals(true, is_string($personId), "'person_id' value is missing in result array");
+        $this->assertStatusCode(200, $this->client);
+
+
+        if ($personId !== null) {
+
+            self::$employee->setPersonId($personId);
+
+            /* Edit */
+
+            $newFirstName = self::$employee->getFirstName() . '_TESTING170489';
+            $newLastName = self::$employee->getLastName() . '_TESTING170489';
+            $newAccessLevel = self::$employee->getAccessLevel() === AccessLevelType::ADMIN ?
+                AccessLevelType::SUPER_ADMIN : AccessLevelType::ADMIN;
+
+            $editAdminJson =
+                json_encode(
+                    [
+                        "person_id" => $personId,
+                        "first_name" => $newFirstName,
+                        "last_name" => $newLastName,
+                        "email_address" => self::$employee->getEmailAddress(),
+                        "access_level" => $newAccessLevel,
+                    ]);
+
+            $this->client->request('PUT',
+                $this::DECLARE_ADMIN_ENDPOINT . $this->endpointSuffixes[self::PUT_editAdmin],
+                array(),
+                array(),
+                $this->defaultHeaders,
+                $editAdminJson
+            );
+
+            /** @var JsonResponse $response */
+            $response = $this->client->getResponse();
+            $data = json_decode($response->getContent(), true);
+            $this->assertArrayHasKey(Constant::RESULT_NAMESPACE, $data);
+            $this->assertStatusCode(200, $this->client);
+
+            $resultArray = ResultUtil::getResultArray($response);
+
+            $this->assertEquals(true, is_array($resultArray), "'Result' array is missing in edit output");
+            if (is_array($resultArray)) {
+                $this->assertEquals($newFirstName, ArrayUtil::get('first_name', $resultArray), "'first_name' is not updated");
+                $this->assertEquals($newLastName, ArrayUtil::get('last_name', $resultArray), "'last_name' is not updated");
+                $this->assertEquals($newAccessLevel, ArrayUtil::get('access_level', $resultArray), "'access_level' is not updated");
+            }
+
+            /* Deactivate */
+
+            $deleteAdminJson =
+                json_encode(
+                    [
+                        "person_id" => $personId,
+                    ]);
+
+            $this->client->request('PUT',
+                $this::DECLARE_ADMIN_ENDPOINT . $this->endpointSuffixes[self::PUT_deactivateAdmin],
+                array(),
+                array(),
+                $this->defaultHeaders,
+                $deleteAdminJson
+            );
+
+            /** @var JsonResponse $response */
+            $response = $this->client->getResponse();
+            $data = json_decode($response->getContent(), true);
+            $this->assertStatusCode(200, $this->client);
+
+            $resultArray = ResultUtil::getResultArray($response);
+
+            $entityIsReturnedInJsonData = false;
+
+            if ($entityIsReturnedInJsonData) {
+
+                $this->assertArrayHasKey(Constant::RESULT_NAMESPACE, $data);
+
+                $this->assertEquals(true, is_array($resultArray), "'Result' array is missing in delete output");
+                if (is_array($resultArray)) {
+                    $this->assertEquals(false, ArrayUtil::get('is_active', $resultArray), "Admin has not been deactivated");
+                }
+
+            } else {
+                $this->assertEquals('ok', $resultArray, "output does not equal 'ok'");
+            }
+
+        }
+
     }
 
 
@@ -115,6 +267,21 @@ class AdminTest extends WebTestCase
      */
     public static function tearDownAfterClass()
     {
+        if (self::$employee->getPersonId() !== null) {
+            /** @va Employee $employee */
+            $employee = self::$em->getRepository(Employee::class)
+                ->findOneByPersonId(self::$employee->getPersonId());
 
+            if ($employee) {
+                $id = $employee->getId();
+                $sql = "DELETE FROM action_log WHERE user_account_id = $id OR action_by_id = $id";
+                self::$em->getConnection()->query($sql)->execute();
+
+                self::$em->remove($employee);
+                self::$em->flush();
+            }
+
+            DoctrineUtil::updateTableSequence(self::$em->getConnection(), ['person']);
+        }
     }
 }
