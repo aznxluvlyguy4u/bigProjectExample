8ff8501bcd7e01d5c062a57f075aee233ffb9cfc
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-09-21 17:15:16 +0200

    Add complete nelmio annotations for VwaEmployeeAPIController
    Former-commit-id: 06569294bc47d2658b21436090152cdd8be4baa0

--- src/AppBundle/Controller/VwaEmployeeAPIController.php
+++ src/AppBundle/Controller/VwaEmployeeAPIController.php
@@ -22,6 +22,31 @@ class VwaEmployeeAPIController extends APIController implements VwaEmployeeAPICo
     /**
      * Get overview of all VWA Employees.
      *
+     * ### Response body ###
+     *
+     *  {
+     *      "result": [
+     *          {
+     *              "person_id": "da1494820dfc7052c90e67f2d34f9a27950b8cd5",
+     *              "first_name": "Johnny",
+     *              "last_name": "Boyz",
+     *              "email_address": "jb@vwa.nl",
+     *              "is_active": true,
+     *              "invitation_date": "2017-09-05T12:14:17+0200",
+     *              "type": "VwaEmployee"
+     *          },
+     *          {
+     *              "person_id": "da1494820dfc7052c90e67f2d34f9a27950b8cd6",
+     *              "first_name": "John",
+     *              "last_name": "Man",
+     *              "email_address": "jbm@vwa.nl",
+     *              "is_active": true,
+     *              "invitation_date": "2017-09-05T12:14:17+0200",
+     *              "type": "VwaEmployee"
+     *          }
+     *      ]
+     *  }
+     *
      * @ApiDoc(
      *   section = "VWA",
      *   headers={
@@ -33,6 +58,10 @@ class VwaEmployeeAPIController extends APIController implements VwaEmployeeAPICo
      *     }
      *   },
      *   resource = true,
+     *   statusCodes={
+     *     200="Returned when successful",
+     *     401="Unauthorized"
+     *   },
      *   description = "Get overview of all VWA Employees."
      * )
      * @param Request $request the request object
@@ -47,7 +76,31 @@ class VwaEmployeeAPIController extends APIController implements VwaEmployeeAPICo
 
 
     /**
-     * Get details of a single VWA Employee.
+     * Get details of a single VWA Employee. A VWA Employee can use "me" instead of "{id}" to get their own data.
+     *
+     * ### Response body ###
+     *
+     * {
+     *  "result":
+     *      {
+     *          "person_id": "da1494820dfc7052c90e67f2d34f9a27950b8cd5",
+     *          "first_name": "Dadio",
+     *          "last_name": "Masters",
+     *          "email_address": "dm@vwa.nl",
+     *          "is_active": true,
+     *          "invitation_date": "2017-09-05T12:14:17+0200",
+     *          "invited_by":
+     *          {
+     *              "person_id": "1e71b4c3d52365fc21ea2a48d997728712c3ecc3",
+     *              "first_name": "Admin",
+     *              "last_name": "Wanita",
+     *              "email_address": "aw@nsfo.nl",
+     *              "is_active": true,
+     *              "type": "Employee"
+     *          },
+     *          "type": "VwaEmployee"
+     *      }
+     * }
      *
      * @ApiDoc(
      *   section = "VWA",
@@ -60,6 +113,11 @@ class VwaEmployeeAPIController extends APIController implements VwaEmployeeAPICo
      *     }
      *   },
      *   resource = true,
+     *   statusCodes={
+     *     200="Returned when successful",
+     *     400="Bad request if no vwa employee has found for the given id",
+     *     401="Unauthorized"
+     *   },
      *   description = "Get details of a single VWA Employee."
      * )
      * @param string $id
@@ -77,6 +135,38 @@ class VwaEmployeeAPIController extends APIController implements VwaEmployeeAPICo
     /**
      * Create a new VWA Employee. An invitation email with the login data is send after creation.
      *
+     * ### Request body ###
+     *
+     *  {
+     *      "first_name":"Dadio",
+     *      "last_name":"Masters",
+     *      "email_address":"dm@vwa.nl"
+     *  }
+     *
+     * ### Response body ###
+     *
+     * {
+     *  "result":
+     *      {
+     *          "person_id": "da1494820dfc7052c90e67f2d34f9a27950b8cd5",
+     *          "first_name": "Dadio",
+     *          "last_name": "Masters",
+     *          "email_address": "dm@vwa.nl",
+     *          "is_active": true,
+     *          "invitation_date": "2017-09-05T12:14:17+0200",
+     *          "invited_by":
+     *          {
+     *              "person_id": "1e71b4c3d52365fc21ea2a48d997728712c3ecc3",
+     *              "first_name": "Admin",
+     *              "last_name": "Wanita",
+     *              "email_address": "aw@nsfo.nl",
+     *              "is_active": true,
+     *              "type": "Employee"
+     *          },
+     *          "type": "VwaEmployee"
+     *      }
+     * }
+     *
      * @ApiDoc(
      *   section = "VWA",
      *   headers={
@@ -88,6 +178,11 @@ class VwaEmployeeAPIController extends APIController implements VwaEmployeeAPICo
      *     }
      *   },
      *   resource = true,
+     *   statusCodes={
+     *     200="Returned when successful",
+     *     400="Bad request if an active vwa employee already exists, or input format is incorrect",
+     *     401="Unauthorized"
+     *   },
      *   description = "Create a new VWA Employee. An invitation email with the login data is send after creation. "
      * )
      * @param Request $request the request object
@@ -104,6 +199,45 @@ class VwaEmployeeAPIController extends APIController implements VwaEmployeeAPICo
     /**
      * Edit details of a single VWA Employee.
      *
+     * To skip editing certain values this endpoint provides two options.
+     * Patch: You can exclude they key-value pairs you do not want to edit.
+     * Put: you can return them with the values unchanged.
+     *
+     * Note! passwords may only be changed by the VWA employee themselves.
+     *
+     * ### Request body ###
+     *
+     *  {
+     *      "first_name":"Dadio",
+     *      "last_name":"Masters",
+     *      "email_address":"dm@vwa.nl",
+     *      "password": "askjfdkfigjjdkjfkj"
+     *  }
+     *
+     * ### Response body ###
+     *
+     * {
+     *  "result":
+     *      {
+     *          "person_id": "da1494820dfc7052c90e67f2d34f9a27950b8cd5",
+     *          "first_name": "Dadio",
+     *          "last_name": "Masters",
+     *          "email_address": "dm@vwa.nl",
+     *          "is_active": true,
+     *          "invitation_date": "2017-09-05T12:14:17+0200",
+     *          "invited_by":
+     *          {
+     *              "person_id": "1e71b4c3d52365fc21ea2a48d997728712c3ecc3",
+     *              "first_name": "Admin",
+     *              "last_name": "Wanita",
+     *              "email_address": "aw@nsfo.nl",
+     *              "is_active": true,
+     *              "type": "Employee"
+     *          },
+     *          "type": "VwaEmployee"
+     *      }
+     * }
+     *
      * @ApiDoc(
      *   section = "VWA",
      *   headers={
@@ -115,6 +249,11 @@ class VwaEmployeeAPIController extends APIController implements VwaEmployeeAPICo
      *     }
      *   },
      *   resource = true,
+     *   statusCodes={
+     *     200="Returned when successful",
+     *     400="Bad request if vwa employee has already been deactivated, or input format is incorrect",
+     *     401="Unauthorized, including when an password change is requested by someone other than the own user"
+     *   },
      *   description = "Edit details of a single VWA Employee."
      * )
      * @param string $id
@@ -132,6 +271,30 @@ class VwaEmployeeAPIController extends APIController implements VwaEmployeeAPICo
     /**
      * Deactivate a single VWA Employee.
      *
+     * ### Response body ###
+     *
+     * {
+     *  "result":
+     *      {
+     *          "person_id": "da1494820dfc7052c90e67f2d34f9a27950b8cd5",
+     *          "first_name": "Dadio",
+     *          "last_name": "Masters",
+     *          "email_address": "dm@vwa.nl",
+     *          "is_active": true,
+     *          "invitation_date": "2017-09-05T12:14:17+0200",
+     *          "invited_by":
+     *          {
+     *              "person_id": "1e71b4c3d52365fc21ea2a48d997728712c3ecc3",
+     *              "first_name": "Admin",
+     *              "last_name": "Wanita",
+     *              "email_address": "aw@nsfo.nl",
+     *              "is_active": true,
+     *              "type": "Employee"
+     *          },
+     *          "type": "VwaEmployee"
+     *      }
+     * }
+     *
      * @ApiDoc(
      *   section = "VWA",
      *   headers={
@@ -143,6 +306,11 @@ class VwaEmployeeAPIController extends APIController implements VwaEmployeeAPICo
      *     }
      *   },
      *   resource = true,
+     *   statusCodes={
+     *     200="Returned when successful",
+     *     400="Bad request if vwa employee has already been deactivated",
+     *     401="Unauthorized"
+     *   },
      *   description = "Deactivate a single VWA Employee."
      * )
      * @param string $id
@@ -160,6 +328,19 @@ class VwaEmployeeAPIController extends APIController implements VwaEmployeeAPICo
     /**
      * Retrieve a valid access token.
      *
+     * ### Response body ###
+     *
+     *  {
+     *      "result": {
+     *          "access_token": "i8e278fnhe__some_access_token__fWGWQGqgt42twh",
+     *          "user": {
+     *              "first_name": "Charlie",
+     *              "last_name": "Checker",
+     *              "email_address": "cc@vwa.nl"
+     *          }
+     *      }
+     *  }
+     *
      * @ApiDoc(
      *   section = "VWA",
      *   requirements={
@@ -172,6 +353,7 @@ class VwaEmployeeAPIController extends APIController implements VwaEmployeeAPICo
      *     }
      *   },
      *   resource = true,
+     *   statusCodes={200="Returned when successful",401="Unauthorized"},
      *   description = "Retrieve a valid access token."
      * )
      * @param Request $request the request object
