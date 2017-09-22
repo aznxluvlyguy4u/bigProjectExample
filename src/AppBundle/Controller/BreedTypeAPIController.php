89587a62efcf016b37639336aaa06247d44e4ce6
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-09-22 06:09:32 +0200

    Create BreedType endpoint
    Former-commit-id: ca3d8bad6152c4c3a40dd63d3842aa2796c4ae4b

---
+++ src/AppBundle/Controller/BreedTypeAPIController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Component\HttpFoundation\JsonResponse;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+/**
+ * Class BreedTypeAPIController
+ * @package AppBundle\Controller
+ * @Route("/api/v1/breed-type")
+ */
+class BreedTypeAPIController extends APIController implements BreedTypeAPIControllerInterface
+{
+    /**
+     * Get BreedTypes.
+     *
+     * @ApiDoc(
+     *
+     *   requirements={
+     *     {
+     *       "name"="AccessToken",
+     *       "dataType"="string",
+     *       "requirement"="",
+     *       "description"="A valid accesstoken belonging to the user that is registered with the API"
+     *     }
+     *   },
+     *   resource = true,
+     *   description = "Get BreedTypes",
+     * )
+     * @param Request $request the request object
+     * @return JsonResponse
+     * @Route("")
+     * @Method("GET")
+     */
+    function getBreedTypes(Request $request)
+    {
+        return $this->get('app.breed_type')->getBreedTypes($request);
+    }
+
+}
