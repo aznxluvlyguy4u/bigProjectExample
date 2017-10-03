52198625988660a5002ef97f27e0dd49e67eaf60
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-10-03 16:54:51 +0200

    Create service to recalculate breedcodes of offspring
    Former-commit-id: 348cc9aa76b0a834b79dbb5096af59b3290e619b

---
+++ src/AppBundle/Service/DataFix/OffspringBreedCodeRecalculator.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace AppBundle\Service\DataFix;
+
+
+use AppBundle\Util\BreedCodeUtil;
+use AppBundle\Util\SqlUtil;
+
+class OffspringBreedCodeRecalculator extends DataFixServiceBase
+{
+    /** @var int */
+    private $recalculationCount;
+    /** @var array */
+    private $animalIdsRecalculatedBreedCodes;
+
+
+    private function resetClassVariables()
+    {
+        $this->recalculationCount = 0;
+        $this->animalIdsRecalculatedBreedCodes = [];
+    }
+
+
+    /**
+     * @param int $animalId
+     * @param bool $onlyRecalculateForChildren
+     * @param bool $resetCounter
+     * @return int|null
+     */
+    public function recursiveRecalculate($animalId, $onlyRecalculateForChildren = true, $resetCounter = true)
+    {
+        if ($resetCounter) {
+            $this->resetClassVariables();
+        }
+
+        if (!is_int($animalId) && !ctype_digit($animalId)) {
+            return null;
+        }
+
+
+        $recalculateForChildren = false;
+        if ($onlyRecalculateForChildren) {
+            $recalculateForChildren = true;
+
+        } else {
+            $isUpdated = BreedCodeUtil::updateBreedCodeBySql($this->getConnection(), $animalId);
+            if ($isUpdated) {
+                $this->recalculationCount++;
+                $recalculateForChildren = true;
+            }
+        }
+
+        if($recalculateForChildren) {
+            foreach ($this->getOffspringIds($animalId) as $childId) {
+                if (!key_exists($childId, $this->animalIdsRecalculatedBreedCodes)) {
+                    $this->recursiveRecalculate($childId, false, false);
+                    $this->animalIdsRecalculatedBreedCodes[$childId] = $childId;
+                }
+            }
+        }
+
+        return $this->recalculationCount;
+    }
+
+
+    /**
+     * @param int $animalId
+     * @return array
+     */
+    private function getOffspringIds($animalId)
+    {
+        if (!is_int($animalId) && !ctype_digit($animalId)) {
+            return [];
+        }
+
+        $sql = "SELECT id FROM animal WHERE parent_mother_id = $animalId OR parent_father_id = $animalId";
+        $results = $this->getConnection()->query($sql)->fetchAll();
+
+        return SqlUtil::getSingleValueGroupedSqlResults('id', $results, true, false);
+    }
+
+
+}
