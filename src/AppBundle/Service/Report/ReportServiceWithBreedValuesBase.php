c423657f0cf54aef52d7b5b78c85a1bd79d76dad
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-12-11 10:53:55 +0100

    Refactor breedvalues sql query logic into a separate service
    Former-commit-id: 4bd68ad5f9fb6f7eafff5991df4963534b3e26c1

---
+++ src/AppBundle/Service/Report/ReportServiceWithBreedValuesBase.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace AppBundle\Service\Report;
+
+use AppBundle\Service\AWSSimpleStorageService;
+use AppBundle\Service\CsvFromSqlResultsWriterService as CsvWriter;
+use AppBundle\Service\ExcelService;
+use AppBundle\Service\UserService;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Snappy\GeneratorInterface;
+use Symfony\Bridge\Monolog\Logger;
+use Symfony\Bridge\Twig\TwigEngine;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class ReportServiceWithBreedValuesBase extends ReportServiceBase
+{
+    /** @var BreedValuesReportQueryGenerator */
+    protected $breedValuesReportQueryGenerator;
+
+    public function __construct(EntityManagerInterface $em, ExcelService $excelService, Logger $logger,
+                                AWSSimpleStorageService $storageService, CsvWriter $csvWriter, UserService $userService, TwigEngine $templating,
+                                TranslatorInterface $translator,
+                                GeneratorInterface $knpGenerator,
+                                BreedValuesReportQueryGenerator $breedValuesReportQueryGenerator,
+                                $cacheDir, $rootDir, $folderName, $filename)
+    {
+        parent::__construct($em, $excelService, $logger, $storageService, $csvWriter, $userService, $templating, $translator,
+            $knpGenerator,$cacheDir, $rootDir, $folderName, $filename);
+
+        $this->breedValuesReportQueryGenerator = $breedValuesReportQueryGenerator;
+    }
+}
