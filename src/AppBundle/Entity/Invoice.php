c42132cc71059fcbfa8c1a9a4a422660a2e1290a
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2017-12-12 11:36:23 +0100

    ENTITY CHANGE make documentUrl in Invoice nullable
    Former-commit-id: 97ca257080b1c0215cf928d45de7bf9a230ca31f

--- src/AppBundle/Entity/Invoice.php
+++ src/AppBundle/Entity/Invoice.php
@@ -55,7 +55,7 @@ class Invoice
     /**
      * @var string
      *
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", nullable=true)
      * @JMS\Type("string")
      */
     private $documentUrl;
