ee04c909b70083858632e525a62b1636fa8cb7f6
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2018-01-11 16:58:53 +0100

    Validate animal batch edit for duplicate ulns, stns and ids in the same request body
    Former-commit-id: a65b5e478e04c1f17d8874a8c1a5572ee7efd4c1

--- src/AppBundle/Service/AnimalDetailsBatchUpdaterService.php
+++ src/AppBundle/Service/AnimalDetailsBatchUpdaterService.php
@@ -4,9 +4,9 @@
 namespace AppBundle\Service;
 
 
+use AppBundle\Component\HttpFoundation\JsonResponse;
 use AppBundle\Constant\JsonInputConstant;
 use AppBundle\Entity\Animal;
-use AppBundle\Util\ArrayUtil;
 use AppBundle\Util\RequestUtil;
 use AppBundle\Util\ResultUtil;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,32 +14,107 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AnimalDetailsBatchUpdaterService extends ControllerServiceBase
 {
+    /**
+     * @param Request $request
+     * @return \AppBundle\Component\HttpFoundation\JsonResponse
+     */
     public function updateAnimalDetails(Request $request)
     {
         $content = RequestUtil::getContentAsArray($request);
 
-        $animals = $content->get(JsonInputConstant::ANIMALS);
+        $animalsArray = $content->get(JsonInputConstant::ANIMALS);
+
+        /** @var Animal[] $animalsWithNewValues */
+        $animalsWithNewValues = $this->getBaseSerializer()->denormalizeToObject($animalsArray, Animal::class, true);
 
         $ids = [];
-        foreach ($animals as $animal) {
-            $animalId = ArrayUtil::get('id', $animal);
-            if (is_int($animalId)) {
-                $ids[] = $animalId;
-            } elseif ($animalId !== null) {
-                throw new \Exception("Animal 'id' is missing");
+        foreach ($animalsWithNewValues as $animal) {
+            if ($animal->getId()) {
+                $ids[] = $animal->getId();
+            } elseif ($animal->getId() !== null) {
+                return ResultUtil::errorResult("Animal 'id' is missing", Response::HTTP_PRECONDITION_REQUIRED);
             }
         }
 
         try {
-            $animals = $this->getManager()->getRepository(Animal::class)->findByIds($ids);
+            $currentAnimals = $this->getManager()->getRepository(Animal::class)->findByIds($ids);
         } catch (\Exception $exception) {
             return ResultUtil::errorResult($exception->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
         }
 
-        $serializedAnimalsOutput = AnimalService::getSerializedAnimalsInBatchEditFormat($this, $animals);
+        $firstValidationResult = $this->validateForDuplicateValuesWithinRequestBody($animalsWithNewValues);
+        if ($firstValidationResult instanceof JsonResponse) {
+            return $firstValidationResult;
+        }
+
+
+
+        $serializedAnimalsOutput = AnimalService::getSerializedAnimalsInBatchEditFormat($this, $currentAnimals);
 
         return ResultUtil::successResult([
             JsonInputConstant::ANIMALS => $serializedAnimalsOutput[JsonInputConstant::ANIMALS]
         ]);
+    }
+
+
+    /**
+     * @param Animal[] $animalsWithNewValues
+     * @return JsonResponse|bool
+     */
+    private function validateForDuplicateValuesWithinRequestBody(array $animalsWithNewValues = [])
+    {
+        $foundValuesSets = [
+            JsonInputConstant::ID => [],
+            JsonInputConstant::ULN => [],
+            JsonInputConstant::STN => [],
+        ];
+
+        $duplicateValuesSets = [
+            JsonInputConstant::ID => [],
+            JsonInputConstant::ULN => [],
+            JsonInputConstant::STN => [],
+        ];
+
+        foreach ($animalsWithNewValues as $animalsWithNewValue) {
+            $values = [
+                JsonInputConstant::ID => $animalsWithNewValue->getId(),
+                JsonInputConstant::ULN => $animalsWithNewValue->getUln(),
+                JsonInputConstant::STN => $animalsWithNewValue->getPedigreeString(),
+            ];
+
+            foreach (array_keys($values) as $typeKey) {
+                $value = $values[$typeKey];
+                if ($value === null) {
+                    continue;
+                }
+
+                if (key_exists($value, $foundValuesSets[$typeKey])) {
+                    $duplicateValuesSets[$typeKey][$value] = $value;
+                } else {
+                    $foundValuesSets[$typeKey][$value] = $value;
+                }
+            }
+        }
+
+        $errorMessage = '';
+        $prefix = '';
+        foreach ($duplicateValuesSets as $typeKey => $duplicateValues) {
+            if (count($duplicateValues) > 0) {
+                switch ($typeKey) {
+                    case JsonInputConstant::ULN: $errorMessageKey = 'THE FOLLOWING DUPLICATE ULNS WERE INSERTED'; break;
+                    case JsonInputConstant::STN: $errorMessageKey = 'THE FOLLOWING DUPLICATE PEDIGREE NUMBERS WERE INSERTED'; break;
+                    case JsonInputConstant::ID: $errorMessageKey = 'THE FOLLOWING DUPLICATE IDS WERE INSERTED'; break;
+                    default: break;
+                }
+                $errorMessage .= $prefix . $this->translateUcFirstLower($errorMessageKey) . ': '.implode(', ', $duplicateValues).'.';
+                $prefix = ' ';
+            }
+        }
+
+        if ($errorMessage !== '') {
+            return ResultUtil::errorResult($errorMessage, Response::HTTP_PRECONDITION_REQUIRED);
+        }
+
+        return true;
     }
 }
