b47d54de1d2feff16c53b45258842e78c191ca37
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2018-01-15 16:25:26 +0100

    Deactivate deprecated loadClassCache in app.php and app_dev.php
    Former-commit-id: 6efdcdb68910a3a66d498b8d18b07a3fe039ef9c

--- web/app_dev.php
+++ web/app_dev.php
@@ -27,7 +27,7 @@ $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
+// $kernel->loadClassCache(); depreciated since php 7
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
