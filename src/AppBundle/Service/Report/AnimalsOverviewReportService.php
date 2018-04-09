8cee2b7f3fcc139c3135b5e0fd6ff6c96a49d764
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2018-04-09 17:24:45 +0200

    Validate if referenceDate for AnimalsOverviewReport is not in the future
    Former-commit-id: e50d5a9c5eeb79b5084332c7a5252ecd974d8a04

--- src/AppBundle/Service/Report/AnimalsOverviewReportService.php
+++ src/AppBundle/Service/Report/AnimalsOverviewReportService.php
@@ -7,11 +7,14 @@ namespace AppBundle\Service\Report;
 use AppBundle\Enumerator\AccessLevelType;
 use AppBundle\Enumerator\FileType;
 use AppBundle\Enumerator\QueryParameter;
+use AppBundle\Util\DateUtil;
 use AppBundle\Util\ProcessUtil;
 use AppBundle\Util\RequestUtil;
 use AppBundle\Util\ResultUtil;
+use AppBundle\Util\TimeUtil;
 use AppBundle\Validation\AdminValidator;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AnimalsOverviewReportService extends ReportServiceWithBreedValuesBase implements ReportServiceInterface
 {
@@ -37,7 +40,12 @@ class AnimalsOverviewReportService extends ReportServiceWithBreedValuesBase impl
 
             $this->concatValueAndAccuracy = RequestUtil::getBooleanQuery($request,QueryParameter::CONCAT_VALUE_AND_ACCURACY, self::CONCAT_BREED_VALUE_AND_ACCURACY_BY_DEFAULT);
             $pedigreeActiveEndDateLimit = RequestUtil::getDateQuery($request,QueryParameter::PEDIGREE_ACTIVE_END_DATE, new \DateTime());
-            $activeUbnReferenceDateString = RequestUtil::getDateQuery($request,QueryParameter::REFERENCE_DATE, new \DateTime())->format('Y-m-d');
+            $activeUbnReferenceDate = RequestUtil::getDateQuery($request,QueryParameter::REFERENCE_DATE, new \DateTime());
+            $activeUbnReferenceDateString = $activeUbnReferenceDate->format('Y-m-d');
+
+            if (TimeUtil::isDateInFuture($activeUbnReferenceDate)) {
+                return ResultUtil::errorResult($this->translateErrorMessages('REFERENCE DATE CANNOT BE IN THE FUTURE'), Response::HTTP_PRECONDITION_REQUIRED);
+            }
 
             $this->setLocaleFromQueryParameter($request);
 
