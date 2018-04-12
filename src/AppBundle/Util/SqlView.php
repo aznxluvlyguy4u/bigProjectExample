f6029fc9a6154f69a12cde2193040e722858914b
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2018-04-12 11:23:31 +0200

    Initialize litter details view table
    Former-commit-id: 66ed3ade83c2010acdc6e20cfa31066e1400c663

--- src/AppBundle/Util/SqlView.php
+++ src/AppBundle/Util/SqlView.php
@@ -4,6 +4,7 @@
 namespace AppBundle\Util;
 
 
+use AppBundle\Enumerator\RequestStateType;
 use AppBundle\Traits\EnumInfo;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Exception\TableExistsException;
@@ -13,6 +14,7 @@ class SqlView
     use EnumInfo;
 
     const VIEW_ANIMAL_LIVESTOCK_OVERVIEW_DETAILS = 'view_animal_livestock_overview_details';
+    const VIEW_LITTER_DETAILS = 'view_litter_details';
     const VIEW_LOCATION_DETAILS = 'view_location_details';
     const VIEW_MINIMAL_PARENT_DETAILS = 'view_minimal_parent_details';
     const VIEW_PEDIGREE_REGISTER_ABBREVIATION = 'view_pedigree_register_abbreviation';
@@ -89,12 +91,41 @@ class SqlView
     {
         switch ($viewName) {
             case self::VIEW_ANIMAL_LIVESTOCK_OVERVIEW_DETAILS: return self::animalLiveStockOverviewDetails();
+            case self::VIEW_LITTER_DETAILS: return self::litterDetails();
             case self::VIEW_LOCATION_DETAILS: return self::locationDetails();
             case self::VIEW_MINIMAL_PARENT_DETAILS: return self::minimalParentDetails();
             case self::VIEW_PEDIGREE_REGISTER_ABBREVIATION: return self::pedigreeRegisterAbbreviation();
             case self::VIEW_PERSON_FULL_NAME: return self::personFullName();
             default: return null;
         }
+    }
+
+
+    /**
+     * @return string
+     */
+    private static function litterDetails()
+    {
+        return "SELECT
+                  l.litter_id,
+                  r.id as pedigree_register_registration_id,
+                  l.breeder_number,
+                  l.breed_code,
+                  l.location_of_birth_id,
+                  l.ubn_of_birth,
+                  litter.status = '".RequestStateType::COMPLETED."' as is_completed
+                FROM (
+                  SELECT
+                    litter_id,
+                    MAX(NULLIF(breed_code,'')) as breed_code,
+                    MAX(NULLIF(NULLIF(SUBSTR(pedigree_number, 1,5),''),'00000')) as breeder_number,
+                    MAX(location_of_birth_id) as location_of_birth_id,
+                    MAX(NULLIF(ubn_of_birth,'')) as ubn_of_birth
+                  FROM animal a
+                  GROUP BY litter_id
+                )l
+                LEFT JOIN pedigree_register_registration r ON r.breeder_number = l.breeder_number
+                INNER JOIN litter ON litter.id = l.litter_id";
     }
 
 
@@ -270,4 +301,6 @@ class SqlView
                 ".SqlUtil::createSqlValuesString(Translation::getEnglishPredicateToAbbreviationArray())."
                 ) AS predicate(english, abbreviation) ON predicate.english = a.predicate";
     }
+
+
 }
