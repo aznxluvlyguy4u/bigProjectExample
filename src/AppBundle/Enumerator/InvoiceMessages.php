a9645b19b552c78210e23a7a91765a68ff607e07
werner <werner@jongensvantechniek.nl>
2018-04-24 15:16:47 +0200

    Add invoice message enums
    Former-commit-id: 5ee3d3b3df24841b13a042519c30374562689672

---
+++ src/AppBundle/Enumerator/InvoiceMessages.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace AppBundle\Enumerator;
+
+
+class InvoiceMessages
+{
+    const NEW_INVOICE_TYPE = "NEW_INVOICE";
+    const NEW_INVOICE_SUBJECT = "Nieuwe factuur";
+    const NEW_INVOICE_MESSAGE = "U heeft een nieuwe factuur ontvangen";
+}
