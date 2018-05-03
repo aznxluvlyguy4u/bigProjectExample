d8938747959ea36e058311db5bacc14c0360a026
werner <werner@jongensvantechniek.nl>
2018-05-03 14:37:59 +0200

    Add initial functions for Twinfield
    Former-commit-id: 681fa4034a61a66eb580c4c2c9cfb08b4739868d

--- src/AppBundle/Service/Invoice/TwinfieldService.php
+++ src/AppBundle/Service/Invoice/TwinfieldService.php
@@ -4,16 +4,35 @@
 namespace AppBundle\Service\Invoice;
 
 
-use PhpTwinfield\ApiConnectors\ArticleApiConnector;
+use AppBundle\Entity\Invoice;
+use AppBundle\Service\BaseSerializer;
+use AppBundle\Service\CacheService;
+use AppBundle\Service\ControllerServiceBase;
+use AppBundle\Service\UserService;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpTwinfield\ApiConnectors\InvoiceApiConnector;
 use PhpTwinfield\Secure\WebservicesAuthentication;
+use Symfony\Bridge\Monolog\Logger;
+use Symfony\Component\Translation\TranslatorInterface;
 
-class TwinfieldService
+class TwinfieldService extends ControllerServiceBase
 {
     private $authenticationConnection;
 
-    private $invoiceConnector;
+    private $invoiceConnection;
 
-    public function __construct()
+    public function __construct(BaseSerializer $baseSerializer, CacheService $cacheService, EntityManagerInterface $manager, UserService $userService, TranslatorInterface $translator, Logger $logger, $twinfieldUser, $twinfieldPassword, $twinfieldOrganisation)
     {
+        parent::__construct($baseSerializer, $cacheService, $manager, $userService, $translator, $logger);
+        $this->authenticationConnection = new WebservicesAuthentication($twinfieldUser, $twinfieldPassword, $twinfieldOrganisation);
+        $this->invoiceConnection = new InvoiceApiConnector($this->authenticationConnection);
+    }
+
+    public function sendInvoiceToTwinfield(Invoice $invoice) {
+        $twinfieldInvoice = new \PhpTwinfield\Invoice();
+        $twinfieldInvoice->setInvoiceDate($invoice->getInvoiceDate());
+        $twinfieldInvoice->setInvoiceNumber($invoice->getInvoiceNumber());
+        $twinfieldInvoice->setCurrency("euro");
+        $twinfieldInvoice->setInvoiceType("FACTUUR");
     }
 }
