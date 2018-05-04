d444d48a3f87e38f7ddbf19972c5a773613af54b
werner <werner@jongensvantechniek.nl>
2018-05-04 11:38:46 +0200

    Add skeleton files for twinfield services
    Former-commit-id: f68d4211380c4ba5e6773c99fffd57e8a615445a

---
+++ src/AppBundle/Service/Twinfield/TwinfieldCustomerService.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace AppBundle\Service\Twinfield;
+
+
+class TwinfieldCustomerService
+{
+
+}
--- src/AppBundle/Service/Twinfield/TwinfieldInvoiceService.php
+++ src/AppBundle/Service/Twinfield/TwinfieldInvoiceService.php
@@ -5,12 +5,15 @@ namespace AppBundle\Service\Invoice;
 
 
 use AppBundle\Entity\Invoice;
+use AppBundle\Entity\InvoiceRuleSelection;
 use AppBundle\Service\BaseSerializer;
 use AppBundle\Service\CacheService;
 use AppBundle\Service\ControllerServiceBase;
 use AppBundle\Service\UserService;
 use Doctrine\ORM\EntityManagerInterface;
+use PhpTwinfield\ApiConnectors\CustomerApiConnector;
 use PhpTwinfield\ApiConnectors\InvoiceApiConnector;
+use PhpTwinfield\InvoiceLine;
 use PhpTwinfield\Secure\WebservicesAuthentication;
 use Symfony\Bridge\Monolog\Logger;
 use Symfony\Component\Translation\TranslatorInterface;
@@ -21,11 +24,14 @@ class TwinfieldService extends ControllerServiceBase
 
     private $invoiceConnection;
 
+    private $customerConnection;
+
     public function __construct(BaseSerializer $baseSerializer, CacheService $cacheService, EntityManagerInterface $manager, UserService $userService, TranslatorInterface $translator, Logger $logger, $twinfieldUser, $twinfieldPassword, $twinfieldOrganisation)
     {
         parent::__construct($baseSerializer, $cacheService, $manager, $userService, $translator, $logger);
         $this->authenticationConnection = new WebservicesAuthentication($twinfieldUser, $twinfieldPassword, $twinfieldOrganisation);
         $this->invoiceConnection = new InvoiceApiConnector($this->authenticationConnection);
+        $this->customerConnection = new CustomerApiConnector($this->authenticationConnection);
     }
 
     public function sendInvoiceToTwinfield(Invoice $invoice) {
@@ -34,5 +40,21 @@ class TwinfieldService extends ControllerServiceBase
         $twinfieldInvoice->setInvoiceNumber($invoice->getInvoiceNumber());
         $twinfieldInvoice->setCurrency("euro");
         $twinfieldInvoice->setInvoiceType("FACTUUR");
+
+        /** @var InvoiceRuleSelection $selection */
+        foreach ($invoice->getInvoiceRuleSelections() as $selection) {
+            $line = new InvoiceLine();
+            $line->setQuantity($selection->getAmount());
+            $line->setDescription($selection->getInvoiceRule()->getDescription());
+            $line->setArticle($selection->getInvoiceRule()->getLedgerCategory()->getCode());
+            $line->setUnitsPriceExcl($selection->getInvoiceRule()->getPriceExclVat());
+            $line->setUnitsPriceInc(
+                ($selection->getInvoiceRule()->getPriceExclVat() * $selection->getInvoiceRule()->getVatPercentageRate())
+            );
+            $twinfieldInvoice->addLine($line);
+        }
+        $twinfieldInvoice->setDeliverAddressNumber($invoice->getCompanyAddressStreetNumber());
     }
+
+
 }
