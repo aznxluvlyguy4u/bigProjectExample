ca8992d2dc27fdd5d0f7a4555ace515eb3b316a0
werner <werner@jongensvantechniek.nl>
2018-05-17 16:42:59 +0200

    Add enums for formating date(time) objects
    Former-commit-id: bf616d45a9353669013f741145b03874d8c0aad8

--- src/AppBundle/Enumerator/DateTimeFormats.php
+++ src/AppBundle/Enumerator/DateTimeFormats.php
@@ -6,5 +6,7 @@ namespace AppBundle\Enumerator;
 
 class DateTimeFormats
 {
-
+    const DAY_MONTH_YEAR = "d-m-Y";
+    const YEAR = "Y";
+    const DAY_MONTH_YEAR_HOUR_MINUTE_SECOND = "d-m-Y H:i:s";
 }
