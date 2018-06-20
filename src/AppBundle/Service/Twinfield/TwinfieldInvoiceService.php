066c5c4770c902220c7d0b3ab64d4311b881f83a
werner <werner@jongensvantechniek.nl>
2018-06-20 10:42:27 +0200

    Add a reAuthenticate function, to reauthenticate the connection with twinfield
    Former-commit-id: 0c367ae6ad2106a2339d6a69bc7f6d484d99e323

--- src/AppBundle/Service/Twinfield/TwinfieldInvoiceService.php
+++ src/AppBundle/Service/Twinfield/TwinfieldInvoiceService.php
@@ -25,7 +25,14 @@ use Symfony\Component\Translation\TranslatorInterface;
 
 class TwinfieldInvoiceService extends ControllerServiceBase
 {
+    /** @var WebservicesAuthentication */
     private $authenticationConnection;
+
+    private $user;
+
+    private $password;
+
+    private $organisation;
     /**
      * @var InvoiceApiConnector
      */
@@ -37,6 +44,9 @@ class TwinfieldInvoiceService extends ControllerServiceBase
         $this->authenticationConnection = new WebservicesAuthentication($twinfieldUser, $twinfieldPassword, $twinfieldOrganisation);
         $this->invoiceConnection = new InvoiceApiConnector($this->authenticationConnection);
         $this->twinfieldCustomerService = $twinfieldCustomerService;
+        $this->user = $twinfieldUser;
+        $this->password = $twinfieldPassword;
+        $this->organisation = $twinfieldOrganisation;
     }
 
     public function sendInvoiceToTwinfield(Invoice $invoice) {
@@ -84,5 +94,59 @@ class TwinfieldInvoiceService extends ControllerServiceBase
         }
     }
 
+    public function reAuthenticate() {
+        $this->authenticationConnection = new WebservicesAuthentication($this->user, $this->password, $this->organisation);
+        $this->invoiceConnection = new InvoiceApiConnector($this->authenticationConnection);
+    }
+
+    public function sendAllInvoicesToTwinfield($invoices) {
+        $resultSet = [];
+
+        /** @var Invoice $invoice */
+        foreach ($invoices as $invoice) {
+            /** @var Customer $customer */
+            $customer = $this->twinfieldCustomerService->getSingleCustomer($invoice->getCompanyDebtorNumber(), $invoice->getCompanyTwinfieldAdministrationCode());
+            $customer->setCode($invoice->getCompanyDebtorNumber());
+            $office = new Office();
+            $office->setCode($invoice->getCompanyTwinfieldAdministrationCode());
+            $twinfieldInvoice = new \PhpTwinfield\Invoice();
+            $twinfieldInvoice->setInvoiceDate($invoice->getInvoiceDate());
+            $twinfieldInvoice->setCurrency("EUR");
+            $twinfieldInvoice->setInvoiceType("FACTUUR");
+            $twinfieldInvoice->setCustomer($customer);
+            $twinfieldInvoice->setOffice($office);
+            $twinfieldInvoice->setStatus("concept");
+            $twinfieldInvoice->setPaymentMethod("cash");
+            /** @var InvoiceRuleSelection $selection */
+            foreach ($invoice->getInvoiceRuleSelections() as $selection) {
+                $line = new InvoiceLine();
+                $line->setQuantity($selection->getAmount());
+                $line->setArticle($selection->getInvoiceRule()->getArticleCode());
+                switch ($selection->getInvoiceRule()->getVatPercentageRate()) {
+                    case 0:
+                        $line->setVatCode("VN");
+                        break;
+
+                    case 6:
+                        $line->setVatCode("VL");
+                        break;
+
+                    case 21:
+                        $line->setVatCode("VH");
+                        break;
+                }
+                if ($selection->getInvoiceRule()->getSubArticleCode()) {
+                    $line->setSubArticle($selection->getInvoiceRule()->getSubArticleCode());
+                }
+                $twinfieldInvoice->addLine($line);
+            }
+            $resultSet[] = $twinfieldInvoice;
+        }
+        try {
+            return $this->invoiceConnection->sendAll($resultSet);
+        } catch (Exception $e) {
+            return $e;
+        }
+    }
 
 }
