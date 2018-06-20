de7390adc02a2ba75c138974da22cf5da95bd271
Johnnie Ho <johnnieho89@gmail.com>
2018-06-20 13:17:15 +0200

    Add worker enumerators
    Former-commit-id: ca8462071a49f3356d6174437d90efe4c87ca8db

---
+++ src/AppBundle/Enumerator/ReportType.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace AppBundle\Enumerator;
+
+
+class ReportType
+{
+    const ANNUAL_ACTIVE_LIVE_STOCK = 1;
+    const ANNUAL_TE_100 = 2;
+    const FERTILIZER_ACCOUNTING = 3;
+    const INBREEDING_COEFFICIENT = 4;
+    const PEDIGREE_CERTIFICATE = 5;
+    const ANIMALS_OVERVIEW = 6;
+    const ANNUAL_ACTIVE_LIVE_STOCK_RAM_MATES = 7;
+    const OFF_SPRING = 8;
+    const PEDIGREE_REGISTER_OVERVIEW = 9;
+}
