098ddb38238be4a54aa663bcd22ed0619270b76f
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2018-09-08 09:32:07 +0200

    Ignore spaces in uln input for findAnimal() route
    Former-commit-id: f287733c867b6b87395b18fdc88f0f65389a1f37

--- src/AppBundle/Service/AnimalService.php
+++ src/AppBundle/Service/AnimalService.php
@@ -317,12 +317,13 @@ class AnimalService extends DeclareControllerServiceBase implements AnimalAPICon
         }
 
         $data = RequestUtil::getContentAsArray($request);
-        if(!Validator::verifyUlnFormat($data['uln'])) {
+        $uln = strtr($data->get('uln'), [' ' => '']);
+        if(!Validator::verifyUlnFormat($uln)) {
             return ResultUtil::errorResult('Dit is geen geldige ULN.', Response::HTTP_BAD_REQUEST);
         }
 
         try {
-            $animal = $this->getManager()->getRepository(Animal::class)->findByUlnOrPedigree($data['uln'], true);
+            $animal = $this->getManager()->getRepository(Animal::class)->findByUlnOrPedigree($uln, true);
             if(!$animal)
                 return ResultUtil::errorResult('Dit dier bestaat niet.', Response::HTTP_BAD_REQUEST);
 
