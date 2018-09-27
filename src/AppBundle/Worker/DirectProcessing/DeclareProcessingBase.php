bd704646fc4e2cef2ded897a4d7f1c3d8055765b
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2018-09-27 12:10:41 +0200

    Close last open residence by Animal and Location in DeclareProcessingBase
    Former-commit-id: 2d26f3c58f2cfbfd6e25186d7fa427f7ea002925

--- src/AppBundle/Worker/DirectProcessing/DeclareProcessingBase.php
+++ src/AppBundle/Worker/DirectProcessing/DeclareProcessingBase.php
@@ -4,9 +4,30 @@
 namespace AppBundle\Worker\DirectProcessing;
 
 
+use AppBundle\Entity\Animal;
+use AppBundle\Entity\AnimalResidence;
+use AppBundle\Entity\Location;
 use AppBundle\Service\ControllerServiceBase;
 
 class DeclareProcessingBase extends ControllerServiceBase
 {
+    /**
+     * @param Animal $animal
+     * @param Location $location
+     * @param \DateTime $endDate
+     */
+    protected function closeLastOpenAnimalResidence(Animal $animal, Location $location, $endDate)
+    {
+        if (!$endDate) {
+            return;
+        }
 
+        $animalResidence = $this->getManager()->getRepository(AnimalResidence::class)
+            ->getLastOpenResidenceOnLocation($location, $animal);
+        if ($animalResidence && !$animalResidence->getEndDate()) {
+            $animalResidence->setEndDate($endDate);
+            $animalResidence->setIsPending(false);
+            $this->getManager()->persist($animalResidence);
+        }
+    }
 }
