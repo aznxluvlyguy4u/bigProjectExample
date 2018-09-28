59f8d1331500927c3591dd5b715880007f197fcc
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2018-09-28 16:58:51 +0200

    Create new animal in DeclareProcessorBase
    Former-commit-id: eabc3036fc0d1af3241c4e0f09b7e96805434b3e

--- src/AppBundle/Worker/DirectProcessor/DeclareProcessorBase.php
+++ src/AppBundle/Worker/DirectProcessor/DeclareProcessorBase.php
@@ -7,11 +7,17 @@ namespace AppBundle\Worker\DirectProcessor;
 use AppBundle\Entity\Animal;
 use AppBundle\Entity\AnimalResidence;
 use AppBundle\Entity\DeclareBase;
+use AppBundle\Entity\Ewe;
 use AppBundle\Entity\Location;
+use AppBundle\Entity\Neuter;
+use AppBundle\Entity\Ram;
 use AppBundle\Entity\Tag;
+use AppBundle\Enumerator\AnimalObjectType;
+use AppBundle\Enumerator\GenderType;
 use AppBundle\Enumerator\JmsGroup;
 use AppBundle\Service\ControllerServiceBase;
 use AppBundle\Service\DeclareControllerServiceBase;
+use AppBundle\Util\StringUtil;
 
 class DeclareProcessorBase extends ControllerServiceBase
 {
@@ -62,5 +68,39 @@ class DeclareProcessorBase extends ControllerServiceBase
 
         return $this->getManager()->getRepository(Tag::class)
             ->findByUlnNumberAndCountryCode($ulnCountryCode, $ulnNumber);
+    }
+
+
+    /**
+     * @param string $ulnCountryCode
+     * @param string $ulnNumber
+     * @param string $gender
+     * @return Ewe|Neuter|Ram
+     */
+    protected function createNewAnimal($ulnCountryCode, $ulnNumber, $gender)
+    {
+        $gender = strtolower($gender);
+        $isRam = $gender === strtolower(GenderType::M)
+            || $gender === strtolower(GenderType::MALE)
+            || $gender === strtolower(AnimalObjectType::RAM);
+
+        $isEwe = $gender === strtolower(GenderType::V)
+            || $gender === strtolower(GenderType::FEMALE)
+            || $gender === strtolower(AnimalObjectType::EWE);
+
+        if ($isRam) {
+            $animal = new Ram();
+        } elseif ($isEwe) {
+            $animal = new Ewe();
+        } else {
+            $animal = new Neuter();
+        }
+
+        $animal->setUlnCountryCode($ulnCountryCode);
+        $animal->setUlnNumber($ulnNumber);
+        $animal->setAnimalOrderNumber(StringUtil::getLast5CharactersFromString($ulnNumber));
+        $animal->setIsAlive(true);
+
+        return $animal;
     }
 }
