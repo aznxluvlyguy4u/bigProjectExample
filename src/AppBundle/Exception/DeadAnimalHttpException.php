a0f09d3e60376f933e73741f19029616e51512d2
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2018-10-25 15:33:40 +0200

    Create AnimalNotOnDepartLocationHttpException and DeadAnimalHttpException
    Former-commit-id: aefeb461cf848db026dc647afa7ff0485453e403

---
+++ src/AppBundle/Exception/DeadAnimalHttpException.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace AppBundle\Exception;
+
+
+use AppBundle\Entity\Animal;
+use Symfony\Component\HttpKernel\Exception\PreconditionFailedHttpException;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class DeadAnimalHttpException extends PreconditionFailedHttpException
+{
+    /**
+     * DeclareToOtherCountryHttpException constructor.
+     * @param TranslatorInterface $translator
+     * @param string|null $uln
+     * @param \Exception|null $previous
+     * @param int $code
+     */
+    public function __construct(TranslatorInterface $translator,
+                                ?string $uln,
+                                \Exception $previous = null, int $code = 0)
+    {
+        parent::__construct(
+            $this->getMessageFromUln($translator, $uln),
+            $previous,
+            $code
+        );
+    }
+
+    /**
+     * @param TranslatorInterface $translator
+     * @param string|null $uln
+     * @return string
+     */
+    private function getMessageFromUln(TranslatorInterface $translator, ?string $uln): string
+    {
+        return $translator->trans('ANIMAL IS ALREADY DEAD'). '.' . (empty($uln) ? '' : ' ULN: '.$uln);
+    }
+}
