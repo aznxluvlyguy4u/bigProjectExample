36826211b6c56425203b5fe21d5de8a81cf1fc1b
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2018-12-10 11:17:00 +0100

    Use EnumInfo trait in SqsCommandType
    Former-commit-id: 39078e8f9b051be04bf1897225138a7c374edf2e

--- src/AppBundle/Enumerator/SqsCommandType.php
+++ src/AppBundle/Enumerator/SqsCommandType.php
@@ -4,8 +4,12 @@
 namespace AppBundle\Enumerator;
 
 
+use AppBundle\Traits\EnumInfo;
+
 class SqsCommandType
 {
+    use EnumInfo;
+
     const SYNC_HEALTH_CHECK = 1;
     const SYNC_ANIMAL_RELOCATION = 2;
 }
