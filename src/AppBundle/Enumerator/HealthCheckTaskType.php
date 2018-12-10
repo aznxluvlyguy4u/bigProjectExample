0dee537038f2edea43b039d527f98b5a5ec1b77e
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2018-12-10 11:18:34 +0100

    Create ProcessType and HealthCheckTaskType enumerators
    Former-commit-id: 5a635066106aa4c24539d2e52cae40a8724b803e

---
+++ src/AppBundle/Enumerator/HealthCheckTaskType.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace AppBundle\Enumerator;
+
+
+use AppBundle\Entity\RetrieveAnimals;
+
+class HealthCheckTaskType
+{
+    const RETRIEVE_ANIMALS = RetrieveAnimals::class;
+}
---
+++ src/AppBundle/Enumerator/ProcessType.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace AppBundle\Enumerator;
+
+use AppBundle\Traits\EnumInfo;
+
+/**
+ * Class ProcessType
+ *
+ * The names can only contain letters and numbers. No dashes, spaces and underscores.
+ */
+class ProcessType
+{
+    use EnumInfo;
+
+    const SQS_FEEDBACK_WORKER = 'SqsFeedbackWorker';
+}
