ae2caf801e6da842fcaa471758018987530d5049
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2018-12-11 14:21:06 +0100

    Extend location validation message
    Former-commit-id: 4607c2307af5546c0c29d78df611e5019f4ba65a

--- src/AppBundle/Service/DeclareControllerServiceBase.php
+++ src/AppBundle/Service/DeclareControllerServiceBase.php
@@ -338,7 +338,8 @@ abstract class DeclareControllerServiceBase extends ControllerServiceBase
     {
         if (!$location->isDutchLocation()) {
             $message = ucfirst($this->translator->trans(StringUtil::getDeclareTranslationKey($declareClazz, true)))
-            .' '.$this->translator->trans('ARE ONLY ALLOWED FOR DUTCH UBNS');
+            .' '.$this->translator->trans('ARE ONLY ALLOWED FOR DUTCH UBNS') . '. '
+            . $this->translator->trans('SELECT EXPORT TO RELOCATE ANIMALS TO A UBN IN ANOTHER COUNTRY');
             throw new PreconditionFailedHttpException($message);
         }
     }
