5347d4fdfd8fe363535a810e6ae784c9827e67c8
Rudolf Sneep <rudolf.sneep@protonmail.com>
2019-01-10 17:09:33 +0100

    Get Token by personId
    Former-commit-id: b04053c15877b7a6edd74bdcc2a70851ce9c5f19

--- src/AppBundle/Entity/TokenRepository.php
+++ src/AppBundle/Entity/TokenRepository.php
@@ -2,6 +2,20 @@
 
 namespace AppBundle\Entity;
 
+use AppBundle\Enumerator\TokenType;
+
 class TokenRepository extends BaseRepository {
 
+    /**
+     * @param Person $person
+     * @return Token|null
+     */
+    public function findByClientIdPrioritizedByGhostTokenType(Person $person): ?Token
+    {
+        $token = $this->findOneBy(['owner' => $person->getId(), 'type' => TokenType::GHOST]);
+        if (!$token) {
+            $token = $this->findOneBy(['owner' => $person->getId(), 'type' => TokenType::ACCESS]);
+        }
+        return $token;
+    }
 }
