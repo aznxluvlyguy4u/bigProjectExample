2ea5bfe83f0a22fe803818e615e8b8adbaa9a937
Werner <werner@jongensvantechniek.nl>
2019-03-11 16:48:18 +0100

    Add FeedbackQueueInvoiceMessageService
    Former-commit-id: fe72b3c41610e381d28f242106422c5a63cb823b

--- src/AppBundle/Service/FeedbackQueueInvoiceMessageService.php
+++ src/AppBundle/Service/FeedbackQueueInvoiceMessageService.php
@@ -4,7 +4,28 @@
 namespace AppBundle\Service;
 
 
+use AppBundle\Enumerator\SqsCommandType;
+use AppBundle\Entity\JsonFormat\CreateBatchInvoicesMessage;
+use Enqueue\Util\UUID;
+use function MongoDB\BSON\toJSON;
+use Symfony\Component\HttpFoundation\Request;
+
 class FeedbackQueueInvoiceMessageService
 {
+    /** @var BaseSerializer */
+    private $serializer;
 
+    /** @var AwsFeedbackQueueService */
+    private $feedbackQueueService;
+
+    public function instantiateServices(BaseSerializer $serializer, AwsFeedbackQueueService $awsFeedbackQueue) {
+        $this->serializer = $serializer;
+        $this->feedbackQueueService = $awsFeedbackQueue;
+    }
+
+    public function createBatchInvoiceMessage(Request $request) {
+        $requestJson = json_decode($request->getContent(), true);
+        $date = $requestJson["controlDate"];
+        return $this->feedbackQueueService->send($date, "BATCH_INVOICE_GENERATION", UUID::generate());
+    }
 }
