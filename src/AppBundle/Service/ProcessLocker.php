1a6440d979e90d21d32f09bc05258c30ffbfbfa7
Rudolf Sneep <rudolf.sneep@protonmail.com>
2019-04-24 10:27:26 +0200

    Remove SqsFeedbackProcessor info logs
    Former-commit-id: 5e7437fc8a828bf6d53ab4a37d49ab9ab471b1e6

--- src/AppBundle/Service/ProcessLocker.php
+++ src/AppBundle/Service/ProcessLocker.php
@@ -112,7 +112,6 @@ class ProcessLocker implements ProcessLockerInterface
         $processes[$newProcessId] = $newProcessId;
 
         $this->updateLockFileByGroupName($processGroupName, $processes);
-        $this->logger->info('Added process ' . $newProcessId);
 
         return $newProcessId;
     }
@@ -141,9 +140,6 @@ class ProcessLocker implements ProcessLockerInterface
         if (key_exists($processId, $processes)) {
             unset($processes[$processId]);
             $this->updateLockFileByGroupName($processGroupName, $processes);
-            $this->logger->info('Removed process, processId: ' . $processId);
-        } else {
-            $this->logger->info('Process is not locked, processId: ' . $processId);
         }
     }
 
@@ -213,9 +209,7 @@ class ProcessLocker implements ProcessLockerInterface
             $this->updateLockFile($lockFilePath, []);
         }
 
-        $this->logger->info('Get lockfile info from: '.$lockFilePath);
         $content = file_get_contents($lockFilePath);
-        $this->logger->info('Lockfile content: ' . ($content ? $content : 'empty'));
         return json_decode($content, true);
     }
 
