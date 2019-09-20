cfbeb0c44d9821cf61214bf8de5cf9a5c795d26c
Rudolf Sneep <rudolf.sneep@protonmail.com>
2019-09-20 10:23:01 +0200

    Fix and add more subtotals to company register report
    Former-commit-id: 692b774cf25835a6bebb373714447b8a5b0b5094

--- src/AppBundle/Service/Report/CompanyRegisterReportService.php
+++ src/AppBundle/Service/Report/CompanyRegisterReportService.php
@@ -251,6 +251,12 @@ WHERE a.id IN (
     COALESCE(SUM(count) FILTER ( WHERE type = 'Ewe' AND one_year_or_older ISNULL ), 0) AS ewes_missing_date_of_birth,
     COALESCE(SUM(count) FILTER ( WHERE type = 'Ram' AND one_year_or_older ISNULL ), 0) AS rams_missing_date_of_birth,
     COALESCE(SUM(count) FILTER ( WHERE type = 'Neuter' AND one_year_or_older ISNULL ), 0) AS neuters_missing_date_of_birth,
+    COALESCE(SUM(count) FILTER ( WHERE type = 'Ewe' ), 0) AS total_ewes,
+    COALESCE(SUM(count) FILTER ( WHERE type = 'Ram' ), 0) AS total_rams,
+    COALESCE(SUM(count) FILTER ( WHERE type = 'Neuter' ), 0) AS total_neuters,
+    COALESCE(SUM(count) FILTER ( WHERE one_year_or_older ), 0) AS total_one_year_or_older,
+    COALESCE(SUM(count) FILTER ( WHERE one_year_or_older = FALSE ), 0) AS total_younger_than_one_year,
+    COALESCE(SUM(count) FILTER ( WHERE one_year_or_older ISNULL ), 0) AS total_missing_date_of_birth,
     COALESCE(SUM(count) FILTER ( WHERE one_year_or_older ISNULL ), 0) AS animals_missing_date_of_birth,
     COALESCE(SUM(count), 0) AS total_animal_count_on_reference_date
 FROM (
