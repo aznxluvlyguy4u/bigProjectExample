91a8c1673f1cfafa3e6dff828020bea4ab202a49
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2020-02-15 17:09:00 +0100

    feat(StarEwesCalculationTaskService): prepare litter data
    Former-commit-id: 4c5b88c0695989a485f43eae1425c1e214c97ecd

--- src/AppBundle/Service/Task/StarEwesCalculationTaskService.php
+++ src/AppBundle/Service/Task/StarEwesCalculationTaskService.php
@@ -5,6 +5,7 @@ namespace AppBundle\Service\Task;
 
 use AppBundle\Entity\Location;
 use AppBundle\Entity\Person;
+use AppBundle\Util\LitterUtil;
 use AppBundle\Util\ResultUtil;
 use Doctrine\ORM\EntityManager;
 use Symfony\Bridge\Monolog\Logger;
@@ -42,10 +43,26 @@ class StarEwesCalculationTaskService
     {
         // LOGIC HERE!!
         try {
+            $this->prepareLitterData();
 
             return ResultUtil::successResult('ok');
         } catch (\Exception $exception) {
             return ResultUtil::errorResult($exception->getMessage(), $exception->getCode());
         }
+    }
+
+    private function prepareLitterData()
+    {
+        $this->logger->notice('Updating litter ordinals...');
+        $updatedLitterOrdinalCount = LitterUtil::updateLitterOrdinals($this->em->getConnection());
+        $this->logger->notice($updatedLitterOrdinalCount.' litter ordinals updated');
+        $removeLitterOrdinalCount = LitterUtil::removeLitterOrdinalFromRevokedLitters($this->em->getConnection());
+        $this->logger->notice($removeLitterOrdinalCount.' litter ordinals removed from revoked litters');
+
+        $this->logger->notice('Updating litter cumulativeBornAliveCount values ...');
+        $cumulativeBornAliveCountUpdates = LitterUtil::updateCumulativeBornAliveCount($this->em->getConnection());
+        $this->logger->notice($cumulativeBornAliveCountUpdates.' cumulativeBornAliveCount values updated');
+
+        LitterUtil::updateLitterOffspringExteriorAndStarEweValues($this->em->getConnection(), null, $this->logger);
     }
 }
