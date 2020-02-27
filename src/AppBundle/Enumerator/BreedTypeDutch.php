150e9ef9d07f8ec86cf8d38619cff44717ed979a
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2020-02-27 11:29:57 +0100

    feat: translate all BLIND_FACTOR as 'Erfelijke afwijking'
    Former-commit-id: 6c5afe1225de8ef77bfc95f1ea671d3cf50b480b

--- src/AppBundle/Enumerator/BreedTypeDutch.php
+++ src/AppBundle/Enumerator/BreedTypeDutch.php
@@ -8,7 +8,8 @@ class BreedTypeDutch
 {
     use EnumInfo;
 
-    const BLIND_FACTOR = "BLINDFACTOR";
+    const BLIND_FACTOR = "ERFELIJKE AFWIJKING"; // TODO remove this when BLIND_FACTOR is replaced in the database
+    // const HEREDITARY_DEVIATION = "ERFELIJKE AFWIJKING"; TODO only add this if BLIND_FACTOR is replaced in the database
     const MEAT_LAMB_FATHER = "VLEESLAMVADERDIER";
     const MEAT_LAMB_MOTHER = "VLEESLAMMOEDERDIER";
     const PARENT_ANIMAL = "OUDERDIER";
