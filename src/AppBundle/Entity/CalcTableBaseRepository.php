a5a2e34848862a9521f801fe2f28a1b1e29bb954
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2020-04-20 13:22:53 +0200

    feat(CalcTableBaseRepository): set log level to debug for generating calc tables

--- src/AppBundle/Entity/CalcTableBaseRepository.php
+++ src/AppBundle/Entity/CalcTableBaseRepository.php
@@ -4,15 +4,15 @@
 namespace AppBundle\Entity;
 
 
+use AppBundle\Util\LoggerUtil;
 use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
 
 class CalcTableBaseRepository extends BaseRepository
 {
     private function log(?LoggerInterface $logger, string $message)
     {
-        if ($logger) {
-            $logger->notice($message);
-        }
+        LoggerUtil::log($message, $logger, LogLevel::DEBUG);
     }
 
     protected function logClearingTable(?LoggerInterface $logger, string $tableName)
