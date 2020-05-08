76e372c87034d193a02d7ae40d06eb14e26dd2d2
Rudolf Sneep <rudolf@jongensvantechniek.nl>
2020-05-08 19:28:49 +0200

    feat(NsfoResidencesFixCommand): create command suitable for cron job fix for animal residences

---
+++ src/AppBundle/Command/NsfoResidencesFixCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Service\DataFix\UbnHistoryFixer;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class NsfoResidencesFixCommand extends ContainerAwareCommand
+{
+    const TITLE = 'FIX ALL ANIMAL RESIDENCES BY CURRENT LOCATION OF ANIMALS';
+
+    protected function configure()
+    {
+        $this
+            ->setName('nsfo:residences:fix')
+            ->setDescription(self::TITLE)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** @var UbnHistoryFixer $ubnHistoryFixer */
+        $ubnHistoryFixer = $this->getContainer()->get(UbnHistoryFixer::class);
+        $ubnHistoryFixer->fixAllAnimalResidenceRecordsByCurrentAnimalLocation();
+    }
+
+}
