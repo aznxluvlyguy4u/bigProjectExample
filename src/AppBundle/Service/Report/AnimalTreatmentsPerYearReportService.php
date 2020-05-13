70c356eeced699dfaafd0da8ef03911daf7fccef
davidmesman <david@uptimemonkeys.com>
2020-05-13 13:00:34 +0200

    Added the animal stn, father stn and mother stn

--- src/AppBundle/Service/Report/AnimalTreatmentsPerYearReportService.php
+++ src/AppBundle/Service/Report/AnimalTreatmentsPerYearReportService.php
@@ -91,23 +91,37 @@ class AnimalTreatmentsPerYearReportService extends ReportServiceBase
 
         $sql = "
             SELECT 
-                a.id,
                 t.description AS treatment_description,
                 t.start_date AS latest_start_date,
                 COUNT(t.id) AS treatment_count,
+                a.id,
                 a.gender,
-                va.n_ling,
                 CONCAT(a.uln_country_code, a.uln_number) AS uln,
                 a.date_of_birth,
                 a.breed_code,
-                p.abbreviation AS pedigree_register
+                CONCAT(a.pedigree_country_code, a.pedigree_number) AS animal_stn,
+                CONCAT(father.pedigree_country_code, father.pedigree_number) AS stn_father,
+                CONCAT(mother.pedigree_country_code, mother.pedigree_number) AS stn_mother,
+                p.abbreviation AS pedigree_register,
+                va.n_ling                
             FROM animal a
             INNER JOIN view_animal_livestock_overview_details va ON a.id = va.animal_id
             INNER JOIN treatment_animal ta ON a.id = ta.animal_id
             INNER JOIN treatment t ON ta.treatment_id = t.id
             INNER JOIN pedigree_register p ON a.pedigree_register_id = p.id
+            INNER JOIN animal father ON father.id = a.parent_father_id
+            INNER JOIN animal mother ON mother.id = a.parent_mother_id
             ".$mainFilter."
-            GROUP BY t.description, t.start_date, a.id, p.abbreviation, va.n_ling
+            GROUP BY 
+            t.description, 
+            t.start_date, 
+            a.id, 
+            p.abbreviation, 
+            va.n_ling,         
+            father.pedigree_country_code,
+            father.pedigree_number,
+            mother.pedigree_country_code,
+            mother.pedigree_number
         ";
 
         $conn = $this->em->getConnection();
@@ -131,12 +145,15 @@ class AnimalTreatmentsPerYearReportService extends ReportServiceBase
                 $animalId = $item['id'];
                 $result[$animalId]['id'] = $animalId;
                 $result[$animalId][ReportServiceBase::staticTranslateColumnHeader($this->translator, 'uln')] = $item['uln'];
+                $result[$animalId][ReportServiceBase::staticTranslateColumnHeader($this->translator, 'stn')] = $item['animal_stn'];
                 $result[$animalId][ReportServiceBase::staticTranslateColumnHeader($this->translator, 'date_of_birth')] = $item['date_of_birth'];
                 $result[$animalId][ReportServiceBase::staticTranslateColumnHeader($this->translator, 'n_ling')] =
                     ($item['n_ling']) ? $item['n_ling'] : '-';
                 $result[$animalId][ReportServiceBase::staticTranslateColumnHeader($this->translator, 'gender')] = $item['gender'];
                 $result[$animalId][ReportServiceBase::staticTranslateColumnHeader($this->translator, 'breed_code')] = $item['breed_code'];
                 $result[$animalId][ReportServiceBase::staticTranslateColumnHeader($this->translator, 'pedigree_register')] = $item['pedigree_register'];
+                $result[$animalId][ReportServiceBase::staticTranslateColumnHeader($this->translator, 'stn_father')] = $item['stn_father'];
+                $result[$animalId][ReportServiceBase::staticTranslateColumnHeader($this->translator, 'stn_mother')] = $item['stn_mother'];
 
                 $filteredData = array_filter($data,
                     function (array $item) use ($animalId, $treatment) {
