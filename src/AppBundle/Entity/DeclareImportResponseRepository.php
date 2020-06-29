3f9e3352582ee57ad69ad8deed9ca244c45dd947
davidmesman <david@uptimemonkeys.com>
2020-06-29 16:53:18 +0200

    feat(DeclareArrivalResponseRepository+): Added an option to search for collar-color and -number on and made the search case-insensitive

--- src/AppBundle/Entity/DeclareImportResponseRepository.php
+++ src/AppBundle/Entity/DeclareImportResponseRepository.php
@@ -35,65 +35,66 @@ class DeclareImportResponseRepository extends BaseRepository {
         $locationId = $location->getId();
         if(!is_int($locationId)) { return []; }
 
-        $countSql = "SELECT COUNT(*) AS totalitems
-             FROM declare_base b
-               INNER JOIN declare_import a ON a.id = b.id
+        $filter = "
+               WHERE ( 
+                    LOWER(a.uln_country_code) LIKE LOWER(:query) OR
+                    a.uln_number LIKE :query OR 
+                    LOWER(a.pedigree_country_code) LIKE LOWER(:query) OR
+                    a.pedigree_number LIKE :query OR
+                    LOWER(a.collar_color) LIKE LOWER(:query) OR
+                    a.collar_number LIKE :query OR
+                    CONCAT(LOWER(a.collar_color), a.collar_number) LIKE LOWER(:query)
+                  ) 
+                  AND request_state IN (
+                    '".RequestStateType::OPEN."', 
+                    '".RequestStateType::REVOKING."', 
+                    '".RequestStateType::REVOKED."', 
+                    '".RequestStateType::FINISHED."', 
+                    '".RequestStateType::FINISHED_WITH_WARNING."'
+                  )
+             AND a.location_id = ".$locationId." 
+        ";
+
+        $joins = "
+              INNER JOIN declare_import di ON di.id = db.id
                INNER JOIN (
-                 SELECT y.request_id, y.message_number, y.error_code, y.error_message
-                 FROM declare_base_response y
+                 SELECT dbr.request_id, dbr.message_number, dbr.error_code, dbr.error_message
+                 FROM declare_base_response dbr
                    INNER JOIN (
                                 SELECT request_id, MAX(log_date) as log_date
                                 FROM declare_base_response
                                 GROUP BY request_id
-                              ) z ON z.log_date = y.log_date AND z.request_id = y.request_id
-                 )r ON r.request_id = b.request_id
-             WHERE ( 
-                    a.uln_country_code LIKE :query OR
-                    a.uln_number LIKE :query OR 
-                    a.pedigree_country_code LIKE :query OR
-                    a.pedigree_number LIKE :query
-                  ) 
-                  AND request_state IN (
-                    '".RequestStateType::OPEN."', 
-                    '".RequestStateType::REVOKING."', 
-                    '".RequestStateType::REVOKED."', 
-                    '".RequestStateType::FINISHED."', 
-                    '".RequestStateType::FINISHED_WITH_WARNING."'
-                  )
-             AND location_id = ".$locationId." 
-             GROUP BY location_id"
+                              ) dbri ON dbri.log_date = dbr.log_date AND dbri.request_id = dbr.request_id
+                 ) r ON r.request_id = db.request_id
+                 LEFT JOIN animal a ON a.id = di.animal_id 
+        ";
+
+        $countSql = "SELECT COUNT(*) AS totalitems
+             FROM declare_base db
+             ".$joins."   
+             ".$filter."
+             GROUP BY a.location_id"
         ;
 
-        $sql = "SELECT b.request_id, log_date, a.uln_country_code, a.uln_number,
-                  pedigree_country_code, pedigree_number, is_import_animal,
-                  import_date as arrival_date, animal_country_origin as country_origin, animal_uln_number_origin, request_state, 
-                  r.message_number, r.error_code, r.error_message
-                FROM declare_base b
-                  INNER JOIN declare_import a ON a.id = b.id
-                  INNER JOIN (
-                    SELECT y.request_id, y.message_number, y.error_code, y.error_message
-                    FROM declare_base_response y
-                      INNER JOIN (
-                                   SELECT request_id, MAX(log_date) as log_date
-                                   FROM declare_base_response
-                                   GROUP BY request_id
-                                 ) z ON z.log_date = y.log_date AND z.request_id = y.request_id
-                    )r ON r.request_id = b.request_id
-                WHERE ( 
-                    a.uln_country_code LIKE :query OR
-                    a.uln_number LIKE :query OR 
-                    a.pedigree_country_code LIKE :query OR
-                    a.pedigree_number LIKE :query
-                  ) 
-                  AND request_state IN (
-                    '".RequestStateType::OPEN."', 
-                    '".RequestStateType::REVOKING."', 
-                    '".RequestStateType::REVOKED."', 
-                    '".RequestStateType::FINISHED."', 
-                    '".RequestStateType::FINISHED_WITH_WARNING."'
-                  )
-                AND location_id = ".$locationId." 
-                ORDER BY b.log_date DESC
+        $sql = "SELECT 
+                    db.request_id, 
+                    log_date, 
+                    a.uln_country_code, 
+                    a.uln_number,
+                    a.pedigree_country_code, 
+                    a.pedigree_number, 
+                    a.is_import_animal,
+                    import_date as arrival_date, 
+                    a.animal_country_origin as country_origin, 
+                    animal_uln_number_origin, 
+                    request_state, 
+                    r.message_number, 
+                    r.error_code, 
+                    r.error_message
+                FROM declare_base db
+             ".$joins."   
+             ".$filter."
+                ORDER BY db.log_date DESC
                 OFFSET 10 * (".$page." - 1)
                 FETCH NEXT 10 ROWS ONLY"
         ;
